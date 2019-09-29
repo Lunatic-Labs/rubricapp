9fa48cff45a31ff45f6405456029dffe33a02602
runqzhao <runqzhao@uiowa.edu>
2019-09-28 23:49:39 -0500

    fix label

--- building_user_login_system-master/finish/test.py
+++ building_user_login_system-master/finish/test.py
@@ -1,9 +1,4 @@
 import openpyxl
 from openpyxl import load_workbook
 
-# medical_data = openpyxl.load_workbook("d:\\MIMICPython\\resources\\medical_new.xlsx")
-# medical_data_worksheet = medical_data['medical_new']
-# last_col = [x.value for x in list(medical_data_worksheet.iter_cols())[-2]]
-# for value in last_col:
-#     if value == "Alzheimer's-disease":
-#         newvalue ==
+#
