bd3b07e275015165520645571753da0751f49205
jzheng16 <jzheng16@uiowa.edu>
2020-03-05 17:57:16 -0600

    fixed issue about downloading getting the older version of file

--- building_user_login_system-master/finish/app.py
+++ building_user_login_system-master/finish/app.py
@@ -1574,16 +1574,15 @@ def download_page(project_id, evaluation_name, group, type):
                                grades=temp_eva_in_group)
 
 
-def getNow():
-    t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    return t
+# def getNow():
+#     t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     return t
 
 
 # download xlsx file or html file
-@app.route('/download/<string:project_id>/<string:evaluation_name>', methods=['GET', 'POST'])
+@app.route('/download/<string:project_id>/<string:evaluation_name>/<string:current_time>', methods=['GET', 'POST'])
 @login_required
-def download(project_id, evaluation_name):
-    # evaluation_name = evaluation_name + "-" + current_time
+def download(project_id, evaluation_name, current_time):
     project = Permission.query.filter_by(project_id=project_id).first()
     path_to_load_project = "{}/{}/{}".format(base_directory, current_user.username, project.project)
     path_to_evaluation_file = "{}/evaluation.xlsx".format(path_to_load_project)
@@ -1600,10 +1599,9 @@ def download(project_id, evaluation_name):
     for col_item in list(group_worksheet.iter_cols())[0]:
         if col_item.value != "groupid":
             group_col.append(col_item.value)
-    current_time = getNow();
+    filename = "{}_{}_{}.xlsx".format(project.project, evaluation_name, current_time)
     if evaluation_name == "all_eva":
         try:
-            filename = "{}_{}_{}.xlsx".format(project.project, evaluation_name, current_time)
             return send_file(path_to_evaluation_file, attachment_filename=filename, as_attachment=True)
             # msg = "Successfully downloaded"
             # return redirect(url_for("project_profile_jumptool", project_id=project_id, msg=msg))
