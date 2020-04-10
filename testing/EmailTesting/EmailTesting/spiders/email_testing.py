a59c94870e23f98428121e5affdc597e8c7b487b
JackyBreak <jackybreak1997@gmail.com>
2020-04-09 21:09:04 -0500

    done testing with dispostable.com

--- testing/EmailTesting/EmailTesting/spiders/email_testing.py
+++ testing/EmailTesting/EmailTesting/spiders/email_testing.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-marker = "Testing1"
+marker = "TestEmail8"
+failure_emails = []
 
 
 class EmailTestingSpider(scrapy.Spider):
     name = 'email_testing'
     allowed_domains = ['dispostable.com']
-
     start_urls = []
-    for i in range(0, 21):
-        email_address = 'https://www.dispostable.com/inbox/rubricapp-a{}/'.format(str(i))
+    for i in range(0, 200):
+        email_address = 'https://www.dispostable.com/inbox/rubricapp-c{}/'.format(str(i))
         start_urls.append(email_address)
 
     def parse(self, response):
         content = response.xpath('//strong[contains(text(), "{}")]'.format(marker))
+
         if not content:
             url = response.url
             address_name = url.split("/")[-2]
             address_body = "@dispostable.com"
             address = address_name + address_body
-            print("Email Address " + address + " did not receive the email!\n")
+            failure_emails.append(address)
+            print("\nEmail Address " + address + " did not receive the email!")
+            print("total number of {} did not receive email".format(len(failure_emails)))
--- testing/EmailTesting/EmailTesting/spiders/main.py
+++ testing/EmailTesting/EmailTesting/spiders/main.py
@@ -2,5 +2,5 @@ from scrapy.cmdline import execute
 
 import sys
 import os
-
+# First run email_sender.py or send email through the web, then run email_testing to detect emails
 execute(["scrapy", "crawl", "email_testing"])
