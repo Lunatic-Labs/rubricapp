e520daa8024231ea2c5d36f53766c5e9fa8002c9
Brandon Myers <brandon-d-myers@uiowa.edu>
2020-10-29 01:18:55 -0500

    first selenium test and directions

--- tests/README.md
+++ tests/README.md
@@ -1,1 +1,11 @@
 set up Selenium
+
+requires packages
+
+unittest
+selenium
+nose
+
+
+run all tests with 
+nosetests
---
+++ tests/login_tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+from selenium.webdriver import Chrome
+
+class TestLogin(unittest.TestCase):
+
+    def test_login(self):
+        driver = Chrome()
+
+        with Chrome() as driver:
+            #your code inside this indent
+            driver.get("http://localhost:5000")
+            driver.find_element_by_link_text("Login").click()
+            driver.find_element_by_id("email").send_keys("sampleuser13@mailinator.com")
+            driver.find_element_by_id("password").send_keys("abcdefgh")
+            driver.find_element_by_css_selector(".btn").click()
+            self.assertEqual(driver.current_url, "http://localhost:5000/instructor_project")
+
+if __name__ == '__main__':
+    unittest.main()
