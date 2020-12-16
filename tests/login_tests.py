348fce7baa7ab8e95e73c00c2713dcbd1c8201c2
Junnan-wang <Wangjunnan2015@gmail.com>
2020-12-15 20:03:40 -0600

    Update signUp test

--- tests/login_tests.py
+++ tests/login_tests.py
@@ -4,6 +4,25 @@ from selenium.webdriver import Chrome
 
 class TestLogin(unittest.TestCase):
 
+
+    def test_signUp(self):
+        driver = Chrome()
+
+        with Chrome() as driver:
+            driver.get("http://localhost:5000")
+            driver.find_element_by_link_text("Sign up").click()
+            driver.find_element_by_id("email").send_keys("sampleuser13@mailinator.com")
+            driver.find_element_by_id("password").send_keys("abcdefgh")
+            driver.find_element_by_css_selector(".btn").click()
+            if driver.current_url == "http://localhost:5000/login":  # if username already exists
+                self.assertTrue(True)
+            elif driver.current_url == "http://localhost:5000/signup": # if a new username created
+                self.assertTrue(True)
+            else:
+                self.assertTrue(False)
+          
+            
+
     def test_login(self):
         driver = Chrome()
 
@@ -15,6 +34,12 @@ class TestLogin(unittest.TestCase):
             driver.find_element_by_id("password").send_keys("abcdefgh")
             driver.find_element_by_css_selector(".btn").click()
             self.assertEqual(driver.current_url, "http://localhost:5000/instructor_project")
+
+
+
+
+    
+
 
 if __name__ == '__main__':
     unittest.main()
