63b54230df90fdae74443b378c058decc3ee6830
mekealbrown <97410284+mekealbrown@users.noreply.github.com>
2022-09-30 12:44:24 -0500

    Removed unneeded import

--- tests/loginDriver.py
+++ tests/loginDriver.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from signUpDriver import SignUp
 import time
 
 
