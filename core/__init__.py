2643df65185cf4b0b125a17abf83ea21f7eb7c8f
Dan-DDT <72162793+Dan-DDT@users.noreply.github.com>
2022-10-09 12:02:33 -0500

    Migration modules

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,6 @@
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import Flask, render_template, redirect, url_for, request, send_file, jsonify
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from flask_sqlalchemy import SQLAlchemy
 from filelock import Timeout, FileLock
-from flask_bootstrap import Bootstrap
 from fpdf import FPDF, HTMLMixin
 from flask_wtf import FlaskForm
 
