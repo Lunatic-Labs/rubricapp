f568de4685ceca2a13cfc29dc83c3661640daa85
Etl1117 <evantaylorlipe@gmail.com>
2022-10-15 16:38:18 -0500

    Created new conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,37 @@
-"""
-This file essentially creates the app in a test-like environment
-for all test cases. This allows for all test cases to reuse this code
-so that this portion of a test is not repeated throughout every single
-test case.
-"""
-
 import pytest
+
 from app import create_app
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from app import Library
-
+from app import User
 
 @pytest.fixture()
-def test_client():
-    flask_app = create_app()
+def app():
+    app = create_app()
+    app.config.update ({
+        "TESTING": True,
+    })
+
     db = SQLAlchemy()
     login_manager = LoginManager()
     login_manager.login_view = "users.login"
     files_dir = "."
     register = Library()
-    def initialize_extensions(flask_app):
-        bootstrap = Bootstrap(flask_app)
-        db.init_app(flask_app)
-        login_manager.init_app(flask_app)
-    # Create a test client using the Flask application configured for testing
-    with flask_app.test_client() as testing_client:
-        yield testing_client # this is where the testing happens
 
-"""
-This might be a method for implementing authetication... Look at later
+    yield app
 
-class AuthActions(object):
-    def __init__(self, client):
-        self._client = client
+    def initialize_extensions(app):
+        bootstrap = Bootstrap(app)
+        db.init_app(app)
+        login_manager.init_app(app)
 
-    def login(self, username='test', password='test'):
-        return self._client.post(
-            '/auth/login',
-            data={'username': username, 'password': password}
-        )
+@pytest.fixture()
+def client(app):
+    return app.test_client()
 
-    def logout(self):
-        return self._client.get('/auth/logout')
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
 
-
-@pytest.fixture
-def auth(client):
-    return AuthActions(client)
-"""
--- tests/functional/test_index.py
+++ tests/functional/test_index.py
@@ -10,13 +10,13 @@ def index():
     return render_template('index.html')
 """
 
-def test_home_page(test_client):
+def test_home_page(client):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/' page is requested (GET)
     THEN check that the response is valid
     """
-    response = test_client.get('/')
+    response = client.get('/')
     assert response.status_code == 200
     assert b"Welcome to ELIPSS SkillBuilder" in response.data
     # assert b"This shouldn't work." in response.data
