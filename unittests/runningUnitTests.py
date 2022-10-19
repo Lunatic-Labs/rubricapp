a004e92c761d294428c787dbd55472dd199d9ba4
Luis Godinez <godinez.luis2020@gmail.com>
2022-10-19 16:32:49 -0500

    added test_select_row_by_group_id  to runningUnitTests.py

--- unittests/runningUnitTests.py
+++ unittests/runningUnitTests.py
@@ -18,5 +18,6 @@ from select_index_by_group_eva_owner_Tests import *
 from test_get_students_by_group import *
 from test_select_students_by_group import *
 from test_select_row_by_index import *
+from test_select_row_by_group_id import *
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
