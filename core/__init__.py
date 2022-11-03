e8dc90fd894cc807263eb38e5d771adec6ff228b
DanielDiazTortolero <danield2510@hotmail.com>
2022-11-03 16:47:26 -0500

    New module format
    Changed according to suggestions.

--- core/__init__.py
+++ core/__init__.py
@@ -79,9 +79,6 @@ base_directory = base_directory + "/users"
 import CRUD.evaluation
 import CRUD.permission
 import CRUD.project
-import CRUD.read
-import CRUD.update
-import CRUD.delete
 import functions
 import pages
 import operations
