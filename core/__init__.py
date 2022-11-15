1851ecc6db126e6d3ee0bc097d94d4cd8f98ab13
Etl1117 <evantaylorlipe@gmail.com>
2022-11-15 17:17:50 -0600

    Progress on conftest.py

--- core/__init__.py
+++ core/__init__.py
@@ -34,44 +34,54 @@ import smtplib
 from email.message import EmailMessage
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# from dataBase import *
-
+files_dir = "."
 register = Library()
-
-# file directory
-# requirement of two arguments: file address of app.py and fire address of root directory.
-files_dir = None
-if len(sys.argv) > 1:
-    files_dir = sys.argv[1]
-elif platform.node() in ['rubric.cs.uiowa.edu', 'rubric-dev.cs.uiowa.edu']:
-    files_dir = "/var/www/wsgi-scripts/rubric"
-else:
-    print(
-        "Requires argument: path to put files and database (suggestion is `pwd` when already in directory containing app.py)")
-    sys.exit(1)
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-if platform.node() in ['rubric.cs.uiowa.edu', 'rubric-dev.cs.uiowa.edu']:
-    dbpass = None
-    with open ("{}/dbpass".format(files_dir), 'r') as f:
-        dbpass = f.readline().rstrip()
-
-    dbuser = None
-    with open ("{}/dbuser".format(files_dir), 'r') as f:
-        dbuser = f.readline().rstrip()
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@127.0.0.1/rubric'.format(dbuser, dbpass)
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/account.db'.format(files_dir)
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+# from dataBase import *
+def create_app():
+
+    # file directory
+    # requirement of two arguments: file address of app.py and fire address of root directory.
+    if len(sys.argv) > 1:
+        files_dir = sys.argv[1]
+    elif platform.node() in ['rubric.cs.uiowa.edu', 'rubric-dev.cs.uiowa.edu']:
+        files_dir = "/var/www/wsgi-scripts/rubric"
+    else:
+        print(
+            "Requires argument: path to put files and database (suggestion is `pwd` when already in directory containing app.py)")
+        sys.exit(1)
+
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+    if platform.node() in ['rubric.cs.uiowa.edu', 'rubric-dev.cs.uiowa.edu']:
+        dbpass = None
+        with open ("{}/dbpass".format(files_dir), 'r') as f:
+            dbpass = f.readline().rstrip()
+
+        dbuser = None
+        with open ("{}/dbuser".format(files_dir), 'r') as f:
+            dbuser = f.readline().rstrip()
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@127.0.0.1/rubric'.format(dbuser, dbpass)
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/account.db'.format(files_dir)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    initialize_extensions(app)
+
+    return app
+
+
+def initialize_extensions(app):
+    bootstrap = Bootstrap(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+app = create_app()
 
 # SET THE BASE DIRECTORY
 os.chdir(".")
@@ -87,3 +97,4 @@ import CRUD.delete
 import functions
 import pages
 import operations
+from tests import *
--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -5,28 +5,32 @@ This file (conftest.py) creates the instance of a testing client.
 import pytest
 from flask import Flask
 from core import *
-from migrations import *
-from objects import *
+from objects import load_user
 from functions import *
 from flask_login import LoginManager, UserMixin
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import FlaskLoginClient
-from core import db
 
 @pytest.fixture()
 def client():
 
+    app = create_app()
     app.test_client_class = FlaskLoginClient
     app.config.update ({
-        'TESTING': True,
+        'TESTING': True
     })
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/core/account.db'.format(
+            files_dir)
         
-    user = load_user(2)
+    with app.app_context():
+        db.create_all()
+        user = load_user(2)
+        current_user = user
         # project_profile('test@email.comtest@email.comTestfull', 'sucess')
 
-    with app.test_client(user=user) as client:
-        yield client
+        with app.test_client(user=user) as client:
+            yield client
 
 # @pytest.fixture()
 # def choose_project(client):
