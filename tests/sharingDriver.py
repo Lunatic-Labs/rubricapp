981e4d17f5cd5494c0544eeceb348050b62aef79
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2022-11-29 22:11:25 -0600

    Removed unnecessary imports

--- tests/sharingDriver.py
+++ tests/sharingDriver.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from loginDriver import LogIn
-from ratingDriver import Rating
 import time
 
 class Sharing:
