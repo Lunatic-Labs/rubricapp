ea28cce07d8fdbbf81242389cd6bb377fb04327d
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2022-11-29 22:35:57 -0600

    Building off of work done from branch 298-test5_sharingpy-test1-checking-project-name-link-works, added a new function in sharingDriver.py that returns the current url after clicking manage projects button! Added test2 that checks if manage button works

--- tests/test5_sharing.py
+++ tests/test5_sharing.py
@@ -3,14 +3,16 @@ from sharingDriver import Sharing
 import unittest
 import random
 import os
-
+ 
 class TestSharing(unittest.TestCase):
-    email = "signupname" + str(random.getrandbits(12)) + str(
-        random.getrandbits(12)) + str(random.getrandbits(12)) + "@gmail.com"
-    invalidemail = "signup@test.test"
+    email = "signupname" + str(random.getrandbits(12)) + str(random.getrandbits(12)) + str(random.getrandbits(12)) + "@gmail.com"
     password = "abcdefgh"
-    reversepassword = "hgfedcba"
-    shortpassword = "abc"
+    projectname = "Project Name Test"
+    projectdescription = "Project Description"
+    rosterfile = os.getcwd().replace(os.path.join(os.path.sep, "tests"), "") + os.path.join(os.path.sep, "core") + os.path.join(os.path.sep, "sample_file") + os.path.join(os.path.sep, "rosters") + os.path.join(os.path.sep, "sample_roster.xlsx")
+    rubricfile = os.getcwd().replace(os.path.join(os.path.sep, "tests"), "") + os.path.join(os.path.sep, "core") + os.path.join(os.path.sep, "sample_file") + os.path.join(os.path.sep, "rubrics") + os.path.join(os.path.sep, "teamwork") + os.path.join(os.path.sep, "teamwork_scale3.json")
+    evaluationname = "Evaluation Name Test"
+    evaluationdescription = "This is a test description for the Evaluation!"
 
     # New Test for signing up
     def test0_signup_new_user (self):
@@ -20,8 +22,18 @@ class TestSharing(unittest.TestCase):
 
     # Manage Projects Page
     # New Test for checking project name link works
+    def test1_check_project_name_link_works(self):
+        create_sharing = Sharing()
+        url = create_sharing.create_sharing_return_current_url_after_clicking_project_name_link(self.email, self.password, self.projectname, self.projectdescription, self.rosterfile, self.rubricfile, self.evaluationname, self.evaluationdescription)
+        del create_sharing
+        self.assertTrue(url.find("load_project"))
 
     # New Test for checking if manage button works
+    def test2_check_if_manage_button_works(self):
+        create_sharing = Sharing()
+        url = create_sharing.create_sharing_return_current_url_after_clicking_manage_projects_button(self.email, self.password, self.projectname)
+        del create_sharing
+        self.assertTrue(url.find("project_profile"))
 
     # New Test for checking if delete button works
 
@@ -55,6 +67,6 @@ class TestSharing(unittest.TestCase):
     # New Test for checking if clicking update group button after editing a student's email that is valid works
 
     # New Test for checking if clicking update group button after editing a student's email that is invalid works
-
+    
 if __name__ == "__main__":
     unittest.main()
