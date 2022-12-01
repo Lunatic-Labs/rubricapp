852cd48938e0805cdb106287346b2fd658137477
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2022-12-01 12:47:44 -0600

    Buliding off of branch 303-test5_sharingpy-test6-checking-if-send-email-button-works, added test7 that checks if the send with scores switch works! Added new function that returns checked after clicking the send scores with switch

--- tests/sharingDriver.py
+++ tests/sharingDriver.py
@@ -47,6 +47,65 @@ class Sharing:
         self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
         self.driver.find_element(By.LINK_TEXT, projectname).click()
         return self.driver.current_url
+    
+    # New function that returns the current url after clicking manage projects button
+    def create_sharing_return_current_url_after_clicking_manage_projects_button(self, email, password):
+        Sharing.login_user(self, email, password)
+        self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+        self.driver.find_element(By.LINK_TEXT, "Manage").click()
+        return self.driver.current_url
+    
+    # New function that returns the text after clicking delete project button
+    def create_sharing_return_text_after_clicking_delete_project_button(self, email, password, projectname):
+        Sharing.login_user(self, email, password)
+        self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+        self.driver.find_element(By.LINK_TEXT, "Warning ! Delete the Rubric").click()
+        text = self.driver.switch_to.alert.text
+        self.driver.switch_to.alert.accept()
+        try:
+            self.driver.find_element(By.LINK_TEXT, projectname)
+        except:
+            text = "not found"
+        return text
+    
+    # New function that returns the current url after clicking downloading button
+    def create_sharing_return_current_url_after_clicking_downloading_button(self, email, password, projectname, projectdescription, roster_file, rubric_file, evaluationname, evaluationdescription):
+        Sharing.create_rating(self, email, password, projectname, projectdescription, roster_file, rubric_file, evaluationname, evaluationdescription)
+        self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+        self.driver.find_element(By.LINK_TEXT, "Download all evaluations").click()
+        return self.driver.current_url
+      
+    # New function that returns the current url after clicking the projects link in the breadcrumbs
+    def create_sharing_return_current_url_after_clicking_projects_link_in_the_breadcrumbs(self, email, password):
+         Sharing.login_user(self, email, password)
+         self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+         self.driver.find_element(By.LINK_TEXT, "Manage").click()
+         self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+         return self.driver.current_url
+ 
+    # New function that returns the text after clicking the send email button
+    def create_sharing_return_text_after_clicking_send_email_button(self, email, password, projectname, evaluationname):
+        Sharing.login_user(self, email, password)
+        self.driver.find_element(By.LINK_TEXT, projectname).click()
+        self.driver.find_element(By.LINK_TEXT, "Create a New Evaluation").click()
+        self.driver.find_element(By.ID, "evaluation_name").send_keys(evaluationname + "2")
+        self.driver.find_element(By.ID, "evaluation_submit").click()
+        self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+        self.driver.find_element(By.LINK_TEXT, "Manage").click()
+        time.sleep(1)
+        self.driver.find_element(By.LINK_TEXT, "Send Email").click()
+        text = self.driver.find_element(By.CLASS_NAME, "sending_progress_message").text
+        return text
+    
+
+    # New function that returns checked after clicking the send with scores switch
+    def create_sharing_return_checked_after_clicking_send_with_scores_switch(self, email, password):
+        Sharing.login_user(self, email, password)
+        self.driver.find_element(By.LINK_TEXT, "Manage Projects").click()
+        self.driver.find_element(By.LINK_TEXT, "Manage").click()
+        self.driver.find_element(By.CLASS_NAME, "switch").click()
+        checked = self.driver.find_element(By.CLASS_NAME, "switchCheckbox").get_attribute("checked")
+        return checked
 
     def __del__(self):
         self.driver.quit()
