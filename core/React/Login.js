708da4c2c984c1bba2bc74cdcd289295f85418c5
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-01-13 00:07:24 -0600

    Created Login.js and newlogin.html!
    In "core/React/Login.js" file:
    - Added React code that contains a main component with html elements that make up the original login page!
    - Added supported class names from bootstrap!
    In "core/templates/newlogin.html" file:
    - Added boiler plate html code!
    - Added link tags for bootstrap!
    - Added script tag for boostrap!
    - Added script tags for React!
    -Added script tags for Login.js!

---
+++ core/React/Login.js
@@ -0,0 +1,42 @@
+class Main extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+                <div className="container" style={{"max-width":"20rem"}}>
+                    <div className="row mt-5 mb-5">
+                        <h1 className="text-center">ELIPSS SkillBuilder</h1>
+                    </div>
+                    <div className="alert-info"><h4></h4></div>
+                    <div className="row mb-3">
+                        <h2>Please Login:</h2>
+                        <a href="/signup">Don't yet have an account? Sign up.</a>
+                    </div>
+                    <div className="row">
+                        <form className="card d-flex" method="POST" action="/login">
+                            <div className="card-body row">
+                                <div className="column d-flex gap-2 mt-1 mb-3">
+                                    <label id="emailLabel">Email</label>
+                                    <input id="email" type="email" placeholder=""/>
+                                </div>
+                                <div className="column d-flex gap-2 mt-1 mb-3">
+                                    <label id="passwordLabel">Password</label>
+                                    <input id="password" type="password" placeholder=""/>
+                                </div>
+                                <div className="column d-flex gap-2 mt-1 mb-3">
+                                    <input id="rememberMeInput" type="checkbox"/>
+                                    <label id="rememberMeLabel">Remember Me</label>
+                                </div>
+                                <div className="column">
+                                    <button className="btn btn-dark" id="loginButton" type="submit">Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
