7e9482549594daf73339cf622953cec8a103b8a9
Luis Godinez <godinez.luis2020@gmail.com>
2023-02-23 15:27:35 -0600

    removed seasons enum. Now term. Experimenting with db.ForeignKey() syntax

--- models/SFI.py
+++ models/SFI.py
@@ -1,8 +1,7 @@
 from core import db, UserMixin
-from sqlalchemy import ForeignKey
 
 class SuggestionsForImprovement(UserMixin, db.Model):
     sfi_id = db.Column(db.Integer, primary_key=True)
-    rubric_id = db.Column(db.Integer, ForeignKey("Rubric.rubric_id"), nullable=False)
-    category_id = db.Column(db.Integer,ForeignKey("Category.category_id"), nullable=False)
-    sfi_text = db.Column(db.String(10000), nullable=False)
+    rubric_id = db.Column(db.Integer, db.ForeignKey("Rubric.rubric_id"), nullable=False)
+    category_id = db.Column(db.Integer,db.ForeignKey("Category.category_id"), nullable=False)
+    sfi_text = db.Column(db.JSON, nullable=False)
