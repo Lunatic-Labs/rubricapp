833cff6c23e035a34292abdc8a37ce3fa8881e48
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-03-02 16:16:56 -0600

    Updated name of table in models/User.py from User to Users because User table already exits and our new table is one that will be needed to implement in the future

--- models/User.py
+++ models/User.py
@@ -1,10 +1,11 @@
-from core import db, UserMixin
-from sqlalchemy import ForeignKey
+from core import db, UserMixin, generate_password_hash
+from sqlalchemy import ForeignKey 
+from sqlalchemy import BOOLEAN
 # tables in database; each class match to a table in database
 #   *size of username, project_id, owner, project_name should be consistent in different tables.
 #   *password is encrypted
 
-class User(UserMixin, db.Model):
+class Users(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(30), nullable=False)
     lname = db.Column(db.String(30), nullable=False)
@@ -12,10 +13,8 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(80), nullable=False)
     role = db.Column(db.String(20), nullable=False)     #role in university; ex. instructor or ta
     lms_id = db.Column(db.Integer, unique=True, nullable=True)
-    consent = db.Column(db.Bool, nullable=False)
+    consent = db.Column(db.Boolean, nullable=False)
     owner_id = db.Column(db.Integer, ForeignKey("User.user_id"), nullable=False)
-    from core import db, UserMixin, generate_password_hash
-
 
 def get_users():
     try:
