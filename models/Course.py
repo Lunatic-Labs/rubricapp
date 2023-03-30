19a300001af71fc8c2de997fd9fd0e79caa78414
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-03-30 13:32:27 -0500

    Updates the api __init__.py file to import from the correct folder Routes. Updated the Course.py file to import Boolean attribute and use that imported Boolean type!

--- models/Course.py
+++ models/Course.py
@@ -1,8 +1,6 @@
 from core import db, UserMixin
 import enum
-from sqlalchemy import ForeignKey, Enum
-
-
+from sqlalchemy import ForeignKey, Enum, Boolean
 
 class Course(UserMixin, db.Model):
     course_id = db.Column(db.Integer, primary_key=True)
@@ -10,5 +8,5 @@ class Course(UserMixin, db.Model):
     course_name = db.Column(db.String(10), nullable=False)
     year = db.Column(db.Date, nullable=False)
     term = db.Column(db.String(50), nullable=False)
-    active = db.Column(db.Bool, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
     admin_id = db.Column(db.Integer, ForeignKey("User.user_id"), nullable=False)
