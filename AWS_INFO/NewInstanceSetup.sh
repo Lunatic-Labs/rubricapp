96b75d24a1ff64db9526374b28c9b9988cfb8449
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-03-31 19:13:39 -0500

    Updated changes to our bashfile! Updated host name to 0.0.0.0 in run.py!

--- AWS_INFO/NewInstanceSetup.sh
+++ AWS_INFO/NewInstanceSetup.sh
@@ -1,97 +1,54 @@
 #!/bin/bash
 
-#sudo shutdown -r now
+# These command needs to be run before anything!!!
+# sudo shutdown -r now
+# mkdir /home/ubuntu/rubricapp/POGIL_DEV
+# cd /home/ubuntu/rubricapp/POGIL_DEV
+# sudo apt install git
+# git clone https://github.com/Lunatic-Labs/rubricapp.git
 
 sudo apt update
-Sudo apt upgrade
-
+sudo apt upgrade
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-
 sudo apt install python3-venv
-
-mkdir POGIL_PRODUCTION
-cd POGIL_PRODUCTION
-
 python3 -m venv pogilenv
-
 source pogilenv/bin/activate
-
 pip install wheel
-
 pip install gunicorn flask
-
-#sudo apt install git
-
-#git clone https://github.com/Lunatic-Labs/rubricapp.git
-
-sed "s/debug=True/host='0.0.0.0'/" run.py
-##from core import create_app, app
-##app = Flask(__name__)
-
-
-##if __name__ == "__main__":
-    ##app.run(host='0.0.0.0')
-
-
-
 sudo ufw allow 5000
-
-pip install -r requirements.txt
-
-python3 run.py
-
-cd rubricapp
+pip install -r /home/POGIL_DEV/rubricapp/requirements.txt
+python3 run.py .
 gunicorn --bind 0.0.0.0:5000 wsgi:app
-
 deactivate
-
-[Unit] > /etc/systemd/system/rubricapp.service
-Description=Gunicorn instance to serve my rubricapp > /etc/systemd/system/rubricapp.service
-After=network.target > /etc/systemd/system/rubricapp.service
-
-[Service] > /etc/systemd/system/rubricapp.service
-User=ubuntu > /etc/systemd/system/rubricapp.service
-Group=www-data > /etc/systemd/system/rubricapp.service
-WorkingDirectory=/home/ubuntu/POGIL_PRODUCTION/rubricapp > /etc/systemd/system/rubricapp.service
-Environment= “PATH=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/ > /etc/systemd/system/rubricapp.service
-
-ExecStart=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/gunicorn --workers 3 --bind unix:rubricapp.sock -m 007 wsgi:app > /etc/systemd/system/rubricapp.service
-
-
-[Install] > /etc/systemd/system/rubricapp.service
-WantedBy=multi-user.target > /etc/systemd/system/rubricapp.service
-
-
+mkdir /home/ubuntu/POGIL_DEV/rubricapp/
+echo "[Unit]" >> /etc/systemd/system/rubricapp.service
+echo "Description=Gunicorn instance to serve my rubricapp" >> /etc/systemd/system/rubricapp.service
+echo "After=network.target" >> /etc/systemd/system/rubricapp.service
+echo "[Service]" >> /etc/systemd/system/rubricapp.service
+echo "User=ubuntu" >> /etc/systemd/system/rubricapp.service
+echo "Group=www-data" >> /etc/systemd/system/rubricapp.service
+echo "WorkingDirectory=/home/ubuntu/POGIL_PRODUCTION/rubricapp" >> /etc/systemd/system/rubricapp.service
+echo "Environment= \"PATH=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/\"" >> /etc/systemd/system/rubricapp.service
+echo "ExecStart=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/gunicorn --workers 3 --bind unix:rubricapp.sock -m 007 wsgi:app" >> /etc/systemd/system/rubricapp.service
+echo "[Install]" >> /etc/systemd/system/rubricapp.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/rubricapp.service
 sudo systemctl start rubricapp
 sudo systemctl enable rubricapp
-
 sudo systemctl status rubricapp
-
 sudo apt install nginx
-
-sudo  /etc/nginx/sites-available/rubricapp
-
-server { > /etc/nginx/sites-available/rubricapp
-    listen 80; > /etc/nginx/sites-available/rubricapp
-    #server_name 172.31.30.80 www. 172.31.30.80; > /etc/nginx/sites-available/rubricapp
-    server_name 172.31.30.80;; > /etc/nginx/sites-available/rubricapp
-
-    location / { > /etc/nginx/sites-available/rubricapp
-        include proxy_params; > /etc/nginx/sites-available/rubricapp
-        proxy_pass http://unix:/home/ubuntu/POGIL_PRODUCTION/rubricapp/rubricapp.sock; > /etc/nginx/sites-available/rubricapp
- } > /etc/nginx/sites-available/rubricapp
-} > /etc/nginx/sites-available/rubricapp
-
+echo "server {" >> /etc/nginx/sites-available/rubricapp
+echo "  listen 80;" >> /etc/nginx/sites-available/rubricapp
+echo "  server_name 172.31.30.80;" >> /etc/nginx/sites-available/rubricapp
+echo "\n" >> /etc/nginx/sites-availabe/rubricapp
+echo "  location / {" >> /etc/nginx/sites-available/rubricapp
+echo "      include proxy_params;" >> /etc/nginx/sites-available/rubricapp
+echo "      proxy_pass http://unix:/home/ubuntu/POGIL_PRODUCTION/rubricapp/rubricapp.sock;" >> /etc/nginx/sites-available/rubricapp
+echo "  }" >> /etc/nginx/sites-available/rubricapp
+echo "}" >> /etc/nginx/sites-available/rubricapp
 sudo ln -s /etc/nginx/sites-available/rubricapp /etc/nginx/sites-enabled
-
 sudo systemctl restart nginx
-
 sudo ufw delete allow 5000
 sudo ufw allow 'Nginx Full'
-
 sudo nginx -s reload
-
 sudo unlink /etc/nginx/sites-enabled/default
-
 sudo chmod 755 /home/ubuntu
-
