244da9b14aae4d7cec78ddbc54bea72aa6fcfc99
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-04-03 21:59:55 -0500

    Updated __init__.py file!

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 from flask_cors import CORS
 bp = Blueprint('api', __name__)
 cors = CORS(bp, resources={r"/api/*": {"origins": "*"}})
-from api.routes import User_routes
-from api.routes import Course_routes
-from api.routes import Rubric_routes
+from api.Routes import User_routes
+from api.Routes import Course_routes
+from api.Routes import Rubric_routes
