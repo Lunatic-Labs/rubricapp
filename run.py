d2ac5962fff4f0a2ff60ad390dbcdb76d25642e2
HondaCivic420 <tneema501@gmail.com>
2023-04-13 13:19:14 -0500

    updating wsgi.py and run.py to fit local machine needs

--- run.py
+++ run.py
@@ -6,7 +6,10 @@ from core import create_app, app
 
 if __name__ == '__main__':
     # db.create_all() # only run it the first time
+    #The app.run(debug = True) line is needed if we are working on our local machine
     app.run(debug=True)
+
+    #the app.run(host="0.0.0.0") line is currently commented out and if and only when we are seting up an EC2 instance
     #app.run(host="0.0.0.0")
 
     # token: MFFt4RjpXNMh1c_T1AQj
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+#the lines below are used to help set up the EC2 Instance
 from core import app
 if __name__ == "__main__":
     app.run()
+
