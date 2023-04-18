f7e6c94ea3761764c75a79caff7d4f76ad7246da
Luis Godinez <godinez.luis2020@gmail.com>
2023-04-18 15:58:20 -0500

    User.py

--- BackEndFlask/models/user.py
+++ BackEndFlask/models/user.py
@@ -3,11 +3,6 @@ from sqlalchemy import ForeignKey, BOOLEAN
 # tables in database; each class match to a table in database
 #   *size of username, project_id, owner, project_name should be consistent in different tables.
 #   *password is encrypted
-from sqlalchemy.exc import SQLAlchemyError
-
-class InvalidUserID(Exception):
-    "Raised when user_id does not exist!!!"
-    pass
 
 class Users(UserMixin, db.Model):
     __tablename__ = "Users"
@@ -23,182 +18,141 @@ class Users(UserMixin, db.Model):
     owner_id = db.Column(db.Integer, ForeignKey("Users.user_id", ondelete="CASCADE"), nullable=False)
 
 def get_users():
-    try: 
+    try:
         return Users.query.all()
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
+    except:
+        return False
 
 def get_user(user_id):
-    try:
-        one_user = Users.query.filter_by(user_id=user_id)
-        if(type(one_user) == type(None)):
-            raise InvalidUserID
-        return one_user
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
-    except InvalidUserID:
-        error = "Invalid user_id, user_id does not exist!"
-        return error
-
-def get_user_password(user_id):
-    try:
-        one_user = get_user(user_id).first()
-        if(type(one_user) == type(None)):
-            raise InvalidUserID
-        return one_user.password
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
-    except InvalidUserID:
-        error = "Invalid user_id, user_id does not exist!"
-        return error
+    one_user = Users.query.filter_by(id=user_id)
+    return one_user
 
 def create_user(user):
-    try: 
-        new_fname = user[0]
-        new_lname = user[1]
-        new_email = user[2]
-        new_password = user[3]
-        new_role = user[4]
-        new_lms_id = user[5]
-        new_consent = user[6]
-        new_owner_id = user[7]
+    try:
+        (new_fname, new_lname, new_email, new_password, new_role, new_lms_id, new_consent, new_owner_id) = user
         password_hash = generate_password_hash(new_password, method='sha256')
         new_user = Users(fname=new_fname, lname=new_lname, email=new_email, password=password_hash, role=new_role, lms_id=new_lms_id, consent=new_consent, owner_id=new_owner_id)
+        # new_user = Users(new_fname, new_lname, new_email, password_hash, new_role, new_lms_id, new_consent, new_owner_id)
+        print(new_user)
         db.session.add(new_user)
         db.session.commit()
-        return new_user
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
+        print(Users.query.all())
+        return True
+    except:
+        return False
 
-def replace_user(user, id):
+def replace_user(user_id, new_first_name, new_last_name, new_email, new_password, new_role, new_institution, new_consent):
     try:
-        one_user = Users.query.filter_by(user_id=id).first()
-        if(type(one_user) == type(None)):
-            raise InvalidUserID
-        one_user.fname = user[0]
-        one_user.lname = user[1]
-        one_user.email = user[2]
-        one_user.password = user[3]
-        one_user.role = user[4]
-        one_user.lms_id = user[5]
-        one_user.consent = user[6]
-        one_user.owner_id = user[7]
+        one_user = Users.query.filter_by(id=user_id)
+        password_hash = generate_password_hash(new_password, method='sha256')
+        one_user.first_name = new_first_name
+        one_user.last_name = new_last_name
+        one_user.email = new_email
+        one_user.password = password_hash
+        one_user.role = new_role
+        one_user.institution = new_institution
+        one_user.consent = new_consent
+        db.session.add(one_user)
         db.session.commit()
-        return one_user
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
-    except InvalidUserID:
-        error = "Invalid user_id, user_id does not exist!"
-        return error
+        all_users = Users.query.all()
+        return all_users 
+    except:
+        return False
 
-"""
-All code below has not been updated since user.py was modified on 4/15/2023
-"""
-
-# def update_user_first_name(user_id, new_first_name):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.first_name = new_first_name
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_first_name(user_id, new_first_name):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.first_name = new_first_name
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
     
-# def update_user_last_name(user_id, new_last_name):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.last_name = new_last_name
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_last_name(user_id, new_last_name):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.last_name = new_last_name
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-# def update_user_email(user_id, new_email):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.email = new_email
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_email(user_id, new_email):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.email = new_email
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-# def update_user_password(user_id, new_password):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         password_hash = generate_password_hash(new_password, method='sha256')
-#         one_user.password = password_hash
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_password(user_id, new_password):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        password_hash = generate_password_hash(new_password, method='sha256')
+        one_user.password = password_hash
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-# def update_user_role(user_id, new_role):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.role = new_role
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_role(user_id, new_role):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.role = new_role
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-# def update_user_institution(user_id, new_institution):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.institution = new_institution
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_institution(user_id, new_institution):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.institution = new_institution
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
+    
 
-# def update_user_consent(user_id, new_consent):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         one_user.consent = new_consent
-#         db.session.add(one_user)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except:
-#         return False
+def update_user_consent(user_id, new_consent):
+    try:
+        one_user = Users.query.filter_by(id=user_id).first()
+        one_user.consent = new_consent
+        db.session.add(one_user)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-"""
-Delete is meant for the summer semester!!!
-"""    
+def delete_user(user_id):
+    try:
+        Users.query.filter_by(id=user_id).delete()
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
 
-# def delete_user(user_id):
-#     try:
-#         one_user = Users.query.filter_by(user_id=user_id).first()
-#         Users.query.filter_by(user_id=user_id).delete()
-#         db.session.commit()
-#         return one_user
-#     except SQLAlchemyError as e:
-#         error = str(e.__dict__['orig'])
-#         return error
-
-# def delete_all_users():
-#     try:
-#         all_users = Users.query.all()
-#         db.session.delete(all_users)
-#         db.session.commit()
-#         all_users = Users.query.all()
-#         return all_users
-#     except SQLAlchemyError as e:
-#         error = str(e.__dict__['orig'])
-#         return error
+def delete_all_users():
+    try:
+        all_users = Users.query.all()
+        db.session.delete(all_users)
+        db.session.commit()
+        all_users = Users.query.all()
+        return all_users
+    except:
+        return False
