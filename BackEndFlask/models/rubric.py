b3bf7e68af0882914fc0514431219a5cc428bed8
Luis Godinez <godinez.luis2020@gmail.com>
2023-04-18 12:40:01 -0500

    rubric.py is now consistent with recent changes

--- BackEndFlask/models/rubric.py
+++ BackEndFlask/models/rubric.py
@@ -1,5 +1,10 @@
 from core import db, UserMixin
 from sqlalchemy import ForeignKey
+from sqlalchemy.exc import SQLAlchemyError
+
+class InvalidRubricID(Exception):
+    "Raised when rubric_id does not exist!!!"
+    pass
 
 class Rubric(UserMixin, db.Model):
     __tablename__ = "Rubric"
@@ -11,22 +16,50 @@ class Rubric(UserMixin, db.Model):
 def get_rubrics():
     try:
         return Rubric.query.all()
-    except:
-        return False
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
 
 def get_rubric(rubric_id):
-    one_rubric = Rubric.query.filter_by(rubric_id=rubric_id)
-    return one_rubric
+    try:
+        one_rubric = Rubric.query.filter_by(rubric_id=rubric_id)
+        if(type(one_rubric) == type(None)):
+            raise InvalidRubricID
+        return one_rubric
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
+    except InvalidRubricID:
+        error = "Invalid rubric_id, rubric_id does not exist!"
+        return error
 
 def create_rubric(rubric):
     try:
-        (new_rubric_name, new_rubric_desc) = rubric
+        new_rubric_name = rubric[0]
+        new_rubric_desc = rubric[1]
         new_rubric = Rubric(rubric_name=new_rubric_name, rubric_desc=new_rubric_desc)
         db.session.add(new_rubric)
         db.session.commit()
-        return True
-    except:
-        return False
+        return new_rubric
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error  
+
+def replace_rubric(rubric, rubric_id):
+    try:
+        one_rubric = Rubric.query.filter_by(rubric_id=rubric_id).first()
+        if(type(one_rubric) == type(None)):
+            raise InvalidRubricID
+        one_rubric.rubric_name = rubric[0]
+        one_rubric.rubric_desc = rubric[1]
+        db.session.commit()
+        return one_rubric
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
+    except InvalidRubricID:
+        error = "Invalid rubric_id, rubric_id does not exist!"
+        return error
 
 """
 All code below has not been updated since user.py was modified on 4/15/2023
