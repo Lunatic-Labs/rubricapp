9b51d86a39fdad27e9add3aefcfc181594d27435
HondaCivic420 <tneema501@gmail.com>
2023-04-20 15:27:35 -0500

    updating setupStartScript.sh

--- setupStartScript.sh
+++ setupStartScript.sh
@@ -1,18 +1,18 @@
 setupStartScript () {
 printf '=============== Create a startup script =============== \n'
 # create a .env file for the environment variables
- sudo bash -c 'cat > /home/ubuntu/RecipeAPI/\.env <<EOF
+ sudo bash -c 'cat > /home/ubuntu/rubricapp/\.env <<EOF
 export SECRET_KEY='wqrtaeysurid6lr7'
 export FLASK_CONFIG=development
-export DATABASE_URL='postgresql://thegaijin:12345678@cp3-db-instance.cjfdylbgjjyu.us-west-2.rds.amazonaws.com:5432/recipe_db'
 '
+#export DATABASE_URL='postgresql://thegaijin:12345678@cp3-db-instance.cjfdylbgjjyu.us-west-2.rds.amazonaws.com:5432/recipe_db'
 # create a startup script to start the virtual environment, load the environment variables and start the app
-sudo bash -c 'cat > /home/ubuntu/RecipeAPI/startenv.sh <<EOF
+sudo bash -c 'cat > /home/ubuntu/rubricapp/BackendBash.sh <<EOF
 #!/bin/bash
 cd /home/ubuntu
 ls
 source env/bin/activate
-cd RecipeAPI
+cd rubricapp
 source .env
 gunicorn manage:app
 '
