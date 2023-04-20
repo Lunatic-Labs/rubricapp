8354b46c6a60922a4e589c599b3d551c2eb77ce1
Aldo Velarde <aldo.velarde2542001@gmail.com>
2023-04-20 16:43:06 -0500

    Added datatable component to app.js

--- FrontEndReact/front-end-react/src/App.js
+++ FrontEndReact/front-end-react/src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import AdminAddUser from './View/Admin/AddUsers/AdminAddUser';
 import AdminViewUsers from './View/Admin/ViewUsers/AdminViewUsers';
 import AssessmentTask from './View/AssessmentTask/AssessmentTask';
+import DataTable from './View/Admin/ViewCourses/muitable'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to="/admin/view_users"/>} />
-        <Route path='/admin/view_users' element={<AdminViewUsers/>} />
-        <Route path='/admin/add_user' element={<AdminAddUser/>} />
-        <Route path='/assessment_task' element={<AssessmentTask/>}/>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Navigate to="/admin/view_users"/>} />
+    //     <Route path='/admin/view_users' element={<AdminViewUsers/>} />
+    //     <Route path='/admin/add_user' element={<AdminAddUser/>} />
+    //     <Route path='/assessment_task' element={<AssessmentTask/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <DataTable></DataTable>
   );
 }
 
