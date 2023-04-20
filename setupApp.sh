746b866a5351eedf17265fced1665ecfff532e57
HondaCivic420 <tneema501@gmail.com>
2023-04-20 15:51:42 -0500

    updating setupApp.sh

--- setupApp.sh
+++ setupApp.sh
@@ -3,5 +3,8 @@ printf '===============Setup the Application ================== \n'
 virtualenv -p python3 pogilenv
 source pogilenv/bin/activate
 git clone https://github.com/Lunatic-Labs/rubricapp.git
+cd rubricapp
+cd BackEndFlask
 pip3 install -r requirements.txt
+cd ..
 }
