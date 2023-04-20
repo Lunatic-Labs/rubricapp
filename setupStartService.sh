8c4343896c0bb32102d5205217926746a91344ac
HondaCivic420 <tneema501@gmail.com>
2023-04-20 15:29:11 -0500

    updating setupStartService.sh

--- setupStartService.sh
+++ setupStartService.sh
@@ -1,7 +1,7 @@
 setupStartService () {
 printf '============ Configure startup service ============= \n'
 # Create service that starts the app from the startup script
-sudo bash -c 'cat > /etc/systemd/system/recipe.service <<EOF
+sudo bash -c 'cat > /etc/systemd/system/rubricapp.service <<EOF
 [Unit]
 Description=Gunicorn instance to serve my rubricapp
 After=network.target
@@ -9,18 +9,18 @@ After=network.target
 [Service]
 User=ubuntu
 Group=www-data
-WorkingDirectory=/home/ubuntu/POGIL_PRODUCTION/rubricapp
-Environment= “PATH=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/
+WorkingDirectory=/home/ubuntu/rubricapp
+Environment= “PATH=/home/ubuntu/pogilenv/bin/
 
-ExecStart=/home/ubuntu/POGIL_PRODUCTION/pogilenv/bin/gunicorn --workers 3 --bind unix:rubricapp.sock -m 007 wsgi:app
+ExecStart=/home/ubuntu/pogilenv/bin/gunicorn --workers 3 --bind unix:rubricapp.sock -m 007 wsgi:app
 
 
 [Install]
 WantedBy=multi-user.target
 '
-sudo chmod 744 /home/ubuntu/RecipeAPI/startenv.sh
+sudo chmod 744 /home/ubuntu/rubricapp/BackendBash.sh
 sudo chmod 664 /etc/systemd/system/recipe.service
 sudo systemctl daemon-reload
-sudo systemctl enable recipe.service
-sudo systemctl start recipe.service
+sudo systemctl enable rubricapp.service
+sudo systemctl start rubricapp.service
 }
