632eff449afa787f542ae7848c46e244310f96b7
Aldo <aldoe@-HEN.localdomain>
2023-05-09 15:08:55 -0500

    Fixed a connection issues by moddifiy both the init file and now testing progress has resumed

--- BackEndFlask/core/__init__.py
+++ BackEndFlask/core/__init__.py
@@ -6,6 +6,7 @@ import os
 import sys
 import platform
 from django.template import Library
+from models.tests import main
 
 files_dir = "."
 register = Library()
@@ -15,12 +16,22 @@ base_directory = os.getcwd()
 home_directory = base_directory
 base_directory = base_directory + "/users"
 
-def create_app():
+
+def create_app(test_config=None):
     if len(sys.argv) <= 1:
         print("Requires argument: path to put files and database (suggestion is `pwd` when already in directory containing app.py)")
         sys.exit(1)
+    if len(sys.argv) == 2 and sys.argv[1]=="test":
+        main()
+        sys.exit(1)
+
     files_dir = sys.argv[1]
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    if (test_config is None):
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        app.config.from_pyfile('config.py', silent=True)
+        app.config.update(test_config)
     from controller import bp
     app.register_blueprint(bp, url_prefix='/api')
     app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
---
+++ BackEndFlask/models/tests.py
@@ -0,0 +1,142 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData, Table, Column, Integer, String, Boolean, Date
+from sqlalchemy import event
+from sqlalchemy import text
+import sqlite3
+import sys
+import random
+from typing import final
+#--------------------------------------------------------------------
+#Checks the integretity of the database
+#Important funtions to see:
+#
+# mappTesting() checks foriegn key constraints as well as triggers
+# dataIntegrityTesting() checks that data properly lands in the database
+# acidTesting() checks to see if database satisfies acid
+#--------------------------------------------------------------------
+
+#-------------------------------------------------------
+#Creates psudorandom data and checks to see if it landed
+#on the database. Temporaryly disables forigen key 
+#constraints.
+#-------------------------------------------------------
+def cruChecks(engine, Users, Course, numTable):
+    print("Disabling forigen key constraints...")
+    conn = engine.connect()
+    query = 'PRAGMA foreign_keys=OFF'
+    conn.execute(text(query))
+    random.seed(1024)
+    numOfTests = 500
+    randomData = [random.randint(1, 300000)]
+    print("Creating random data and inserting to database...")
+
+    for table in range(numTable):
+        for createRandomData in range(numOfTests-1):
+            temp = random.randint(1, 300000)
+            randomData.append(temp)
+            #add 
+    
+    print("Reading from the database...")
+    for read in range(numOfTests):
+        temp = read
+        #if(data returned not in randomdata) rais Exception("Create and/or Read integrity faliure")   
+    print("Create and Read tests passed.")
+
+    print("Updating values in the database...")
+    for u in range(numOfTests):
+        temp = u
+        #updata database with another random values
+        #update list
+    print("Reading back updates along with all other data...")
+    for finalChecks in range(numOfTests):
+        temp = FileNotFoundError
+        #if data recived not in randomdata then raise Exception("Update failure")
+
+    #should be expanded to include delete checks
+
+    print("CRU tests done.")
+    print("Re-enabling constraints")
+    return 1
+
+#-------------------------------------------------------
+#Checks every tables' constraints. Temporaryaly disables
+#foeign key constraints.
+#-------------------------------------------------------
+def constraintChecks(engine, Users, Course, numTables):
+    #insert random data of the correct typing to each table
+    return 1
+
+def dataIntegrityTesting(engine, Users, Course, numberOfTables):
+    if(not cruChecks(engine, Users, Course, numberOfTables) or not constraintChecks(engine, Users, Course, numberOfTables)): 
+        exit("--------Testing interupted---------")
+    print("Data integrity tests passed")
+    return
+
+def mappingTesting():
+    #we want to be able to ask the database for information and 
+    print("Mapping tests passed")
+    return
+
+def acidTesting():
+    print("ACID tests passed")
+    return
+
+def play(Users):
+    engine = sqlalchemy.create_engine('sqlite:///instance/account.db')
+    _SessionFactory = sessionmaker(bind=engine)
+    Base = declarative_base()
+    Base.metadata.create_all(engine)
+    meta = MetaData()
+    u = Users.select()
+    conn = engine.connect()
+    result = conn.execute(u)
+    for row in result:
+        print(row)
+    return _SessionFactory()
+
+def setup():
+    engine = sqlalchemy.create_engine('sqlite:///instance/account.db')
+    _SessionFactory = sessionmaker(bind=engine)
+    Base = declarative_base()
+    Base.metadata.create_all(engine)
+    meta = MetaData()
+    
+    Users = Table(
+        'Users', meta,
+        Column('user_id', Integer),
+        Column('fname', String(30)),
+        Column('lname', String(30)),
+        Column('email', String(255)),
+        Column('password', String(80)),
+        Column('role', String(80)),
+        Column('lms_id', Integer),
+        Column('consent', Boolean),
+        Column('owner_id', Integer),
+    )
+    Course = Table(
+        'Course', meta,
+        Column('course_id', Integer),
+        Column('course_number', Integer),
+        Column('course_name', String(10)),
+        Column('year', Date),
+        Column('term', String(50)),
+        Column('active', Boolean),
+        Column('admin_id', Integer),
+    )
+
+    return engine,Users,Course
+
+#when swapping off of mysql, for databases like postgres, the log will need testing
+def main():
+    numberOfTables = 2
+    engine,Users,Course = setup()
+    print("-----------------------------------------")
+    dataIntegrityTesting(engine, Users, Course, numberOfTables)
+    print("-----------------------------------------")
+    mappingTesting()
+    print("-----------------------------------------")
+    acidTesting()
+    print("--------END OF Tests-------")
