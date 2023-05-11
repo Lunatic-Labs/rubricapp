1a1be26a0076700e70e7f37bb416aeb2cc84af23
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-11 11:59:34 -0500

    Updated setupEnv.py to account for if account.db exists in core or in instance for when the platform is win32!

--- BackEndFlask/controller/__init__.py
+++ BackEndFlask/controller/__init__.py
@@ -4,5 +4,5 @@ from core import ma
 bp = Blueprint('api', __name__)
 cors = CORS(bp, resources={r"/api/*": {"origins": "*"}})
 from controller.Routes import User_routes
-from controller.Routes import course_routes
+from controller.Routes import Course_routes
 from controller.Routes import Rubric_routes
