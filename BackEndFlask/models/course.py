b2a0823e94bfd690640b22b18b61009b4ea5378f
Etl1117 <evantaylorlipe@gmail.com>
2023-05-11 10:08:48 -0500

    update route now working for course_routes.py

--- BackEndFlask/models/course.py
+++ BackEndFlask/models/course.py
@@ -12,7 +12,6 @@ class Course(UserMixin, db.Model):
     course_id = db.Column(db.Integer, primary_key=True)
     course_number = db.Column(db.Integer, nullable=False)
     course_name = db.Column(db.String(10), nullable=False)
-    # change this back to date when finished writing routes!!!
     year = db.Column(db.String(50), nullable=False)
     term = db.Column(db.String(50), nullable=False)
     active = db.Column(db.Boolean, nullable=False)
@@ -51,12 +50,11 @@ def create_course(course_data):
 
 def replace_course(course_data, course_id):
     try:
-        one_course = Course.query.filter_by(course_id).first()
+        one_course = Course.query.filter_by(course_id=course_id).first()
         print(one_course)
+        print(course_data["course_name"])
         if(type(one_course) == type(None)):
             raise InvalidCourseID
-        course_data = Course(course_number=course_data["course_number"], course_name=course_data["course_name"], 
-                             year=course_data["year"], term=course_data["term"], active=course_data["active"], admin_id=course_data["admin_id"])
         one_course.course_number = course_data["course_number"]
         one_course.course_name = course_data["course_name"]
         one_course.year = course_data["year"]
@@ -74,73 +72,6 @@ def replace_course(course_data, course_id):
 """
 All code below has not been updated since user.py was modified on 4/15/2023
 """
-
-# def update_course_course_number(course_id, new_course_number):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.course_number = new_course_number
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
-    
-# def update_course_course_name(course_id, new_course_name):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.course_name = new_course_name
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
-
-# def update_course_year(course_id, new_year):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.year = new_year
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
-
-# def update_course_term(course_id, new_term):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.institution = new_term
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
-    
-
-# def update_course_active(course_id, new_active):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.active = new_active
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
-    
-# def update_course_admin_id(course_id, new_admin_id):
-#     try:
-#         one_course = Course.query.filter_by(id=course_id).first()
-#         one_course.admin_id = new_admin_id
-#         db.session.add(one_course)
-#         db.session.commit()
-#         all_Course = Course.query.all()
-#         return all_Course
-#     except:
-#         return False
     
 """
 Delete is meant for the summer semester!!!
