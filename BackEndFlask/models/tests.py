7fb08527ec2db5ba172a500425ab1f4fb069c1b8
Aldo <aldoe@-HEN.localdomain>
2023-05-12 14:32:14 -0500

    isolated table tests finished and cru+constraint checks are nearly finsihed with only mapping and acid tests left to go

--- BackEndFlask/models/tests.py
+++ BackEndFlask/models/tests.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import Any, final
+from multiprocessing import Pool
 #--------------------------------------------------------------------
 #Checks the integretity of the database
 #Note if verbose informations is wanted
@@ -13,21 +14,8 @@ from typing import Any, final
 # acidTesting() checks to see if database satisfies acid
 #--------------------------------------------------------------------
 
-#sends an object to the database
-def command(query, db, commit=True):
-    conn = db.engine.connect()
-    if(isinstance(query, str)):
-        conn.execute(text(query))
-    else:
-        conn.execute(query)
-    #ensure that the connection is closed to prevent data corruption
-    #note that without commit the Transaction is is rolledback starting from a begin(or implicit begin)
-    if commit:
-        conn.commit()
-    conn.close()
-
 #values should not be modified after creation
-class DataBaseInfo:
+class DataBase:
     def __init__(self, engine, numOfTables):
         self.engine = engine
         self.numOfTables = numOfTables
@@ -47,6 +35,19 @@ class DataBaseInfo:
         tables.remove('Remove')
         return tables
     
+    #sends an object to the database
+    def command(self, query, commit=True):
+        conn = self.engine.connect()
+        if(isinstance(query, str)):
+            conn.execute(text(query))
+        else:
+            conn.execute(query)
+        #ensure that the connection is closed to prevent data corruption
+        #note that without commit the Transaction is is rolledback starting from a begin(or implicit begin)
+        if commit:
+            conn.commit()
+        conn.close()
+    
 #tests the ability to create and remove a table 
 #by creating a sample table called FORTESTINGPURPOSES
 def isolatedTable(db):
@@ -55,11 +56,38 @@ def isolatedTable(db):
         'FORTESTINGPURPOSES', meta,
         Column('foo', Integer, primary_key=True)
     )
-    
+    print("Creating Dummy table...")
+    meta.create_all(db.engine)
     tables = db.getTables()
-    if('FORTESTINGPURPOSES' not in tables):
-        return 0; 
-    return 1 
+    if('FORTESTINGPURPOSES' not in str(tables)):
+        print("Creating dummy table faluire. Re-enabling forigen keys and Exiting")
+        db.command("PRAGMA foreign_keys=ON")
+        return 0
+    
+    print("Inserting data into dummy table...")
+    testCases = [random.randint(1, 100000)]
+    db.command('INSERT INTO FORTESTINGPURPOSES (foo) values (%s)' %testCases[0])
+    random.seed(testCases[0])
+    for x in range(99):
+        testCases.append(random.randint(1, 1000000))
+        db.command('INSERT INTO FORTESTINGPURPOSES (foo) values (%s)' %testCases[x+1])
+    
+    print("Verifying inserted data...")
+    data = FORTESTINGPURPOSES.select()
+    conn = db.engine.connect()
+    result = conn.execute(data)
+    issue = 1
+    for row in result.fetchall():
+        if(row[0] not in testCases):
+            issue = 0
+            print("Not all data inserted found")
+
+    db.command("DROP TABLE FORTESTINGPURPOSES;")
+    tables = db.getTables()
+    if('FORTESTINGPURPOSES' in str(tables)):
+        db.command("PRAGMA foreign_keys=ON")
+        return 0
+    return issue
 
 #-------------------------------------------------------
 #Creates psudorandom data and checks to see if it landed
@@ -68,15 +96,16 @@ def isolatedTable(db):
 #-------------------------------------------------------
 def cruChecks(db):
     print("Disabling forigen key constraints...")
-    command('PRAGMA foreign_keys=OFF', db)
+    db.command('PRAGMA foreign_keys=OFF')
     random.seed(1024)
-    numOfTests = 500
+    numOfTests = 10
     randomData = [random.randint(1, 300000)]
 
     print("Testing creation of isolated table...")
     if(not isolatedTable(db)):
-        print("Failed to create or delete an isolated table")
+        print("Failed isolated table test")
         print("--------------Exiting Testing--------------------------")
+        db.command('PRAGMA foreign_keys=ON')
         exit()
 
     print("Creating random data and inserting to database...")
@@ -107,7 +136,7 @@ def cruChecks(db):
 
     print("CRU tests done.")
     print("Re-enabling constraints")
-    command('PRAGMA foreign_keys=ON', db)
+    db.command('PRAGMA foreign_keys=ON')
     return 1
 
 #-------------------------------------------------------
@@ -115,8 +144,9 @@ def cruChecks(db):
 #foeign key constraints.
 #-------------------------------------------------------
 def constraintChecks(db):
-    #insert random data of the correct typing to each table
-    return 1
+    issue = 1
+
+    return issue
 
 def dataIntegrityTesting(db):
     if (db.numOfTables != len(db.tables)):
@@ -138,7 +168,7 @@ def acidTesting():
 def play(db):
     engine = sqlalchemy.create_engine('sqlite:///instance/account.db')
     conn = engine.connect()
-    
+    conn.execute()
 
     meta = MetaData()
    #meta = MetaData()
@@ -150,7 +180,7 @@ def play(db):
     #return _SessionFactory()
 
 def setup(numOfTables, verbose):
-    db = DataBaseInfo(sqlalchemy.create_engine('sqlite:///instance/account.db', echo=verbose), numOfTables)
+    db = DataBase(sqlalchemy.create_engine('sqlite:///instance/account.db', echo=verbose), numOfTables)
     _SessionFactory = sessionmaker(bind=db.engine)
     Base = declarative_base()
     Base.metadata.create_all(db.engine)
