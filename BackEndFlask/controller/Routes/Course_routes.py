26fbdacfcc94e26552135c59399d0e687823df2d
Etl1117 <evantaylorlipe@gmail.com>
2023-05-15 12:44:28 -0500

    Spelling errors were corrected.

--- BackEndFlask/controller/Routes/Course_routes.py
+++ BackEndFlask/controller/Routes/Course_routes.py
@@ -34,7 +34,7 @@ def get_all_courses():
     all_courses = get_courses()
     if type(all_courses)==type(""):
         print("[Course_routes /course GET] An error occurred fetching all courses!", all_courses)
-        createBadResponse("An error occured fetching all courses!", all_courses)
+        createBadResponse("An error occurred fetching all courses!", all_courses)
         return response
     results = courses_schema.dump(all_courses)
     print("[Course_routes /course GET] Successfully retrieved all courses!")
@@ -45,17 +45,17 @@ def get_all_courses():
 def get_one_course(id):
     one_course = get_course(id)
     if type(one_course)==type(""):
-        print("[Course_routes /course/<id> GET] An error occurred fetching one course!", one_course)
+        print("[Course_routes /course/<int:id> GET] An error occurred fetching one course!", one_course)
         createBadResponse("An error occurred fetching a course!", one_course)
     results = course_schema.dump(one_course)
     totalCourses = 0
     for course in results:
         totalCourses += 1
     if(totalCourses == 0):
-        print(f"[Course_routes /course/<id> GET] Course_id: {id} does not esit!")
-        createBadResponse("An error occured fetching course!", f"Course_id: {id} does not exist")
+        print(f"[Course_routes /course/<int:id> GET] Course_id: {id} does not exist!")
+        createBadResponse("An error occurred fetching course!", f"Course_id: {id} does not exist")
         return response
-    print("[Course_routes /course/<id>/ GET] Successfully fetched one course!")
+    print("[Course_routes /course/<id> GET] Successfully fetched one course!")
     createGoodResponse("Successfully fetched course!", results, 200)
     return response
 
@@ -67,7 +67,7 @@ def add_course():
         createBadResponse("An error occurred creating a new course!", new_course)
         return response
     results = course_schema.jsonify(new_course)
-    print("[Course_routes /add_course POST] Successfully created a new course!")
+    print("[Course_routes /course POST] Successfully created a new course!")
     createGoodResponse("Successfully created a new course!", {}, 201)
     return response
 
@@ -75,11 +75,11 @@ def add_course():
 def update_course(id):
     updated_course = replace_course(request.json, id)
     if type(updated_course)==type(""):
-        print("[Course_routes /update_course/<id>/ PUT] An error occurred replacing course!", updated_course)
+        print("[Course_routes /course/<int:id> PUT] An error occurred replacing course!", updated_course)
         createBadResponse("An error occurred updating the existing course!", updated_course)
         return response
     results = course_schema.dump(updated_course)
-    print("[Course_routes /update_course/<id>/ PUT] Successfully updated course!")
+    print("[Course_routes /course/<int:id> PUT] Successfully updated course!")
     createGoodResponse("Sucessfully updated existing course!", results, 201)
     return response
 
