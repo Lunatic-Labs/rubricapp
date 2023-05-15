11c7293dadf550f1e9f11b8c9cc93ea193f84fc5
Etl1117 <evantaylorlipe@gmail.com>
2023-05-15 12:38:14 -0500

    method for getting one rubric was updated, has not been tested yet

--- BackEndFlask/controller/Routes/Rubric_routes.py
+++ BackEndFlask/controller/Routes/Rubric_routes.py
@@ -37,22 +37,27 @@ def get_all_rubrics():
         createBadResponse("An error occured fetching all courses!", all_rubrics)
         return response
     results = rubrics_schema.dump(all_rubrics)
-    print("[Rubric+routes /rubric GET] Successfully retrieved all rubrics!")
+    print("[Rubric_routes /rubric GET] Successfully retrieved all rubrics!")
     createGoodResponse("Successfully retrieved all rubrics!", results, 200)
     return response
 
-@bp.route('/rubric/<id>/', methods = ['GET'])
+@bp.route('/rubric/<id>', methods = ['GET'])
 def get_one_rubric():
     one_rubric = get_rubric(id)
-    if type(one_rubric)==type("")
+    if type(one_rubric)==type(""):
         print("[Rubric_routes /rubric/<id>/ GET] An error occurred fetching one rubic!", one_rubric)
         createBadResponse("An error occurred fetching a rubric!", one_rubric)
     results = rubric_schema.dump(one_rubric)
     totalRubrics = 0
     for rubric in results:
         totalRubrics += 1
-    if(totalRubrics == 0)
-        print(f"[Rubric_routes ]")
+    if(totalRubrics == 0):
+        print(f"[Rubric_routes /rubric/<id> GET] Rubric_id {id} does not exist!")
+        createBadResponse("An error occurred fetching course!", f"Course_id: {id} does not exist")
+        return response
+    print("[Rubric_routes /rubric/<id> GET] Successfully fetched one rubric!")
+    createGoodResponse("Successfully fetched rubric!", results, 200)
+    return response
 
 
 class RubricSchema(ma.Schema):
