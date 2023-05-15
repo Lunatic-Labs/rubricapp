bf55e6854191d6db1d05eccf712a688fbd929fa6
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-15 14:09:16 -0500

    renamed file for grammer issues

--- BackEndFlask/controller/Routes/Return_existing_AT.py
+++ BackEndFlask/controller/Routes/Return_existing_AT.py
@@ -33,14 +33,31 @@ def createGoodResponse(message, all_at, status):
 def get_all_at():
     all_ats = get_assessment_tasks()
     if type(all_ats) == type(""):
-        print("[Return_exisiting_AT_routes /assessment_tasks GET] An error occurred fetching all assessment tasks ", all_ats)
+        print("[Return_existing_AT_routes /assessment_tasks GET] An error occurred fetching all assessment tasks ", all_ats)
         createBadResponse("An error occured fetching all assessment tasks ", all_ats)
         return response
     result = ATS_schema.dump(all_ats)
-    print("[Return_exisiting_AT_routes/ assessment_tasks GET] Successfully retrived all assessment tasks!")
+    print("[Return_existing_AT_routes/ assessment_tasks GET] Successfully retrived all assessment tasks!")
     createGoodResponse("Successfully retrieved all assessment tasks!", result, 200)
     return response
 
+@bp.route('/assessment_tasks/<int:id>', methods =['GET'])
+def post_details(id):
+    single_at = get_assessment_task(id)
+    if type(single_at)==type(""):
+        print("[Return_existibg_AT_routes /assessment_tasks/<id> GET] An error occurred fetching one single role ", single_at)
+        createBadResponse("An error occurred fetching a single role ", single_at)
+    result = AT_schema.dump(single_at)
+    allAT = 0
+    for assessment_task in result:
+        allAT += 1
+    if(allAT == 0):
+        print(f"[Return_existing_AT_routes /assessment_tasks/<id> GET] at_id: {id} does not exist!")
+        createBadResponse("An error occured fetching assessment task! ", f"at_id: {id} does not exist")
+        return response
+    print("[Return_existing_AT_routes /role/<id>/ GET] Successfully fetched a single assessment task!")
+    createGoodResponse("Successfully fetched single assessment task!", result, 200)
+    return response
 class ATSchema(ma.Schema):
     class Meta:
         fields = ('at_id','at_name', 'course_id', 'rubric_id', 'at_role', 'due_date', 'suggestions')
