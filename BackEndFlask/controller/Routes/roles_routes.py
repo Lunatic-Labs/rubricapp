6f015c72384fa2577e102c4adcfab29e68c0eb73
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-15 11:13:17 -0500

    updating roles_routes.py to be similar to Evan's course routes

--- BackEndFlask/controller/Routes/roles_routes.py
+++ BackEndFlask/controller/Routes/roles_routes.py
@@ -13,18 +13,46 @@ response = {
     "Access-Control-Allow-Headers": "Content-Type"
 }
 
+def createBadResponse(message, errorMessage):
+    JSON = {"roles": []}
+    response['status'] = 500
+    response["success"] = False
+    response["message"] = message + " " + errorMessage
+    response["content"] = JSON
+
+def createGoodResponse(message, all_role_names, status):
+    JSON = {"croles": []}
+    response["status"] = status
+    response["success"] = True
+    response["message"] = message
+    JSON["roles"].append(all_role_names)
+    response["content"] = JSON
+    JSON = {"roles": []}
+
 @bp.route('/role', methods = ['GET'])
 def get_all_roles():
-    all_roles =  roles_schema.dump (get_roles())
-    return jsonify(all_roles)
+    all_roles = get_roles()
+    if type(all_roles) == type(""):
+        print("[roles_routes /role GET] An error occurred fetching all roles ", all_roles)
+        createBadResponse("An error occured fetching all roles", all_roles)
+        return response
+    result = roles_schema.dump(all_roles)
+    print("[roles_routes/ role GET] Successfully retrived all the roles possible!!")
+    createGoodResponse("Successfully retrieved all courses!", result, 200)
+    return response
 
 @bp.route('/single_role/<id>', methods =['GET'])
-def get_one_role(id):
-    return role_schema.jsonify(get_role(id))    
+def post_details(id):
+    single_role = get_role(id)
+    if type(single_role)==type(""):
+        print("[Course_routes /course/<id> GET] An error occurred fetching one single role", single_role)
+        createBadResponse("An error occurred fetching a single role", single_role)
+    results = role_schema.dump(single_role)
+        
 
-class CourseSchema(ma.ma.Schema):
+class RoleSchema(ma.ma.Schema):
     class Meta:
         fields = ('role_id''role_name')
 
-course_schema = CourseSchema()
-courses_schema = CourseSchema(many=True)
+role_schema = RoleSchema()
+roles_schema = RoleSchema(many=True)
