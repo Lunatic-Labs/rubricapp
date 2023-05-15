3874e7ac4829d4b2fe5f93eee625123d206316f5
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-15 12:18:49 -0500

    change course to role in areas that it needed to be changed

--- BackEndFlask/controller/Routes/roles_routes.py
+++ BackEndFlask/controller/Routes/roles_routes.py
@@ -45,14 +45,14 @@ def get_all_roles():
 def post_details(id):
     single_role = get_role(id)
     if type(single_role)==type(""):
-        print("[Roles_routes /course/<id> GET] An error occurred fetching one single role", single_role)
+        print("[Roles_routes /role/<id> GET] An error occurred fetching one single role", single_role)
         createBadResponse("An error occurred fetching a single role", single_role)
     result = role_schema.dump(single_role)
     allRoles = 0
     for role in result:
         allRoles += 1
     if(allRoles == 0):
-        print(f"[Roles_routes /course/<id> GET] role_id: {id} does not esit!")
+        print(f"[Roles_routes /role/<id> GET] role_id: {id} does not esit!")
         createBadResponse("An error occured fetching course!", f"role_id: {id} does not exist")
         return response
     print("[Roles_routes /role/<id>/ GET] Successfully fetched a single role!")
