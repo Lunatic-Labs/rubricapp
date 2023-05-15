700acaa7c956468b2c813a6e0a1384733dd1f2fe
Etl1117 <evantaylorlipe@gmail.com>
2023-05-15 10:43:41 -0500

    removed unneccessary code

--- BackEndFlask/controller/Routes/new_course_routes.py
+++ BackEndFlask/controller/Routes/new_course_routes.py
@@ -4,10 +4,6 @@ from models.course import *
 from controller import bp
 import ma
 
-JSON = {
-    "courses": []
-}
-
 response = {
     "contentType": "application/json",
     "Access-Control-Allow-Origin": "http://127.0.0.1:5000, http://127.0.0.1:3000, *",
