2718df023da0c02d73188b17ecdbca02a28c4c7d
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-05-16 08:43:06 -0500

    Updated User_routes.py to remove consent_is_null being extracted from User data sent in the request! Updated schemas.py to remove consent_is_null attribute from the Users table! Updated user.py to remove consent_is_null from being extracted for when the user is inserted into the database!

--- BackEndFlask/models/schemas.py
+++ BackEndFlask/models/schemas.py
@@ -144,9 +144,7 @@ class Users(UserMixin, db.Model):
     role_id = db.Column(db.Integer, ForeignKey("Role.role_id"),nullable=False)   
     lms_id = db.Column(db.Integer, unique=True, nullable=True)
     # Need to change consent to a string that can be either yes, no, or nothing!
-    consent = db.Column(db.Boolean, nullable=False)
-    # Added new attribute for consent not yet taken!
-    consent_is_null = db.Column(db.Boolean, nullable=False)
+    consent = db.Column(db.Boolean, nullable=True)
     owner_id = db.Column(db.Integer, ForeignKey("Users.user_id"), nullable=False)
 
 class InstructorTaCourse(UserMixin, db.Model):
