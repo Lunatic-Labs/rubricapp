e1e02c72a54c1a2703b76f2f9b7fe9b3309d51a4
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-05-16 14:21:51 -0500

    Updated from master

--- BackEndFlask/models/user.py
+++ BackEndFlask/models/user.py
@@ -60,7 +60,7 @@ def create_user(user):
         new_consent = user[6]
         # new_owner_id = user[7]
         password_hash = generate_password_hash(new_password)
-        new_user = Users(fname=new_fname, lname=new_lname, email=new_email, password=password_hash, role_id=new_role_id, lms_id=new_lms_id, consent=new_consent)
+        new_user = Users(fname=new_fname, lname=new_lname, email=new_email, password=password_hash, role=new_role_id, lms_id=new_lms_id, consent=new_consent)
         db.session.add(new_user)
         db.session.commit()
         return new_user
@@ -74,13 +74,13 @@ def replace_user(user_data, user_id):
         one_user = Users.query.filter_by(user_id=user_id).first()
         if(type(one_user) == type(None)):
             raise InvalidUserID
-        one_user.fname = user[0]
-        one_user.lname = user[1]
-        one_user.email = user[2]
-        one_user.password = user[3]
-        one_user.role_id = user[4]
-        one_user.lms_id = user[5]
-        one_user.consent = user[6]
+        one_user.fname = user_data[0]
+        one_user.lname = user_data[1]
+        one_user.email = user_data[2]
+        one_user.password = user_data[3]
+        one_user.role_id = user_data[4]
+        one_user.lms_id = user_data[5]
+        one_user.consent = user_data[6]
         # one_user.owner_id = user[7]
         db.session.commit()
         return one_user
