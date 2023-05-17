d4b8feb39557d5da43478d585a0f1b8f867ff955
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-17 10:30:09 -0500

    adding assessment task routes in _init_.py

--- BackEndFlask/controller/__init__.py
+++ BackEndFlask/controller/__init__.py
@@ -7,3 +7,4 @@ from controller.Routes import User_routes
 from controller.Routes import Course_routes
 from controller.Routes import Rubric_routes
 from controller.Routes import Roles_routes
+from controller.Routes import Assessment_task_routes
