c6bd81940f63c37eaf07f6f5e842a95edbe7d5be
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-17 08:43:33 -0500

    updating roles routes here to just be a get

--- BackEndFlask/controller/Routes/Roles_routes.py
+++ BackEndFlask/controller/Routes/Roles_routes.py
@@ -9,7 +9,7 @@ ma = Marshmallow()
 response = {
     "contentType": "application/json",
     "Access-Control-Allow-Origin": "http://127.0.0.1:5000, http://127.0.0.1:3000, *",
-    "Access-Control-Allow-Methods": ['GET', 'POST'],
+    "Access-Control-Allow-Methods": ['GET'],
     "Access-Control-Allow-Headers": "Content-Type"
 }
 
