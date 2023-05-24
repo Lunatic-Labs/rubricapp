b1a4202af36983a0b2e1977eab8cd2af6f62f1a1
Tanish Neema <33641285+Mr615-TN@users.noreply.github.com>
2023-05-24 13:44:41 -0500

    updating assessment task schema in schema.py and assessment_task_routes.py to reflect Brian's changes on whiteboard

--- BackEndFlask/models/schemas.py
+++ BackEndFlask/models/schemas.py
@@ -38,7 +38,6 @@ class AssessmentTask(UserMixin, db.Model):
     course_id = db.Column(db.Integer, ForeignKey("Course.course_id")) # Might have to think about
     rubric_id = db.Column(db.Integer, ForeignKey("Rubric.rubric_id")) # how to handle updates and deletes
     role_id = db.Column(db.Integer, ForeignKey("Role.role_id"))
-    user_id = db.Column(db.Integer, ForeignKey("Users.user_id"))
     uc_id = db.Column(db.Integer, ForeignKey("UserCourse.uc_id")) # Might have to think about
     due_date = db.Column(DateTime(timezone=True), server_default=func.now()) # may need to be updated later
     suggestions = db.Column(db.Boolean, nullable=False)
