94d699941b9fa939b82a1b474cd109598784404e
Aldo <school@vtuber-enjoyer.mozmail.com>
2023-05-26 15:33:58 -0500

    init file changes work on windows. testing for mac need and I need to see whats casuing an issue with production

--- BackEndFlask/core/__init__.py
+++ BackEndFlask/core/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from models.tests import testing
-from dotenv import dotenv_values, load_dotenv
+from dotenv import load_dotenv
 import os
 import sys
 
@@ -12,17 +12,17 @@ if len(sys.argv) == 2 and sys.argv[1]=="test":
         sys.exit(1)
 register = Library()
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['JSON_SORT_KEYS'] = False
+app.config.from_prefixed_env()
 accountDBPath = os.getcwd() + os.path.join(os.path.sep, "core") + os.path.join(os.path.sep, "account.db")
-if os.path.exists(accountDBPath):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./account.db'
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/account.db'
+load_dotenv('./env/.env.development')
+if(len(sys.argv) == 2 and sys.argv[1] == 'p'):
+    load_dotenv('./env/.env.production', override=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MAC') if os.path.exists(accountDBPath) else os.getenv("WIN_LIN")
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+app.config['SECRET_KEY'] = os.getenv('DONT_LOOK')
 db = SQLAlchemy()
 db.init_app(app)
 ma = Marshmallow()
 ma.init_app(app)
-from controller import bp
-app.register_blueprint(bp, url_prefix='/api')
+from controller import bp #why is there an import here rather than above?
+app.register_blueprint(bp, url_prefix='/api') #I need to be able to change this bring it up with B
