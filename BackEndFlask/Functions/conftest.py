120627d780a0a472e4a3227f1531c3b29273c3bb
Luis Godinez <godinez.luis2020@gmail.com>
2023-05-30 10:02:34 -0500

    updated testing setup

--- BackEndFlask/Functions/conftest.py
+++ BackEndFlask/Functions/conftest.py
@@ -11,20 +11,6 @@ from sqlalchemy.orm.session import close_all_sessions
 @pytest.fixture
 def flask_app_mock():
     """Flask application set up."""
-    mock_app =  app
-    mock_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account_test.db'
-    with mock_app.app_context():
-        db.create_all()
-        load_SuperAdminUser()
-        load_existing_roles()
-
-    yield mock_app
-    
-    with mock_app.app_context():
-        db.session.close()
-        engine_container = db.engine
-        engine_container.dispose()
-    close_all_sessions()
     accountDBPath = os.path.join(os.sep, "account.db")
     coreFile = os.getcwd() + os.path.join(os.sep, "core")
     instanceFile = os.getcwd() + os.path.join(os.sep, "instance")
@@ -46,4 +32,19 @@ def flask_app_mock():
                 os.system("del " + "\"" + accountDBPath + "\"")
             except:
                 pass
+
+    mock_app =  app
+    mock_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account_test.db'
+    with mock_app.app_context():
+        db.create_all()
+        load_SuperAdminUser()
+        load_existing_roles()
+
+    yield mock_app
+    
+    with mock_app.app_context():
+        db.session.close()
+        engine_container = db.engine
+        engine_container.dispose()
+    close_all_sessions()
 
