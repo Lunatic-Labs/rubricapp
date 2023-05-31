fe963ed8de7f8d8af219777afeb9808114784078
Tanish Neema <tneema501@gmail.com>
2023-05-30 20:38:14 -0500

    fixing _init_py file to match skil-58

--- BackEndFlask/core/__init__.py
+++ BackEndFlask/core/__init__.py
@@ -1,3 +1,4 @@
+from django.template import Library
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -8,6 +9,7 @@ import sys
 if len(sys.argv) == 2 and sys.argv[1]=="test":
         main()
         sys.exit(1)
+register = Library()
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
