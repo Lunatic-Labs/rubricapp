51a2140766780da9a9a83d0b98c799a11f576e5d
Aldo <school@vtuber-enjoyer.mozmail.com>
2023-06-06 11:58:17 -0500

    log in is ready waiting for pull request #5 to go through to ensure changes work

--- BackEndFlask/controller/Routes/Login_route.py
+++ BackEndFlask/controller/Routes/Login_route.py
@@ -6,24 +6,27 @@ from flask_marshmallow import Marshmallow
 from controller.Route_response import *
 from controller.token.encryption import encodeAuthToken 
 from controller.Routes.User_routes import UserSchema
+from werkzeug.security import check_password_hash
 
-@bp.route('/Login/<string:givenEmail>', methods=['GET'])
-def loginUser(givenEmail):
-    #superadminuser93@skillbuilder.edu
-    givenPassword = "pbkdf2:sha256:600000$2jtJ6wK8IDIySc6o$17754cde2eb25d1297bfc59fc0d861764a6445c154e070fa800515551e847b04"
-    print(givenEmail)
-    user = get_user_by_email(givenEmail)
-    print(user)
+#Before going live switch to https over POST
+@bp.route('/Login', methods=['POST'])
+def loginUser():
+    if request.method == 'POST':
+        email = request.args.get('email')
+        password = request.args.get('password')
+    print(type(email), email)
+    user = get_user_by_email(email)
+    print(type(user['user_id']))
     if type(user) != type(''):
         user = userSchema.dump(user)
-        if(givenPassword == get_user_password(user['user_id'])):
-            print("--------------------------------------------")
+        if(check_password_hash(get_user_password(user['user_id']), password)):
+        #if(password == get_user_password(user['user_id'])):
             authToken = encodeAuthToken(user['user_id'])
-            print(f"[Login_route /user/<str:email> GET] Successfully varfied user: {givenEmail}!")
-            createGoodResponse(f"Successfully verified log in information: {givenEmail}!", userSchema.dump(user), 200, "users", authToken)
+            print(f"[Login_route /user/<str:email> GET] Successfully varfied user: {email}!")
+            createGoodResponse(f"Successfully verified log in information: {email}!", email, 200, "user", authToken)
             return response
-    print(f"[Login_route /user/<str:email> GET] An error occured fetching email or password: {givenEmail} {givenPassword}")
-    createBadResponse(f"Unable ot verify log in information", "", "log in", 401)#why do we need the err msg
+    print(f"[Login_route /Login POST] An error occured fetching email or password: {email} {[password]}")
+    createBadResponse(f"Unable ot verify log in information", "", "log in", 401)
     return response
-     
+
 userSchema = UserSchema()
