30c79415142c65ee68235703fc83e7a81be6f6c5
Aldo <aldoe@-HEN.localdomain>
2023-06-13 18:37:47 -0500

    logout route ready to be used

--- BackEndFlask/controller/Routes/Logout_route.py
+++ BackEndFlask/controller/Routes/Logout_route.py
@@ -5,19 +5,14 @@ from controller import bp
 from flask_marshmallow import Marshmallow
 from controller.Route_response import *
 from controller.Routes.User_routes import UserSchema
-from controller.security.utility import revokeTokens
-from flask_jwt_extended import get_jwt_identity
+from controller.security.utility import revokeTokens, tokenExpired
+from flask_jwt_extended import decode_token
+from jwt import ExpiredSignatureError
 
 @bp.route('/Logout', methods=['POST'])
 def logout():
-    id, token = request.args.get('user_id'), request.args.get('auth_token')
-    if token:
-        id, token = request.args.get('user_id'), request.args.get('auth_token')
-        #payload = decodeAuthToken(token)
-        if id.isnumeric():
-            id = int(id)
-        #if payload != -1 and payload == id:
-        #    blackListToken(token)
-        revokeTokens()
+    id, jwt, refresh = request.args.get('user_id'), request.args.get('access_token'), request.args.get('refresh_tokenn')
+    if jwt and not tokenExpired(jwt): blackListToken(jwt)
+    if refresh and not tokenExpired(refresh): blackListToken(refresh)
+    revokeTokens()
     createGoodResponse("Successfully logged out", id, 200, 'user')
-    return response
--- BackEndFlask/controller/security/utility.py
+++ BackEndFlask/controller/security/utility.py
@@ -6,7 +6,8 @@ from flask_marshmallow import Marshmallow
 from controller.Route_response import *
 from controller.Routes.User_routes import UserSchema
 from core import app
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity
+from jwt import ExpiredSignatureError
+from flask_jwt_extended import create_access_token, create_refresh_token, decode_token
 
 #adding a decorator to act as middleware to block bad tokens
 def badTokenCheck(route):
@@ -20,11 +21,13 @@ def badTokenCheck(route):
                 return response
         return blacklistCheck()
 
+#authenticates a jwt token
 def authenticateViajwt(route):
     def check(route):
         with app.app_context():
             token = request.args.get('access_token')
 
+#creates both a jwt and refresh token
 def createTokens(userID, roleID):
     with app.app_context():
         jwt = create_access_token([userID, roleID])
@@ -33,10 +36,21 @@ def createTokens(userID, roleID):
             refresh = create_refresh_token([userID, roleID])
     return jwt, refresh
 
+#function used to create a new jwt based on refresh token
 def refreshJwt():
     return 3
 
+#takes away jwt and refresh tokens from response
 def revokeTokens():
     with app.app_context():
         if response.get('access_token') : response.pop('access_token')
         if response.get('refresh_token'): response.pop('refresh_token')
+
+#returns true if token is expired
+def tokenExpired(thing):
+    with app.app_context():
+        try:
+            decode_token(thing)
+        except ExpiredSignatureError:
+            return True
+    return False
