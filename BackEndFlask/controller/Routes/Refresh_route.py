3418ab93d5d3bbcfd8da7d569bfb4ab9e2da6a74
Aldo <aldoe@-HEN.localdomain>
2023-06-14 09:49:25 -0500

    refreshing system up and working

--- BackEndFlask/controller/Routes/Refresh_route.py
+++ BackEndFlask/controller/Routes/Refresh_route.py
@@ -6,16 +6,17 @@ from controller import bp
 from flask_marshmallow import Marshmallow
 from controller.Route_response import *
 from controller.Routes.User_routes import UserSchema
-from controller.security.utility import revokeTokens, tokenExpired, tokenUserId
 from flask_jwt_extended import jwt_required, create_access_token
 
 @bp.route('/Refresh', methods=['GET'])
 @jwt_required(refresh=True)
 def refreshToken():
     id = request.args.get('user_id')
-    user = get_user(int(id))
-    user = userSchema.dump(user)
-    createGoodResponse("New access token generated", id, 200, 'Token refreshing', create_access_token([id, user['role_id']]))
+    if id:
+        user = get_user(int(id))
+        user = userSchema.dump(user)
+        createGoodResponse("New access token generated", id, 200, 'Token refreshing', create_access_token([id, user['role_id']]))
+    else: createBadResponse ("Bad request:", "user_id must be provided", None, 400)
     return response
 
 userSchema = UserSchema()
