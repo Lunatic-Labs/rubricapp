6d9cae6558148b6f07200aab5ce68f3f6178e712
Aldo <aldoe@-HEN.localdomain>
2023-06-16 13:01:17 -0500

    fixes

--- BackEndFlask/controller/security/blacklist.py
+++ BackEndFlask/controller/security/blacklist.py
@@ -7,17 +7,20 @@ import math
 import time
 from jwt.exceptions import ExpiredSignatureError
 
+#starts a Redis server as a subprocess using the subprocess.Popen function
+#redirects the standard output and standard error streams to subprocess.DEVNULL to get rid of them
 
 #starts a Redis server as a subprocess using the subprocess.Popen function
 #redirects the standard output and standard error streams to subprocess.DEVNULL to get rid of them
-def startRedis() -> None:
+def startRedis()->None:
     subprocess.Popen(
         'redis-server',
         stdout=subprocess.DEVNULL, 
         stderr=subprocess.DEVNULL
     )
 #checks if a given token exists in a Redis database and returns True if it is blacklisted
-def isTokenBlacklisted(token: str) -> bool:
+#checks if a given token exists in a Redis database and returns True if it is blacklisted
+def isTokenBlacklisted(token:str)->bool:
     try:
         r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
         found = r.get(token)
