a78917042a01016c862cf533807017e5b731b566
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2023-10-09 23:53:45 -0500

    Saving comments for what files to reference

--- FrontEndReact/src/View/Student/View/StudentSelectTeamMembers.js
+++ FrontEndReact/src/View/Student/View/StudentSelectTeamMembers.js
@@ -1,17 +1,23 @@
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import SelectTeamMembers from './SelectTeamMembers';
+import { API_URL } from '../../../App';
 
+// TODO: Fetch all the students and save them into a team
 class StudentSelectTeamMembers extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoaded: null,
-            students: null
+            students: null,
+			// NOTE: Might not need teamss
+			teams: [],
+			users: []
         };
     }
+	// NOTE: Might need to check if the student is already in a team
     componentDidMount() {
-        // 
+        fetch(API_URL + `/team`) 
     }
     render() {
         return(
