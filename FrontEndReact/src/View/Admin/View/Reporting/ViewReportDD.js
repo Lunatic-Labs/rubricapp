b3b6d6d51507baceea669fb8056d89ce8bb66d4e
ksoria0205 <karenn.s@icloud.com>
2023-10-10 13:25:01 -0500

    added ViewReportDD into navbar.js

--- FrontEndReact/src/View/Admin/View/Reporting/ViewReportDD.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewReportDD.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-class ReportDD extends Component{
-    const options = [
-    'one', 'two', 'three'
-    ];
-    const defaultOption = options[0];
-    <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
+export default class ViewReportDD extends Component{
+    render(){
+        const options = [
+        'one', 'two', 'three'
+        ];
+        const defaultOption = options[0];
+        return (
+        <>
+        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
+        </>
+        )
+    }
 }
--- FrontEndReact/src/View/Navbar/Navbar.js
+++ FrontEndReact/src/View/Navbar/Navbar.js
@@ -1090,6 +1090,7 @@ export default class Navbar extends Component {
                        <>
                          <div className='container'>
                                 <h1 className='mt-5'>Reporting</h1>
+                                <ViewReportDD/>
                                 <ViewReportStudent/>
                                 <ViewReportEval/>
                            </div>
