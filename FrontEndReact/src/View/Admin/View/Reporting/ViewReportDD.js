78e16d4a7a3bcebc076d3e21ba688b1bd0b95be0
ksoria0205 <karenn.s@icloud.com>
2023-10-12 14:41:48 -0500

    added material ui select for dropdown on reporting

--- FrontEndReact/src/View/Admin/View/Reporting/ViewReportDD.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewReportDD.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-export default class ViewReportDD extends Component{
-    render(){
-        const options = [
-        'one', 'two', 'three'
-        ];
-        const defaultOption = options[0];
-        return (
-        <>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
-        </>
-        )
-    }
+export default function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Assessment Task: Rubric"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }
