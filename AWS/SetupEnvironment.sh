cebcefb04a05e66004a2c6520b45f5788c21078b
julian v <77362732+jsvamdbork@users.noreply.github.com>
2023-10-17 13:26:48 -0500

    Update SetupEnvironment.sh
    -get removed

--- AWS/SetupEnvironment.sh
+++ AWS/SetupEnvironment.sh
@@ -1,9 +1,9 @@
 setupEnvrionment () {
 printf '=============Setup the environment=============== \n'
 cd ..
-sudo apt-get update
+sudo apt update
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get update
+sudo apt update
 printf '====== Set python3.6 as the default for python3 ======= \n'
 sudo apt-get install -y python3.6 python3-pip nginx python3.6-gdbm
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
