73cc680c363cb7f5f31a6e76eb6331f2699c93dd
julian v <77362732+jsvamdbork@users.noreply.github.com>
2023-10-25 11:35:22 -0500

    Update SetupEnvironment.sh

--- AWS/SetupEnvironment.sh
+++ AWS/SetupEnvironment.sh
@@ -1,15 +1,12 @@
 setupEnvrionment () {
 printf '=============Setup the environment=============== \n'
-cd ..
+
 sudo apt update
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt update
+sudo apt upgrade -y
+
 printf '====== Set python3.6 as the default for python3 ======= \n'
-sudo apt-get install -y python3.6 python3-pip nginx python3.6-gdbm
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 10
-sudo update-alternatives --config -y python3
-pip3 install virtualenv
-#mkdir POGIL_DEV
-#cd POGIL_DEV
+sudo apt install -y python3 
+sudo apt install -y python3-pip
+sudo apt install -y nginx
+sudo apt install -y python3-gdbm
 }
