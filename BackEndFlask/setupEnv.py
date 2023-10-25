257b1ec674f73e8e3928dad0b6c4068b21282b53
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-10-24 23:17:59 -0500

    Saving current progress for setupEnv.py!

--- BackEndFlask/setupEnv.py
+++ BackEndFlask/setupEnv.py
@@ -6,44 +6,48 @@ import atexit
 def main():
     sleepTime = 0.5
     print("[Server] starting...")
-    time.sleep(sleepTime/2)
-    print('[Server] attempting to run pip3 install requirements...')
+    time.sleep(sleepTime)
+
+    if sys.platform.startswith('win32'):
+        # Redis is not supported on Windows!
+        # Therefore, from here on out, Windows cannot be used for development.
+        # Only WSL can be used to allow Windows users to install redis-server!
+        print("[Server] Windows is no longer supported for development...")
+        os.abort()
+
     try:
+        print('[Server] attempting to run pip3 install requirements...')
+        time.sleep(sleepTime)
         if(os.system("pip3 install -r requirements.txt") != 0):
             raise Exception
+        print('[Server] requirements successfully installed.')
         time.sleep(sleepTime)
-    except Exception:
-        print("[Server] attempting to run pip3 install requirements failed...")
-        time.sleep(sleepTime)
-        print('[Server] attempting to run pip install requirements...')
+    except:
+        print('[Server] attempting to run pip3 install requirements failed.')
+        os.abort()
+
+    if sys.platform.startswith('darwin'):
         try:
-            if(os.system("pip install -r requirements.txt") != 0):
-                raise Exception
+            print("\n[Server] attempting to install Redis-Server for MacOS...\n")
             time.sleep(sleepTime)
+            if(os.system("chmod 755 setupHomebrew.sh") != 0):
+                raise Exception
+            if(os.system("./setupHomebrew.sh") != 0):
+                raise Exception
+            if(os.system("brew --version") != 0):
+                raise Exception
+            if(os.system("brew install redis") != 0):
+                raise Exception
+            if(os.system("brew services start redis") != 0):
+                raise Exception
+            if(os.system("brew services info redis") != 0):
+                raise Exception
         except:
-            print("[Server] attempting to run pip install requirements failed...")
-            print("[Server] exiting...")
+            print("[Server] attempting to install Redis-Server for MacOS failed...")
             os.abort()
-     try:
-        print("\n[Server] attempting to install Homebrew using setupHomebrew.sh...\n")
-        if(os.system("chmod 755 setupHomebrew.sh") != 0):
-            raise Exception
-        if(os.system("./setupHomebrew.sh") != 0):
-            raise Exception
-        if(os.system("brew --version") != 0):
-            raise Exception
-        if(os.system("brew install redis") != 0):
-            raise Exception
-        if(os.system("brew services start redis") != 0):
-            raise Exception
-        if(os.system("brew services info redis") != 0):
-            raise Exception
-        time.sleep(sleepTime)
-    except Exception:
-        # Here is where the code will go to automatically install redis-server
-        # for Linux
+    elif sys.platform.startswith('linux'):
         try:
-            print("[Server] attempting to run Homebrew install requirements failed, attempting Linux Install...")
+            print("[Server] attempting to install Redis-Server for Linux...")
             time.sleep(sleepTime)
             if(os.system('sudo apt install lsb-release curl gpg') != 0):
                 raise Exception
@@ -58,18 +62,16 @@ def main():
             if(os.system('redis-server &') != 0):
                 raise Exception
         except:
-            # Here is where the code would go to automatically install redis-server.
-            # However, Redis is not supported on Windows!
-            # Therefore, from here on out, Windows cannot be used for development.
-            # Only WSL can be used to allow Windows users to install redis-server!
             print("[Server] attempting to install Redis-Server for Linux failed...")
-            time.sleep(sleepTime)
+            os.abort()
+
     if (len(sys.argv) == 2 and sys.argv[1]=="resetdb") or (len(sys.argv) == 3 and sys.argv[1]=="resetdb" and sys.argv[2]=="demo"):
         accountDBPath = os.path.join(os.sep, "account.db")
         coreFile = os.getcwd() + os.path.join(os.sep, "core")
         instanceFile = os.getcwd() + os.path.join(os.sep, "instance")
         print("[Server] locating instance folder...")
         time.sleep(sleepTime)
+
         if os.path.exists(instanceFile):
             print("[Server] instance folder found")
             time.sleep(sleepTime)
@@ -81,6 +83,7 @@ def main():
             time.sleep(sleepTime)
             print("[Server] core folder found")
             accountDBPath = coreFile + accountDBPath
+
         if os.path.exists(accountDBPath):
             print("[Server] account.db file exists...")
             time.sleep(sleepTime)
@@ -94,15 +97,6 @@ def main():
             except:
                 print(f"[Server] attempting rm account.db failed...")
                 time.sleep(sleepTime)
-                try:
-                    print(f"[Server] attempting to del account.db...")
-                    time.sleep(sleepTime)
-                    os.system("del " + "\"" + accountDBPath + "\"")
-                    print(f"[Server] successfully del account.db")
-                    time.sleep(sleepTime)
-                except:
-                    print("[Server] attempting del account.db failed...")
-                    time.sleep(sleepTime)
         else:
             print("[Server] account.db file does not exist therefore does not need to be removed")
             time.sleep(sleepTime)
@@ -121,16 +115,7 @@ def main():
             time.sleep(sleepTime)
     except Exception:
         print("[Server] attempting to run python3 dbcreate.py failed...")
-        time.sleep(sleepTime)
-        try:
-            print("[Server] attempting to run python dbcreate.py...")
-            time.sleep(sleepTime)
-            os.system("python dbcreate.py")
-            time.sleep(sleepTime)
-        except:
-            print("[Server] attempting to run python dbcreate.py failed...")
-            print("[Server] exiting...")
-            os.abort()
+        os.abort()
     try:
         print("\n[Server] attempting to run python3 run.py...\n")
         time.sleep(sleepTime)
@@ -138,17 +123,10 @@ def main():
             raise Exception
     except:
         print("[Server] attempting to run python3 run.py failed...")
-        time.sleep(sleepTime)
-        try:
-            print("\n[Server] attempting to run python run.py...\n")
-            time.sleep(sleepTime)
-            os.system("python run.py")
-        except:
-            print("[Server] attempting to run python run.py failed...")
-            print("[Server] exiting...")
-            os.abort()
+        os.abort()
             
     def exit_handler():
+        print("[Server] exiting...")
         os.system("brew services stop redis") != 0
         os.system("redis-cli shutdown") != 0
     atexit.register(exit_handler)
