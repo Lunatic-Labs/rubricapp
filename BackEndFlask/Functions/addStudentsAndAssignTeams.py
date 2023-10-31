f70b9393b6f90c500deaadf04fe1966c00833293
malloc-nbytes <zdhdev@yahoo.com>
2023-10-31 12:15:58 -0500

    Updated helper.py with new function signature for helper_cleanup(). Added cleanup for file closes and returns in addStudentsAndAssignTeams.py -> student_and_team_to_db

--- BackEndFlask/Functions/addStudentsAndAssignTeams.py
+++ BackEndFlask/Functions/addStudentsAndAssignTeams.py
@@ -1,8 +1,7 @@
 from typing import List
 
 from Functions.test_files.population_functions import *
-# from Functions.helper import *
-from Functions.helper import helper_ok, helper_verify_email_syntax, helper_cleanup, helper_create_user
+from Functions.helper import *
 from Functions.customExceptions import *
 from models.user import *
 from models.user_course import *
@@ -10,8 +9,6 @@ from sqlalchemy import *
 from datetime import date
 import itertools
 import csv
-
-# TODO: use helper_cleanup()
 
 
 def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
@@ -24,11 +21,9 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
         roster_file = xlsx_to_csv(roster_file)
 
     try:
-        student_and_team_csv = open(
-            roster_file, mode='r', encoding='utf-8-sig')
+        student_and_team_csv = open(roster_file, mode='r', encoding='utf-8-sig')
     except FileNotFoundError:
-        delete_xlsx(roster_file, is_xlsx)
-        return FileNotFound.error
+        return helper_cleanup(FileNotFound.error)
 
     csv_reader = csv.reader(student_and_team_csv)
     roster = []
@@ -43,14 +38,14 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
 
     ta_info = roster[0]
     if len(ta_info) != 2:
-        pass  # not enough information or too much information
+        return helper_cleanup(NotEnoughColumns.error, student_and_team_csv)
 
     team_name = ta_info[0]
     ta_email = ta_info[1]
 
     team = get_team_by_team_name_and_course_id(team_name, course_id)
     if not helper_ok(team, roster_file, is_xlsx):
-        return team
+        return helper_cleanup(team, student_and_team_csv)
 
     if team is None:
         team = create_team({
@@ -59,37 +54,36 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
             "date_created": str(date.today().strftime("%m/%d/%Y"))
         })
         if not helper_ok(team, roster_file, is_xlsx):
-            delete_xlsx(roster_file, is_xlsx)
-            return team
+            return helper_cleanup(team, student_and_team_csv)
 
     team_course = create_team_course({
         "team_id": team.team_id,
         "course_id": course_id
     })
     if not helper_ok(team_course, roster_file, is_xlsx):
-        return team_course
+        return helper_cleanup(team_course, student_and_team_csv)
 
     # Begin handling TA's.
     course_uses_tas = get_course_use_tas(course_id)
 
     if not helper_ok(course_uses_tas, roster_file, is_xlsx):
-        return course_uses_tas
+        return helper_cleanup(course_uses_tas, student_and_team_csv)
 
     ta_course = None
     if course_uses_tas:
         missing_ta = False
 
         if not helper_verify_email_syntax(ta_email):
-            return SuspectedMisformatting.error
+            return helper_cleanup(SuspectedMisformatting.error, student_and_team_csv)
 
         user = get_user_by_email(ta_email)
         if not helper_ok(user, roster_file, is_xlsx):
-            return user
+            return helper_cleanup(user, student_and_team_csv)
 
         # If the user is not already in the DB.
         if user is None:
             delete_xlsx(roster_file, is_xlsx)
-            return UserDoesNotExist.error
+            return helper_cleanup(UserDoesNotExist.error, student_and_team_csv)
 
         if user.role_id == 5:
             missing_ta = True
@@ -97,32 +91,32 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
         user_id = get_user_user_id_by_email(ta_email)
 
         if not helper_ok(user_id, roster_file, is_xlsx):
-            return user_id
+            return helper_cleanup(user_id, student_and_team_csv)
 
         ta_course = get_user_course_by_user_id_and_course_id(
             user_id, course_id)
         if not helper_ok(ta_course, roster_file, is_xlsx):
-            return ta_course
+            return helper_cleanup(ta_course, student_and_team_csv)
 
         if ta_course is None:
             delete_xlsx(roster_file, is_xlsx)
-            return TANotYetAddedToCourse.error
+            return helper_cleanup(TANotYetAddedToCourse.error, student_and_team_csv)
     else:
         user = get_user(owner_id)
         if not helper_ok(user, roster_file, is_xlsx):
-            return user
+            return helper_cleanup(user, student_and_team_csv)
 
         if user is None:
             delete_xlsx(roster_file, is_xlsx)
-            return UserDoesNotExist.error
+            return helper_cleanup(UserDoesNotExist.error, student_and_team_csv)
 
         course = get_course(course_id)
         if not helper_ok(course, roster_file, is_xlsx):
-            return course
+            return helper_cleanup(course, student_and_team_csv)
 
         courses = get_courses_by_admin_id(owner_id)
         if not helper_ok(courses, roster_file, is_xlsx):
-            return courses
+            return helper_cleanup(courses, student_and_team_csv)
 
         course_found = False
         for admin_course in courses:
@@ -130,17 +124,19 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
                 course_found = True
         if not course_found:
             delete_xlsx(roster_file, is_xlsx)
-            return OwnerIDDidNotCreateTheCourse.error
+            return helper_cleanup(OwnerIDDidNotCreateTheCourse.error, student_and_team_csv)
     # End handling TA's.
 
     # Begin handling students.
     for student_info in roster[1:]:
         if len(student_info) > 3:
             # too many columns
-            assert False and "too many columns"
+            delete_xlsx(roster_file, is_xlsx)
+            return helper_cleanup(TooManyColumns.error, student_and_team_csv)
         elif len(student_info) < 2:
             # not enough columns
-            assert False and "not enough columns"
+            delete_xlsx(roster_file, is_xlsx)
+            return helper_cleanup(NotEnoughColumns.error, student_and_team_csv)
 
         name = student_info[0].strip()  # FN,LN
         last_name = name.replace(",", "").split()[0].strip()
@@ -152,24 +148,24 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
             lms_id = student_info[2]
 
         if not helper_verify_email_syntax(email):
-            return SuspectedMisformatting.error
+            return helper_cleanup(SuspectedMisformatting.error, student_and_team_csv)
 
         user = get_user_by_email(email)
         if not helper_ok(user, roster_file, is_xlsx):
-            return user
+            return helper_cleanup(user, student_and_team_csv)
 
         user_id = None
         if user is None:
             user = helper_create_user(
                 first_name, last_name, email, 4, lms_id, owner_id)
             if not helper_ok(user, roster_file, is_xlsx):
-                return user
+                return helper_cleanup(user, student_and_team_csv)
         user_id = user.user_id
 
         user_course = get_user_course_by_user_id_and_course_id(
             user_id, course_id)
         if not helper_ok(user_course, roster_file, is_xlsx):
-            return user_course
+            return helper_cleanup(user_course, student_and_team_csv)
 
         if user_course is None:
             user_course = create_user_course({
@@ -177,7 +173,7 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
                 "course_id": course_id
             })
             if not helper_ok(user_course, roster_file, is_xlsx):
-                return user_course
+                return helper_cleanup(user_course, student_and_team_csv)
 
         # add student to team
         team_user = create_team_user({
@@ -185,8 +181,5 @@ def student_and_team_to_db(roster_file: str, owner_id: int, course_id: int):
             "user_id": user_id
         })
         if not helper_ok(team_user, roster_file, is_xlsx):
-            return team_user
+            return helper_cleanup(team_user, student_and_team_csv)
 
-
-filepath = "./sample_files/addStudentsAndAssignTeams.csv"
-student_and_team_to_db(filepath, 2, 1)
--- BackEndFlask/Functions/helper.py
+++ BackEndFlask/Functions/helper.py
@@ -24,16 +24,13 @@ def helper_verify_email_syntax(email):
     return True
 
 
-def helper_cleanup(xlsx_file, is_xlsx, return_val, csv_file=None):
+def helper_cleanup(return_val, csv_file=None):
     """
     This function is to be called when an error is encountered.
-    @param xlsx_file: TODO
-    @param is_xlsx: whether or not the file is .xlsx
     @param return_val: the value to be returned
     @param csv_file: csv file we are working with
     @return: return_val
     """
-    delete_xlsx(xlsx_file, is_xlsx)
     if csv_file is not None:
         csv_file.close()
     return return_val
