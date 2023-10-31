ed1e6ce3ce4ac21e73d6c800768965ad692653a3
Aldo Velarde <aldo.velarde2542001@gmail.com>
2023-10-31 15:53:35 -0500

    Finished Button Component!

--- FrontEndReact/src/View/Components/BackButton.js
+++ FrontEndReact/src/View/Components/BackButton.js
@@ -4,18 +4,19 @@ import { grey } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 
 const ColorButton = styled(Button)(({ theme }) => ({
+    borderRadius:"100px",
+    boxShadow: "none",
     color: theme.palette.getContrastText(grey[300]),
     backgroundColor: grey[300],
     '&:hover': {
       backgroundColor: grey[400],
-    },
-    
+      boxShadow: "none"
+    },   
   }));
 
 export default function BackButton (){
-   
     return (
-        <ColorButton sx={{borderRadius:"100px"}} variant="contained" startIcon={<ArrowBackIcon />}>
+        <ColorButton variant="contained" startIcon={<ArrowBackIcon/>}>
             Back
         </ColorButton>
     );
