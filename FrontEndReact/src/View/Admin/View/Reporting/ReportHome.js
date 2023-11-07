5d19a5ee80791d7fc05d9568298d3ad7a39e0f71
Aldo Velarde <aldo.velarde2542001@gmail.com>
2023-11-07 16:52:42 -0600

    Thanks to Amara, I have found the solution!!!!

--- FrontEndReact/src/View/Admin/View/Reporting/ReportHome.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ReportHome.js
@@ -1,202 +1,32 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import { useState } from 'react';
 import AdminViewReport from './AdminViewReport';
+import { Container } from '@mui/material';
+import TabManager from './ReportTabs';
 
 export default function ReportHome(props) {
     var [tab, setTab] = useState('');
     return (
         <>
-            <div className='container'>
-                <Box
-                    sx={{
-                        minHeight:"75vh",
-                        display:"flex",
-                        alignItems:"center"
-                    }}
-                    className='d-flex flex-column mt-5'
-                    >
-                    <Grid
-                        container
-                        rowSpacing={1}
-                        columnSpacing={{
-                            xs: 1,
-                            sm: 2,
-                            md: 3
-                        }}
-                    >
-                        <Grid
-                            sx={{
-                                display:"flex",
-                                justifyContent:"center"
-                            }}
-                            item
-                            xs={3}
-                        >
-                            <div
-                                className='
-                                    d-flex
-                                    flex-column
-                                    m-3
-                                    p-3
-                                    w-100
-                                    justify-content-center
-                                    align-items-center
-                                '
-                                style={{
-                                    borderRadius: '10px'
-                                }}
-                            >        
-                            <h2 style={{ margin: "-90px -5px 5px -200px"}}> {props.chosenCourse["course_name"]} {props.chosenCourse["course_number"]}</h2> 
-                                <Button
-                                    style={{
-                                        width:"100%",
-                                        backgroundColor: "transparent",
-                                        color: "#B0ADAD",
-                                        margin: "-90px 5px 5px 0px",
-                                        position: "center",
-                                        fontWeight: "bold"
-                                    }}
-                                    onClick={() => {
-                                        setTab("Assessment Status");
-                                    }}
-                                >
-                                    Assessment Status
-                                </Button>
-                            </div>
-                        </Grid>
-                        <Grid
-                            sx={{
-                                display:"flex",
-                                justifyContent:"center"
-                            }}
-                            item
-                            xs={3}
-                        >
-                            <div
-                                className='
-                                    d-flex
-                                    flex-column
-                                    m-3
-                                    p-3
-                                    w-100
-                                    justify-content-center
-                                    align-items-center
-                                '
-                                style={{
-                                    borderRadius: '10px'
-                                }}
-                            >
-                                <Button
-                                    style={{
-                                        width:"100%",
-                                        backgroundColor: "transparent",
-                                        color: "#B0ADAD",
-                                        margin: "-90px 5px 5px 0px",
-                                        position: "center",
-                                        fontWeight: "bold"
-                                    }}
-                                    onClick={() => {
-                                        setTab("Ratings and Feedback");
-                                    }}
-                                >
-                                    Ratings and Feedbacks
-                                </Button>
-                            </div>
-                        </Grid>
-                        <Grid
-                            sx={{
-                                display:"flex",
-                                justifyContent:"center"
-                            }}
-                            item
-                            xs={3}
-                        >
-                            <div
-                                className='
-                                    d-flex
-                                    flex-column
-                                    m-3
-                                    p-3
-                                    w-100
-                                    justify-content-center
-                                    align-items-center
-                                '
-                                style={{
-                                    borderRadius: '10px'
-                                }}
-                            >
-                                <Button
-                                    style={{
-                                        width:"100%",
-                                        backgroundColor: "transparent",
-                                        color: "#B0ADAD",
-                                        margin: "-90px 5px 5px 0",
-                                        position: "center",
-                                        fontWeight: "bold"
-                                    }}
-                                    onClick={() => {
-                                        setTab("Improvement");
-                                    }}
-                                >
-                                    Improvement
-                                </Button>
-                            </div>
-                        </Grid>
-                        <Grid
-                            sx={{
-                                display:"flex",
-                                justifyContent:"center"
-                            }}
-                            item
-                            xs={3}
-                        >
-                            <div
-                                className='
-                                    d-flex
-                                    flex-column
-                                    m-3
-                                    p-3
-                                    w-100
-                                    justify-content-center
-                                    align-items-center
-                                '
-                                style={{
-                                    borderRadius: '10px'
-                                }}
-                            >
-                                <Button
-                                    style={{
-                                    width:"100%",
-                                    backgroundColor: "transparent",
-                                    color: "#B0ADAD",
-                                    margin: "-90px 5px 5px 0",
-                                    position: "center",
-                                    fontWeight: "bold",
-                                    }}
-                                    onClick={() => {
-                                        setTab("Calibrations");
-                                    }}
-                                >
-                                  Calibrations
-                                </Button>
-                            </div>
-                        </Grid>
-                    </Grid>
-                    { tab === 'Ratings and Feedback' &&
-                        <AdminViewReport
-                            chosenCourse={props.chosenCourse}
-                        />
-                    }
-                </Box>
-            </div>
-        </>
-    );
-}
+         <h1>
+            {props.chosenCourse["course_name"]} {props.chosenCourse["course_number"]}
+        </h1>
+        <Container>
+            <TabManager setTab={setTab}/>
 
-/*    style={{
-                                        underline: {textDecorationLine: 'underline', color: "#2E8BEF"}
-                                    }}
-                                   >Calibrations</u> */
+            { tab === 'Ratings and Feedback' &&
+                    <AdminViewReport
+                    chosenCourse={props.chosenCourse}
+                 />
+             }
+              {/* { tab === 'Improvement' &&
+                    <AdminViewReport
+                    chosenCourse={props.chosenCourse}
+                 />
+             } */}
+        </Container>
+         </>
+    );
+}  
+                   
+   
--- FrontEndReact/src/View/Admin/View/Reporting/ReportTabs.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ReportTabs.js
@@ -2,67 +2,36 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import ViewAssessmentStatus from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewAssessmentStatus.js';
-import ViewRatingsAndFeedback from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewRatingsAndFeedback.js'
-import ViewImprovement from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewImprovement.js'
-import ViewCalibrations from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewCalibrations.js'
-import { useState } from 'react';
+// import ViewAssessmentStatus from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewAssessmentStatus.js';
+// import ViewRatingsAndFeedback from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewRatingsAndFeedback.js'
+// import ViewImprovement from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewImprovement.js'
+// import ViewCalibrations from '../Admin/View/Reporting/Feedback/ReportingNavbar/Pages/ViewCalibrations.js'
 
-export default function CenteredTabs() {
-  var [tab, setTab] = useState('');
-  function samePageLinkNavigation(event) {
-    if (
-      event.defaultPrevented ||
-      event.button !== 0 || // ignore everything but left-click
-      event.metaKey ||
-      event.ctrlKey ||
-      event.altKey ||
-      event.shiftKey
-    ) {
-      return false;
-    }
-    return true;
-  }
-
-  function Tab(props) {
-    return (
-      <Tab
-        component="a"
-        onClick={(event) => {
-          // Routing libraries handle this, you can remove the onClick handle when using them.
-          if (samePageLinkNavigation(event)) {
-            event.preventDefault();
-          }
-        }}
-        {...props}
-      />
-    );
-  }
-
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  export default function TabManager (props) {
+    var idTab = props.activeTab==="Users"? 0 : (props.activeTab==="Teams" ? 1 : (props.activeTab==="AssessmentTasks") ? 2 : 0);
+    const [value, setValue] = React.useState(idTab);
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
   
-  return (
-    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="Assesment Status" 
-          onClick={() => {
-              setTab(<ViewAssessmentStatus />) 
-              }} />
-        <Tab label="Ratings and Feedback"     
-          onClick={() => {
-              setTab(<ViewRatingsAndFeedback />)
-               }} />
-        <Tab label="Improvement"      
-          onClick={() => {
-              setTab(<ViewImprovement />) }} />
-        <Tab label="Calibrations"     
-          onClick={() => {
-              setTab(<ViewCalibrations />) }} />
-      </Tabs>
-    </Box>
-  );
+    return (
+      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Assesment Status" 
+            onClick={() => {
+                props.setTab("Assessment Status") 
+                }} />
+          <Tab label="Ratings and Feedback"     
+            onClick={() => {
+                props.setTab("Ratings and Feedback");
+                }} />
+          <Tab label="Improvement"      
+            onClick={() => {
+                props.setTab("Improvement") }} />
+          <Tab label="Calibrations"     
+            onClick={() => {
+                props.setTab("Calibrations") }} />
+        </Tabs>
+      </Box>
+    );
   }
