9a8049e4080759aa3766d50852bd5c3a4c1c5117
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-11-08 14:52:59 -0600

    Updated user_course.py by removing unnecessary function get_user_courses_by_user_id_and_role_id()!

--- BackEndFlask/models/user_course.py
+++ BackEndFlask/models/user_course.py
@@ -41,13 +41,6 @@ def get_user_course_by_user_id_and_course_id(user_id, course_id):
         error = str(e.__dict__['orig'])
         return error
 
-def get_user_courses_by_user_id_and_role_id(user_id, role_id):
-    try:
-        return UserCourse.query.filter_by(user_id=user_id, role_id=role_id).all()
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
-    
 def get_user_course(user_course_id):
     try:
         one_user_course = UserCourse.query.filter_by(user_course_id=user_course_id).first()
