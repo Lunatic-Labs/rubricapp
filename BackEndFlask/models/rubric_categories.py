4d0698cdec8b1210353dd83cdd4659b8490235e6
Brennan Cottrell <whiteeaglelane@gmail.com>
2023-11-09 13:31:45 -0600

    rubric categories file

---
+++ BackEndFlask/models/rubric_categories.py
@@ -0,0 +1,16 @@
+from core import db
+from sqlalchemy.exc import SQLAlchemyError
+from models.schemas import RubricCategory 
+
+def create_rubric_category(rubric_category):
+    try:
+        new_category = RubricCategory(
+            rubric_id=rubric_category[0],
+            category_id=rubric_category[1]
+        )
+        db.session.add(new_category)
+        db.session.commit()
+        return new_category
+    except SQLAlchemyError as e:
+        error = str(e.__dict__('orig'))
+        return error
