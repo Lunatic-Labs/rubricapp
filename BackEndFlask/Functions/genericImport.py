26516e0bb6cd833f3c92bf10fd685d7ab6cc56a4
malloc-nbytes <zdhdev@yahoo.com>
2023-11-08 19:17:43 -0600

    added ability to roll back changes in generic_upload()

--- BackEndFlask/Functions/genericImport.py
+++ BackEndFlask/Functions/genericImport.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from core import db
 from Functions.test_files.population_functions import *
 from Functions.helper import helper_verify_email_syntax, helper_create_user, helper_ok, helper_cleanup
 from Functions.customExceptions import *
@@ -11,9 +12,6 @@ import itertools
 import csv
 
 
-# TODO: Require password.
-# TODO: Check to make sure role_id is not None.
-# TODO: `lms_id` needs functionality to be taken as optional when instantiating a new `user`.
 def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
     """
     For bulk uploading either a student or TA.
@@ -39,6 +37,9 @@ def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
     # Renamed `reader` -> `roster`.
     roster: list[list[str]] = list(itertools.tee(csv.reader(student_csv))[0])
 
+    # Begin save point so we can revert changes in the db if an error occurrs.
+    save_point = db.session.begin_nested()
+
     for row in range(0, len(roster)):
         # Renamed `header` -> `person_attribs`.
         person_attribs: list[str] = roster[row]
@@ -47,10 +48,12 @@ def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
         max_person_attribs_count = 4  # Checking for 4 for: FN LN, email, role, (optional) LMS ID
 
         if len(person_attribs) < min_person_attribs_count:
-            return helper_cleanup(user_file, is_xlsx, NotEnoughColumns.error, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, NotEnoughColumns.error, student_csv, save_point=save_point)
 
         if len(person_attribs) > max_person_attribs_count:
-            return helper_cleanup(user_file, is_xlsx, TooManyColumns.error, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, TooManyColumns.error, student_csv, save_point=save_point)
 
         name = person_attribs[0].strip()  # FN,LN
         last_name = name.replace(",", "").split()[0].strip()
@@ -63,7 +66,8 @@ def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
         # TODO: returns tuple, check for the ID attr, or the name.
         role = get_role(role)
         if not helper_ok(role, user_file, is_xlsx):
-            return helper_cleanup(user_file, is_xlsx, role, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, role, student_csv, save_point=save_point)
         role_id = role.role_id
 
         # If the len of `header` == 4, then the LMS ID is present.
@@ -71,36 +75,43 @@ def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
             lms_id = person_attribs[3].strip()
 
         if not helper_verify_email_syntax(email):
-            return helper_cleanup(user_file, is_xlsx, SuspectedMisformatting.error, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, SuspectedMisformatting.error, student_csv, save_point=save_point)
 
         # If `lms_id` is present, and it does not consist of digits
         # then it is invalid.
         if lms_id is not None and not lms_id.isdigit():
-            return helper_cleanup(user_file, is_xlsx, SuspectedMisformatting.error, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, SuspectedMisformatting.error, student_csv, save_point=save_point)
 
         user = get_user_by_email(email)
 
         if not helper_ok(user, user_file, is_xlsx):
-            return helper_cleanup(user_file, is_xlsx, user, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, user, student_csv, save_point=save_point)
 
         # If the user is not already in the DB.
         if user is None:
             created_user = helper_create_user(first_name, last_name, email, role_id, lms_id, owner_id)
             if not helper_ok(created_user, user_file, is_xlsx):
-                return created_user
+                save_point.rollback()
+                return helper_cleanup(user_file, is_xlsx, created_user, student_csv, save_point=save_point)
 
         user_id = get_user_user_id_by_email(email)
         if not helper_ok(user_id, user_file, is_xlsx):
-            return helper_cleanup(user_file, is_xlsx, user_id, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, user_id, student_csv, save_point=save_point)
 
         user_course = get_user_course_by_user_id_and_course_id(user_id, course_id)
         if not helper_ok(user_course, user_file, is_xlsx):
-            return helper_cleanup(user_file, is_xlsx, user_course, student_csv)
+            save_point.rollback()
+            return helper_cleanup(user_file, is_xlsx, user_course, student_csv, save_point=save_point)
 
         if user_course is None:
             user_id = get_user_user_id_by_email(email)
             if not helper_ok(user_id, user_file, is_xlsx):
-                return helper_cleanup(user_file, is_xlsx, user_id, student_csv)
+                save_point.rollback()
+                return helper_cleanup(user_file, is_xlsx, user_id, student_csv, save_point=save_point)
 
             user_course = create_user_course({
                 "user_id": user_id,
@@ -108,8 +119,8 @@ def genericcsv_to_db(user_file: str, owner_id: int, course_id: int):
                 "role_id": role_id,
             })
 
-
             if not helper_ok(user_course, user_file, is_xlsx):
-                return helper_cleanup(user_file, is_xlsx, user_course, student_csv)
+                save_point.rollback()
+                return helper_cleanup(user_file, is_xlsx, user_course, student_csv, save_point=save_point)
 
-    return helper_cleanup(user_file, is_xlsx, None, student_csv)
+    return helper_cleanup(user_file, is_xlsx, None, student_csv, save_point=save_point)
--- BackEndFlask/Functions/helper.py
+++ BackEndFlask/Functions/helper.py
@@ -24,7 +24,7 @@ def helper_verify_email_syntax(email):
     return True
 
 
-def helper_cleanup(xlsx_file, is_xlsx, return_val, csv_file=None):
+def helper_cleanup(xlsx_file, is_xlsx, return_val, csv_file=None, save_point=None):
     """
     This function is to be called when an error is encountered.
     @param xlsx_file: TODO
@@ -33,6 +33,8 @@ def helper_cleanup(xlsx_file, is_xlsx, return_val, csv_file=None):
     @param csv_file: csv file we are working with
     @return: return_val
     """
+    if save_point is not None:
+        save_point.close()
     delete_xlsx(xlsx_file, is_xlsx)
     if csv_file is not None:
         csv_file.close()
