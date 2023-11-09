10982f243045b05506c2ca4f051240754f4c99e6
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2023-11-09 15:57:10 -0600

    Saving progress. Build team might be archived for now

--- FrontEndReact/src/View/Student/View/BuildTeam/BuildTeam.js
+++ FrontEndReact/src/View/Student/View/BuildTeam/BuildTeam.js
@@ -1,81 +1,161 @@
-import React, { Component } from 'react';
+import React, { Component, useId } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import CustomButton from '../Components/CustomButton.js';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import { Grid, IconButton } from '@mui/material';
-import CustomDataTable from '../Components/CustomDataTable.js'
+import CustomDataTable from '../Components/CustomDataTable.js';
 import TextField from '@mui/material/TextField';
-
 
 class BuildTeamTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: {}
+      selected: {},
+      unselected: {},
     };
   }
 
-	handleConfirmTeamClick = () => {
-    // Add your edit functionality here
-		console.log('Confirm Team Button Clicked')
+  handleConfirmTeamClick = () => {
+    // Add your confirm team functionality here
+    console.log('Confirm Team Button Clicked');
   };
 
   handleChange = (user_id) => (event) => {
-    const { selected } = this.state;
-    selected[user_id] = event.target.selected;
-    this.setState({ selected });
-  }
+    const { selected, unselected } = this.state;
+    const targetTable = selected[user_id] ? 'unselected' : 'selected';
 
+    const updatedSelected = { ...selected };
+    const updatedUnselected = { ...unselected };
 
-	render() {
-		const students= this.props.users;
+    if (targetTable === 'selected') {
+      updatedSelected[user_id] = event.target.checked;
+      updatedUnselected[user_id] = !event.target.checked;
+    } else {
+      updatedUnselected[user_id] = event.target.checked;
+      updatedSelected[user_id] = !event.target.checked;
+    }
 
-		const columns = [
-			{
-				name: "first_name",
-				label: "First Name",
-				options: {
-					filter: true,
-					align: "center",
-				}
-			},
-			{
-				name: "last_name",
-				label: "Last Name",
-				options: {
-					filter: true,
-					align: "center"
-				}
-			},
-			{
-			  // TODO: Build select button
+    this.setState({ selected: updatedSelected, unselected: updatedUnselected });
+  };
+
+  render() {
+    const students = this.props.users;
+
+    const selectedColumns = [
+      {
+        name: "first_name",
+        label: "First Name",
+        options: {
+          filter: true,
+          align: "center",
+        },
+      },
+      {
+        name: "last_name",
+        label: "Last Name",
+        options: {
+          filter: true,
+          align: "center",
+        },
+      },
+      {
         name: "user_id",
-        label: "Add",
+        label: "Action",
         options: {
           filter: true,
           sort: false,
           customBodyRender: (user_id) => {
             return (
-              <IconButton aria-label='controlled'
-                onClick={
-                  () => {
-                    this.setState({
-                      selected: this.state.selected[user_id] || false
-                    })
-                    this.handleChange(user_id)
-                  }
-                }
+              <IconButton 
+                aria-label='controlled' 
+                onClick={() => this.handleChange(user_id)}
               >
-              <AddCircleOutlineIcon/>
+                {this.state.selected[user_id] ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />}
               </IconButton>
             );
-          }
-          
-        }
-			}
-		];
+          },
+        },
+      },
+    ];
+
+    const unselectedColumns = [
+      {
+        name: "first_name",
+        label: "First Name",
+        options: {
+          filter: true,
+          align: "center",
+        },
+      },
+      {
+        name: "last_name",
+        label: "Last Name",
+        options: {
+          filter: true,
+          align: "center",
+        },
+      },
+      {
+        name: "user_id",
+        label: "Action",
+        options: {
+          filter: true,
+          sort: false,
+          customBodyRender: (user_id) => {
+            return (
+              <IconButton 
+                aria-label='controlled' 
+                onClick={() => this.handleChange(user_id)}
+              >
+                {this.state.selected[user_id] ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />}
+              </IconButton>
+            );
+          },
+        },
+      },
+    ];
+
+    //
+    // const columns = [
+    //   {
+    //     name: "first_name",
+    //     label: "First Name",
+    //     options: {
+    //       filter: true,
+    //       align: "center",
+    //     },
+    //   },
+    //   {
+    //     name: "last_name",
+    //     label: "Last Name",
+    //     options: {
+    //       filter: true,
+    //       align: "center",
+    //     },
+    //   },
+    //   {
+    //     name: "user_id",
+    //     label: "Action",
+    //     options: {
+    //       filter: true,
+    //       sort: false,
+    //       customBodyRender: (user_id) => {
+    //         return (
+    //           <IconButton 
+    //             aria-label='controlled' 
+    //             onClick={() => this.handleChange(user_id)}
+    //           >
+    //             {this.state.selected[user_id] ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />}
+    //           </IconButton>
+    //         );
+    //       },
+    //     },
+    //   },
+    // ];
+
     const options = {
-    	onRowsDelete: false,
+      onRowsDelete: false,
       download: false,
       print: false,
       selectableRows: "none",
@@ -83,30 +163,32 @@ class BuildTeamTable extends Component {
       responsive: "standard",
       tableBodyMaxHeight: "21rem",
     };
-    
-		return (
-			<>
-       <div style={{ padding: '70px', backgroundColor: '#F8F8F8' }}>
-          <div>
-            <h2 style={{ padding: '25px', marginLeft: '-35px', bold: true }}> Build your new team </h2>
 
+    const selectedStudents = students.filter((student) => this.state.selected[student.user_id]);
+    const unselectedStudents = students.filter((student) => !this.state.selected[student.user_id]);
+
+    return (
+      <>
+        <div style={{ padding: '70px', backgroundColor: '#F8F8F8' }}>
+          <div>
+            <h2 style={{ padding: '25px', marginLeft: '-35px', fontWeight: 'bold' }}>Build your new team</h2>
             <Grid container spacing={2} alignItems='center'>
               <Grid item xs={6}>
-                <h2 style={{ padding: '14px', marginLeft: '-15px' }}> Roster </h2>
+                <h2 style={{ padding: '14px', marginLeft: '-15px' }}>Roster</h2>
               </Grid>
-              <Grid item xs={2.4} container justifyContent='flex-end'>
+              <Grid item xs={2.5} container justifyContent='flex-end'>
                 <Grid item>
-                  <TextField 
-                    label='Team Name' 
-                    variant='outlined' 
+                  <TextField
+                    label='Team Name'
+                    variant='outlined'
                     style={{ width: '190%' }}
                   />
                 </Grid>
               </Grid>
-              <Grid item xs={3.6} container justifyContent='flex-end'>
+              <Grid item xs={3.5} container justifyContent='flex-end'>
                 <Grid item>
-                  <CustomButton 
-                   label='Confirm Team'
+                  <CustomButton
+                    label='Confirm Team'
                     onClick={this.handleConfirmTeamClick}
                     outlined={false}
                     position={{ top: '-25px', right: '0px' }}
@@ -114,28 +196,27 @@ class BuildTeamTable extends Component {
                 </Grid>
               </Grid>
             </Grid>
-
             <Grid container spacing={8}>
               <Grid item xs={6}>
-                <CustomDataTable 
-                  data={students ? students : []} 
-                  columns={columns}
+                <CustomDataTable
+                  data={unselectedStudents}
+                  columns={unselectedColumns}
                   options={options}
                 />
               </Grid>
               <Grid item xs={6}>
-                <CustomDataTable 
-                  data={students ? students : []} 
-                  columns={columns}
+                <CustomDataTable
+                  data={selectedStudents}
+                  columns={selectedColumns}
                   options={options}
                 />
               </Grid>
             </Grid>
           </div>
-       </div>
-			</>
-		)
-	}
+        </div>
+      </>
+    );
+  }
 }
 
 export default BuildTeamTable;
