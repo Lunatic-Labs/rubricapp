720334b9d68f0cb86430a5a9aed067605b865a17
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-11-16 17:15:58 -0600

    Updated User_routes.py by adding a new condition to check that admins are being retrieved and ensures only the SuperAdmin is requesting to view all admins! Also removed unnecessary call of makeAdmin() and updated the valid spot to call makeAdmin() by only passing the user_id of the existing user! Updated user.py by adding logic to create_user() to correctly turn isAdmin to true! Added makeAdmin() and get_users_by_isAdmin()! Updated the demo data to reflect updated logic in create_user()! Updated AdminViewUsers.js by updating the url parameter to be isAdmin=True!

--- BackEndFlask/models/user.py
+++ BackEndFlask/models/user.py
@@ -119,13 +119,32 @@ def create_user(user_data):
             lms_id=user_data["lms_id"],
             consent=user_data["consent"],
             owner_id=user_data["owner_id"],
-            isAdmin=False
+            isAdmin=user_data["role_id"] is not None and user_data["role_id"]==3
         )
         db.session.add(user_data)
         db.session.commit()
         return user_data
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
+        return error
+
+def makeAdmin(user_id):
+    try:
+        user = User.query(user_id=user_id).first()
+        user.isAdmin = True
+        db.session.add(user)
+        db.session.commit()
+        return user
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
+
+def get_users_by_isAdmin():
+    try:
+        admins = User.query.filter_by(isAdmin=True).all()
+        return admins
+    except SQLAlchemyError as e:
+        error = e.__dict__['orig']
         return error
 
 # user_id = 1
@@ -137,21 +156,22 @@ def load_SuperAdminUser():
         "password": "superadminsecretpassword01",
         "lms_id": 0,
         "consent": None,
-        "owner_id": 0
+        "owner_id": 0,
+        "role_id": None
     })
 
 # user_id = 2
 def load_demo_admin():
-    admin = create_user({
+    create_user({
         "first_name": "Braden",
         "last_name": "Grundmann",
         "email": "demoadmin02@skillbuilder.edu",
         "password": "demoadminsecretpassword02",
         "lms_id": 1,
         "consent": None,
-        "owner_id": 1
+        "owner_id": 1,
+        "role_id": 3
     })
-    makeAdmin(admin.user_id, 3)
 
 # user_id = 3
 def load_demo_ta_instructor():
@@ -162,7 +182,8 @@ def load_demo_ta_instructor():
         "password": "demotainstructorsecretpassword03",
         "lms_id": 2,
         "consent": None,
-        "owner_id": 2
+        "owner_id": 2,
+        "role_id": 4
     })
 
 def load_demo_student():
@@ -229,7 +250,8 @@ def load_demo_student():
             "password": f"demostudentsecretpassword{count}",
             "lms_id": count,
             "consent": None,
-            "owner_id": 2
+            "owner_id": 2,
+            "role_id": 5
         })
         count += 1
 
@@ -254,21 +276,6 @@ def replace_user(user_data, user_id):
         error = "Invalid user_id, user_id does not exist!"
         return error
     
-def makeAdmin(user_id, role_id):
-    try:
-        one_user = User.query.filter_by(user_id=user_id).first()
-        if one_user is None:
-            raise InvalidUserID
-        if role_id == 3:
-            one_user.isAdmin = True
-        return one_user
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        return error
-    except InvalidUserID:
-        error = "Invalid user_id, user_id does not exist!"
-        return error
-
 # def update_user_first_name(user_id, new_first_name):
 #     try:
 #         one_user = Users.query.filter_by(user_id=user_id).first()
