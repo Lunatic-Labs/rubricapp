7d820ce26770810443e4ea7ca4efbc49cd4f3138
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-11-29 21:45:04 -0600

    Updated BasicTabs.js by adding navbar.state to access the activeTab!

--- FrontEndReact/src/View/Navbar/BasicTabs.js
+++ FrontEndReact/src/View/Navbar/BasicTabs.js
@@ -3,17 +3,16 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 
-
 export default function BasicTabs (props){
   var navbar = props.navbar;
-  var activeTab = navbar.activeTab;
+  var state = navbar.state;
+  var activeTab = state.activeTab;
   var setNewTab = navbar.setNewTab;
   var idTab = activeTab==="Users"? 0 : (activeTab==="Teams" ? 1 : (activeTab==="AssessmentTasks") ? 2 : 0);
   const [value, setValue] = React.useState(idTab);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
   return (
     <Box>
       <Box sx={{ borderColor: 'divider' }}>
