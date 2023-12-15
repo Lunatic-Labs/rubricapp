4fc965bf7e36f6f629d7be5dc1c3b0cfea1a107a
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-12-15 11:55:08 -0600

    Updated Feedback_routes.py by removing unnecessary commented out import, added necessary whitespace, and fixed small text errors!

--- BackEndFlask/controller/Routes/Feedback_routes.py
+++ BackEndFlask/controller/Routes/Feedback_routes.py
@@ -1,28 +1,38 @@
 from models.feedback import *
-# from models.completed_assessment import *
 from controller import bp
-from flask_marshmallow import Marshmallow
 from controller.Route_response import *
 from datetime import datetime
+
 
 @bp.route("/feedback", methods=["POST"])
 def create_new_feedback(): 
     # given completed_assessment_id and user_id, create feedback entry
     user_id = request.json["user_id"]
     completed_assessment_id = request.json["completed_assessment_id"]
-    
+
     feedback_data = request.json 
     feedback_data["lag_time"] = None
-    
     feedback_data["feedback_time"] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     feedback = create_feedback(request.json)
+
     if feedback == str: 
-        print(f"[ Feedback /feedback POST] An error occurred creating feedback for completed_assessment_id: {completed_assessment_id} and user_id {user_id}")
-        createBadResponse(f"An error occurred creatingfeedback for completed_assessment_id: {completed_assessment_id} and user_id: {user_id}!")
+        print(
+            f"[Feedback_routes.py /feedback POST] An error occurred creating feedback for completed_assessment_id: {completed_assessment_id} and user_id {user_id}: ",
+            feedback
+        )
+        createBadResponse(
+            f"An error occurred creating feedback for completed_assessment_id: {completed_assessment_id} and user_id: {user_id}!",
+            feedback, "feedbacks"
+        )
         return response
-    createGoodResponse("Successfully retrieved all the individual feedback times!", student_feedback_schema.dump(feedback), 200, "feedback")
+
+    createGoodResponse(
+        "Successfully retrieved all the individual feedback times!",
+        student_feedback_schema.dump(feedback),
+        200, "feedbacks"
+    )
+
     return response
-    
     
 
 class StudentFeedbackSchema(ma.Schema):
