f892fa8f11ba6f9dd1adda7c0afd31628d087c7e
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-12-15 14:35:42 -0600

    Updated Rating_routes.py by removing unnecessary imports, added necessary whitespace, and fixed incorrect spelling!

--- BackEndFlask/controller/Routes/Rating_routes.py
+++ BackEndFlask/controller/Routes/Rating_routes.py
@@ -1,9 +1,7 @@
 from models.completed_assessment import *
 from models.feedback import *
 from controller import bp
-from flask_marshmallow import Marshmallow
 from controller.Route_response import *
-from datetime import datetime
 
 """
 assessment_task_id = 5 shows all the data for rating!
@@ -15,12 +13,18 @@ def get_student_individual_ratings():
     # get all the names of all individuals who completed it
     # and their CompletedAssessmentTasks
     assessment_task_id = int(request.args.get("assessment_task_id"))
+
     if assessment_task_id == type(""):
-        print(f"[ Rating /rating GET] An error occurred retrieving all ratings for assessment_task_id: {assessment_task_id}")
-        createBadResponse(f"An error occurred retrieving all ratings for assessment_task_id: {assessment_task_id}!")
+        print(
+            f"[Rating_routes.py /rating GET] An error occurred retrieving all ratings for assessment_task_id: {assessment_task_id}"
+        )
+        createBadResponse(
+            f"An error occurred retrieving all ratings for assessment_task_id: {assessment_task_id}!",
+            assessment_task_id, "Ratings"
+        )
         return response
-    completed =  get_individual_completed_and_student(assessment_task_id)
-    completed = list(completed)
+
+    completed = get_individual_completed_and_student(assessment_task_id)
     
     feedback = completed[3]
     submission = completed[4]
@@ -36,8 +40,14 @@ def get_student_individual_ratings():
     data['last_name'] = completed[1]
     data['rating_observable_characteristics_suggestions_data'] = completed[2]
     data['lag_time'] = str(lag_time)
-    createGoodResponse("Successfully retrieved all individual ratings!", name_rating_schema.dump(data), 200, "ratings")
+
+    createGoodResponse(
+        "Successfully retrieved all individual ratings!",
+        name_rating_schema.dump(data),
+        200, "ratings"
+    )
     return response
+
 
 class NameRatingSchema(ma.Schema):
     class Meta:
@@ -47,6 +57,7 @@ class NameRatingSchema(ma.Schema):
             'rating_observable_characteristics_suggestions_data',
             'lag_time'
         )
+
 
 name_rating_schema = NameRatingSchema()
 name_ratings_schema = NameRatingSchema(many=True)
