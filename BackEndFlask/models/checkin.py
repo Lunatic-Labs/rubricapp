222d6c1bd4f64210981713cccfb0d6416ae92eed
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-12-19 18:14:21 -0600

    Updated checkin.py by removing unnecessary whitespace!

--- BackEndFlask/models/checkin.py
+++ BackEndFlask/models/checkin.py
@@ -14,7 +14,7 @@ def create_checkin(checkin):
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         return error
-    
+
 
 def get_checkins_by_assessment(assessment_task_id):
     try: 
