3909d11456acad2a125144d15426305624b53ee3
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-01-05 01:12:09 -0600

    Updated ShowTeamMembers.js by adding formatting and whitespace!

--- FrontEndReact/src/View/Student/View/BuildTeam/ShowTeamMembers.js
+++ FrontEndReact/src/View/Student/View/BuildTeam/ShowTeamMembers.js
@@ -10,52 +10,56 @@ class ShowTeamMembers extends Component {
         super(props);
         this.state = {
             error: null,
-			errorMessage: null,
+            errorMessage: null,
             isLoaded: false,
             selectedTeam: null,
             users: null
         }
+
         this.removeUser = (user_id) => {
-        var students = this.state.users;
-        var studentsRemaining = [];
-        for(var student = 0; student < students.length; student++) {
-            if(students[student]["user_id"]!==user_id) {
-            studentsRemaining = [...studentsRemaining, students[student]];
+            var students = this.state.users;
+            var studentsRemaining = [];
+
+            for(var student = 0; student < students.length; student++) {
+                if(students[student]["user_id"]!==user_id) {
+                studentsRemaining = [...studentsRemaining, students[student]];
+                }
             }
-        }
-        this.setState({
-            users: studentsRemaining
-        });
+
+            this.setState({
+                users: studentsRemaining
+            });
         }
     }
 
     componentDidUpdate() {
         var navbar = this.props.navbar;
         var team_id = navbar.buildTeam.selectedTeam;
+
         if (team_id !== null && team_id !== this.state.selectedTeam) {
             genericResourceGET(`/user?team_id=${team_id}`, 'users', this);
         }
     }
 
-	render() {
-		const studentColumns = [
-			{
-				name: "first_name",
-				label: "First Name",
-				options: {
-					filter: true,
-					align: "center",
-				}
-			},
-			{
-				name: "last_name",
-				label: "Last Name",
-				options: {
-					filter: true,
-					align: "center"
-				}
-			},
-			{
+    render() {
+        const studentColumns = [
+            {
+                name: "first_name",
+                label: "First Name",
+                options: {
+                    filter: true,
+                    align: "center",
+                }
+            },
+            {
+                name: "last_name",
+                label: "Last Name",
+                options: {
+                    filter: true,
+                    align: "center"
+                }
+            },
+            {
                 name: "user_id",
                 label: "Unassign",
                 options: {
@@ -73,37 +77,37 @@ class ShowTeamMembers extends Component {
                         );
                     }
                 }
-			}
-		];
-
-    const options = {
-        onRowsDelete: false,
-        download: false,
-        print: false,
-        selectableRows: "none",
-        selectableRowsHeader: false,
-        responsive: "standard",
-        tableBodyMaxHeight: "21rem",
-    };
-
-    var navbar = this.props.navbar;
-    var team_id = navbar.buildTeam.selectedTeam;
-    var students = this.state.users;
-
-	return (
-        <>
-            { (team_id !== null) && students !== null &&
-                <>
-                    <CustomDataTable 
-                        data={students}
-                        columns={studentColumns}
-                        options={options}
-                    />
-                </>
             }
-		</>
-		)
-	}
+        ];
+
+        const options = {
+            onRowsDelete: false,
+            download: false,
+            print: false,
+            selectableRows: "none",
+            selectableRowsHeader: false,
+            responsive: "standard",
+            tableBodyMaxHeight: "21rem",
+        };
+
+        var navbar = this.props.navbar;
+        var team_id = navbar.buildTeam.selectedTeam;
+        var students = this.state.users;
+
+        return (
+            <>
+                { (team_id !== null) && students !== null &&
+                    <>
+                        <CustomDataTable 
+                            data={students}
+                            columns={studentColumns}
+                            options={options}
+                        />
+                    </>
+                }
+            </>
+        )
+    }
 }
 
 export default ShowTeamMembers;
