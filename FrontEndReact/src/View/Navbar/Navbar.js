4f364b5569087e5423fddd50c6d4ed17cf924454
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-01-05 00:58:26 -0600

    Updated Navbar.js by removing div!

--- FrontEndReact/src/View/Navbar/Navbar.js
+++ FrontEndReact/src/View/Navbar/Navbar.js
@@ -30,104 +30,102 @@ export default function ButtonAppBar(props) {
   };
 
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar sx={{
-            backgroundColor:"white", 
-            py:"0.5rem", 
-            px:{xs:"0.5rem",md:"1rem"},
-            display: "flex", 
-            justifyContent: "space-between"}}>
-            <Typography variant="h6" component="div" sx={{
-                color: "#2E8BEF",
-                fontFeatureSettings: "'clig' off, 'liga' off",
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{
+          backgroundColor:"white", 
+          py:"0.5rem", 
+          px:{xs:"0.5rem",md:"1rem"},
+          display: "flex", 
+          justifyContent: "space-between"}}>
+          <Typography variant="h6" component="div" sx={{
+              color: "#2E8BEF",
+              fontFeatureSettings: "'clig' off, 'liga' off",
+              fontFamily: "Roboto",
+              fontSize: {xs:"24px", md:"24px"},
+              fontStyle: "normal",
+              fontWeight: "500",
+              lineHeight: "160%",
+              letterSpacing: "0.15px"
+          }}>
+              SkillBuilder
+          </Typography>
+
+          <Box component="div" sx={{ paddingLeft:{xs:"2rem"},display: 'flex', justifyContent:'space-between',alignItems:'center'}}>
+            <Typography variant='h5' sx={{
+                color:"black",
                 fontFamily: "Roboto",
-                fontSize: {xs:"24px", md:"24px"},
+                fontSize: {xs:"12px",md:"14px"},
                 fontStyle: "normal",
-                fontWeight: "500",
-                lineHeight: "160%",
-                letterSpacing: "0.15px"
-            }}>
-                SkillBuilder
+                fontWeight: "400 !important",
+                lineHeight:"150%" 
+                }} >
+                {props.user_name}
             </Typography>
 
-            <Box component="div" sx={{ paddingLeft:{xs:"2rem"},display: 'flex', justifyContent:'space-between',alignItems:'center'}}>
-              <Typography variant='h5' sx={{
-                  color:"black",
-                  fontFamily: "Roboto",
-                  fontSize: {xs:"12px",md:"14px"},
-                  fontStyle: "normal",
-                  fontWeight: "400 !important",
-                  lineHeight:"150%" 
-                  }} >
-                  {props.user_name}
-              </Typography>
+            <Button sx={{minWidth:{xs:"40px"}}} onClick={handleClick} aria-controls={open ? 'account-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}>
+              <img src={ExpandMoreFilled} alt='ExpandMoreFilled'></img>
+            </Button>
 
-              <Button sx={{minWidth:{xs:"40px"}}} onClick={handleClick} aria-controls={open ? 'account-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}>
-                <img src={ExpandMoreFilled} alt='ExpandMoreFilled'></img>
-              </Button>
-
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: 'visible',
-                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                    mt: 1.5,
-                    '& .MuiAvatar-root': {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    '&:before': {
-                      content: '""',
-                      display: 'block',
-                      position: 'absolute',
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: 'background.paper',
-                      transform: 'translateY(-50%) rotate(45deg)',
-                      zIndex: 0,
-                    },
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
                   },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-              >
-                <MenuItem onClick={handleClose}>
-                  <Avatar /> My account
-                </MenuItem>
+                  '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem onClick={handleClose}>
+                <Avatar /> My account
+              </MenuItem>
 
-                <Divider />
+              <Divider />
 
-                <MenuItem onClick={handleClose}>
-                  <ListItemIcon>
-                    <Settings fontSize="small" />
-                  </ListItemIcon>
-                  Settings
-                </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Settings fontSize="small" />
+                </ListItemIcon>
+                Settings
+              </MenuItem>
 
-                <MenuItem>
-                  <ListItemIcon>
-                    <Logout/>
-                  </ListItemIcon>
-                </MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
+              <MenuItem>
+                <ListItemIcon>
+                  <Logout/>
+                </ListItemIcon>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
