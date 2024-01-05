1ee9e6ad63b25863a9232b7e02e23c4d94621e4c
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-01-05 00:23:59 -0600

    Updated queries.py by removing unneeded whitespace!

--- BackEndFlask/models/queries.py
+++ BackEndFlask/models/queries.py
@@ -388,7 +388,6 @@ def get_all_checkins_for_student_for_course(user_id, course_id):
         Parameters:
         user_id: int (The id of a user)
         course_id: int (The id of a course)
-   
     """
     try:
         assessment_task_ids = db.session.query(Checkin.assessment_task_id).\
