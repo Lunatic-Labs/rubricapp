33afe35730c14123a0c8112935ff9881f164e2f5
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-01-18 23:49:51 -0600

    Updated jest.config.js by removing no longer needed transformIgnorePatterns because it did not solve the issue with jest not being able to transpile modules DemoContainer and DemoItem!

--- FrontEndReact/jest.config.js
+++ FrontEndReact/jest.config.js
@@ -185,10 +185,6 @@ const config = {
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  transformIgnorePatterns: [
-    "/node_modules/(?!(@mui/x-date-pickers/)/)"
-  ],
-
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
