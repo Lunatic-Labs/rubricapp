80aefa321b3ed2ac10fd6277add381d6590d4a59
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-01-20 14:01:13 -0600

    Updated Login.test.js by adding a comment letting know that Demo Data is needed for Tests 7-10! Also numbered every test!

--- FrontEndReact/src/View/Login/__tests__/Login.test.js
+++ FrontEndReact/src/View/Login/__tests__/Login.test.js
@@ -16,14 +16,16 @@ import {
 } from '../../../App.js';
 
 
+// NOTE: Tests 7-10 will not pass if Demo Data is not loaded!
 
-test('Login.test.js: should render Login Form component', () => {
+
+test('Login.test.js Test 1: should render Login Form component', () => {
     render(<Login />);
     expect(screen.getByRole('form')).toBeInTheDocument();
 });
 
 
-test('Login.test.js: should show error missing email and password when email and password are missing', async () => {
+test('Login.test.js Test 2: should show error missing email and password when email and password are missing', async () => {
     render(<Login />);
     fireEvent.click(screen.getByLabelText('login_button'));
 
@@ -35,7 +37,7 @@ test('Login.test.js: should show error missing email and password when email and
 });
 
 
-test('Login.test.js: should show error missing email or password when email is missing', async () => {
+test('Login.test.js Test 3: should show error missing email or password when email is missing', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: 'password_test123'}});
     fireEvent.click(screen.getByLabelText('login_button'));
@@ -48,7 +50,7 @@ test('Login.test.js: should show error missing email or password when email is m
 });
 
 
-test('Login.test.js: should show error missing email or password when password is missing', async () => {
+test('Login.test.js Test 4: should show error missing email or password when password is missing', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'test21@test.com'}});
     fireEvent.click(screen.getByLabelText('login_button'));
@@ -61,7 +63,7 @@ test('Login.test.js: should show error missing email or password when password i
 });
 
 
-test('Login.test.js: should show error invalid email when email is invalid and password is not missing', async () => {
+test('Login.test.js Test 5: should show error invalid email when email is invalid and password is not missing', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'invalidEmail1@test.com' }});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: 'testpassword123' }});
@@ -75,7 +77,7 @@ test('Login.test.js: should show error invalid email when email is invalid and p
 });
 
 
-test('Login.test.js: should show error unable to verify when email is valid but password is invalid', async () => {
+test('Login.test.js Test 6: should show error unable to verify when email is valid but password is invalid', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'superadminuser01@skillbuilder.edu' }});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: 'testpassword123' }});
@@ -89,7 +91,7 @@ test('Login.test.js: should show error unable to verify when email is valid but 
 });
 
 
-test('Login.test.js: should show users page for super admin view using super admin credentials', async () => {
+test('Login.test.js Test 7: should show users page for super admin view using super admin credentials', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'superadminuser01@skillbuilder.edu'}});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: super_admin_password}});
@@ -108,7 +110,7 @@ test('Login.test.js: should show users page for super admin view using super adm
 });
 
 
-test('Login.test.js: should show courses page for admin view using demo admin credentials', async () => {
+test('Login.test.js Test 8: should show courses page for admin view using demo admin credentials', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'demoadmin02@skillbuilder.edu'}});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: demo_admin_password}});
@@ -127,7 +129,7 @@ test('Login.test.js: should show courses page for admin view using demo admin cr
 });
 
 
-test('Login.test.js: should show courses page for ta/instructor view using demo ta/instructor credentials', async () => {
+test('Login.test.js Test 9: should show courses page for ta/instructor view using demo ta/instructor credentials', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'demotainstructor03@skillbuilder.edu'}});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: demo_ta_instructor_password}});
@@ -146,7 +148,7 @@ test('Login.test.js: should show courses page for ta/instructor view using demo 
 });
 
 
-test('Login.test.js: should show courses page for student view using demo student credentials', async () => {
+test('Login.test.js Test 10: should show courses page for student view using demo student credentials', async () => {
     render(<Login />);
     fireEvent.change(screen.getByLabelText('email_input'), { target: { value: 'demostudent4@skillbuilder.edu'}});
     fireEvent.change(screen.getByLabelText('password_input'), { target: { value: demo_student_password + '4'}});
@@ -165,7 +167,7 @@ test('Login.test.js: should show courses page for student view using demo studen
 });
 
 
-test('Login.test.js: should show reset password page when clicking link', async () => {
+test('Login.test.js Test 11: should show reset password page when clicking link', async () => {
     render(<Login/>);
     fireEvent.click(screen.getByLabelText('reset_password_button'));
 
