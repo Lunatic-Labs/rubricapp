4543f9064288846a86159cf52923e544943591c8
Braden Duke <bradenduke@bradens-air.bisonnet.lipscomb.edu>
2024-01-22 12:47:13 -0600

    Added folder and file

--- FrontEndReact/src/App.js
+++ FrontEndReact/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './View/Login/Login';
 import './SBStyles.css';
+import AdminAddCustomRubric from './View/Admin/Add/AddCustomRubric/AdminAddCustomRubric';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='*' element={<Navigate to='/'/>} />
-          <Route path='/' element={<Login/>} />
+          {/* <Route path='/' element={<Login/>} /> */}
+          <Route path='/' element={<AdminAddCustomRubric/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ FrontEndReact/src/View/Admin/Add/AddCustomRubric/AdminAddCustomRubric.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+// import '../AddUsers/addStyles.css';
+// import validator from 'validator';
+// import ErrorMessage from '../../../Error/ErrorMessage';
+// import { API_URL } from '../../../../App';
+
+class AdminAddCustomRubric extends Component {
+    render() {
+        return(
+            <div/>
+        )
+    }
+}
+
+export default AdminAddCustomRubric;
