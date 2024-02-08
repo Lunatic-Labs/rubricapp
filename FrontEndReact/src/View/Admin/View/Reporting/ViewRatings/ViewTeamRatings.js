45c5acf36a3e35cf33618881f91b0e09be875f8a
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-02-08 16:19:03 -0600

    Updated ViewTeamRatings.js by adding whitespace!

--- FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewTeamRatings.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewTeamRatings.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import MUIDataTable from 'mui-datatables';
 
-// THE LINK FOR THIS LIBRARY 
-// https://www.npmjs.com/package/mui-datatables#available-plug-ins
+
 
 export default class ViewRatings extends Component {
   render() {
     var allRatings = [];
     var rating = {};
+
     this.props.ratings.map((currentRating) => {
         rating["student_name"] = currentRating["first_name"] + " " + currentRating["last_name"];
+
         if(currentRating["rating_observable_characteristics_suggestions_data"]) {
           Object.keys(currentRating["rating_observable_characteristics_suggestions_data"]).map((category) => {
             return rating[category] = currentRating["rating_observable_characteristics_suggestions_data"][category]["rating"];
           });
+
           return allRatings.push(rating);
         }
+
         return allRatings;
     });
+
     const columns = [
       {
         name: "team_name",
@@ -25,42 +29,42 @@ export default class ViewRatings extends Component {
         options: {
           filter: true,
         }
-      },  
+      },
       {
         name: "Identifying the Goal",
         label: "Identifying the Goal",
         options: {
           filter: true,
         }
-      },  
+      },
       {
         name: "Evaluating",
         label: "Evaluating",
         options: {
           filter: true,
         }
-      },  
+      },
       {
         name: "Analyzing",
         label: "Analyzing",
         options : {
           filter: true,
         }
-      },  
+      },
       {
         name: "Synthesizing",
         label: "Synthesizing",
         options: {
           filter: true,
         }
-      }, 
+      },
       {
         name: "Forming Arguments (Structure)",
         label: "Forming Arguments (Structure)",
         options: {
           filter: true,
         }
-      },  
+      },
       {
         name: "Forming Arguments (Validity)",
         label: "Forming Arguments (Validity)",
@@ -76,8 +80,9 @@ export default class ViewRatings extends Component {
           filter: true,
           align: "center"
         }
-      }  
-    ]
+      }
+    ];
+
     const options= {
       onRowsDelete: false,
       download: false,
@@ -87,10 +92,9 @@ export default class ViewRatings extends Component {
       responsive: "standard",
       tableBodyMaxHeight: "70%",
     };
+
     return (
-      <>
-       <MUIDataTable data={allRatings} columns={columns} options={options}/>
-      </>
+      <MUIDataTable data={allRatings} columns={columns} options={options}/>
     )
   }
 }
