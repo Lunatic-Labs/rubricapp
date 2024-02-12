bc2c5a5cde58c70d8dfe22b820e69a70c83f2258
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-02-11 20:55:58 -0600

    Updated ErrorMessage.js by refactoring the logic to be more clear and scalable!

--- FrontEndReact/src/View/Error/ErrorMessage.js
+++ FrontEndReact/src/View/Error/ErrorMessage.js
@@ -6,32 +6,40 @@ import { Box, Alert } from '@mui/material';
 
 class ErrorMessage extends Component {
     render() {
-        if (Object.hasOwn(this.props, 'fetchedResource')) {
-            return(
-                <Box sx={{ width: "100%", display: "flex", justifyContent: "center"}}>
-                    <Alert aria-label='error_message_alert' sx={{ width: "40%", mt: 2, position:"absolute" }} severity="error" variant="filled">
-                        {
-                            (
-                                this.props.fetchedResource ?
-                                "Fetching " + this.props.fetchedResource :
-                                ( this.props.add ? "Creating a new " : "Updating a new ") + this.props.resource + " resulted in an error: "
-                            )
+        var displayedMessage;
 
-                            + ": " + this.props.errorMessage
-                        }
-                    </Alert>
-                </Box>
-            )
+        if (Object.hasOwn(this.props, 'fetchedResource')) {
+            var message;
+
+            if (this.props.fetchedResource) {
+                message = `Fetching ${this.props.fetchedResource}`;
+
+            } else {
+                var method;
+
+                if (this.props.add) {
+                    method = "Creating a new ";
+
+                } else {
+                    method = "Updating an existing ";
+                }
+
+                message = `${method} ${this.props.resource} resulted in an error`;
+            }
+
+            displayedMessage = `${message}: ${this.props.errorMessage}`;
 
         } else {
-            return(
-                <Box sx={{ width: "100%", display: "flex", justifyContent: "center"}}>
-                   <Alert aria-label='error_message_alert' sx={{ width: "40%", mt: 2, position:"absolute" }} severity="error" variant="filled">
-                        { this.props.errorMessage }
-                    </Alert>
-              </Box>
-            )
+            displayedMessage = this.props.errorMessage;
         }
+
+        return(
+            <Box sx={{ width: "100%", display: "flex", justifyContent: "center"}}>
+                <Alert aria-label='error_message_alert' sx={{ width: "40%", mt: 2, position:"absolute" }} severity="error" variant="filled">
+                    { displayedMessage }
+                </Alert>
+            </Box>
+        )
     }
 }
 
