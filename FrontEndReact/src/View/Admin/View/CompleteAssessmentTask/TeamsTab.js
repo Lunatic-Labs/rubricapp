9a209f96f357546de722c285e3be81f2c58f0ee8
Aldo Velarde <aldo.velarde2542001@gmail.com>
2024-02-29 15:15:40 -0600

    Removed redundant if statement

--- FrontEndReact/src/View/Admin/View/CompleteAssessmentTask/TeamsTab.js
+++ FrontEndReact/src/View/Admin/View/CompleteAssessmentTask/TeamsTab.js
@@ -23,28 +23,24 @@ class TeamsTab extends Component {
             var checkin = this.props.checkin;
             var teamNames = [];
 
-            if(teamMembers.length === 0){
-                teamNames = [...teamNames, <Box key={0}> No Team Members Checked In</Box>]
-            }
-
-            else {        
-                for(var index = 0; index < teamMembers.length; index++){
-                    for (let i = 0; i < checkin.length; i++) {
-                        const currentObject = checkin[i];
-                      
-                        if ('user_id' in currentObject && currentObject.user_id === teamMembers[index]["user_id"]) {
-                            var firstName = teamMembers[index]["first_name"];
-                            var lastName = teamMembers[index]["last_name"];
-                            var fullName = firstName + " " + lastName;
-                            teamNames = [...teamNames, <Box key={index}> {fullName} </Box>];
-                        }
+                   
+            for(var index = 0; index < teamMembers.length; index++){
+                for (let i = 0; i < checkin.length; i++) {
+                    const currentObject = checkin[i];
+                    
+                    if ('user_id' in currentObject && currentObject.user_id === teamMembers[index]["user_id"]) {
+                        var firstName = teamMembers[index]["first_name"];
+                        var lastName = teamMembers[index]["last_name"];
+                        var fullName = firstName + " " + lastName;
+                        teamNames = [...teamNames, <Box key={index}> {fullName} </Box>];
                     }
                 }
-
-                teamNames = teamNames.length === 0
-                ? [<Box key={0}> No Team Members Checked In</Box>]
-                : teamNames;
             }
+
+            teamNames = teamNames.length === 0
+            ? [<Box key={0}> No Team Members Checked In</Box>]
+            : teamNames;
+            
 
             teamList.push(
                 <Tab
