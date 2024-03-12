12c99144dfee50ab85b95bd39cdd7fc70e01d9b3
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-03-12 14:17:11 -0500

    Updated RouteUtilities.py by adding missing word of string and reformatted description message!

--- BackEndFlask/controller/Routes/RouteUtilities.py
+++ BackEndFlask/controller/Routes/RouteUtilities.py
@@ -5,12 +5,12 @@ Custom utilities used throughout the Routes directory.
 
 """
 Desciption:
-Given an array of elements, returns true if any
-element is None, an empty string, or the string
-undefined. Returns false otherwise.
+Given an array of string elements, returns true
+if any string element is None, an empty string,
+or the string undefined. Returns false otherwise.
 
 Parameters:
-arr: The list of elements to check if missing.
+arr: The list of string elements to check if missing.
 """
 def is_any_variable_in_array_missing(arr: list) -> bool:
     for var in arr:
