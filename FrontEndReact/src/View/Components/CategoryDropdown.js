d42ff204e9929efd0d020b736acc555052c86037
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-03-22 10:06:53 -0500

    Updated CategoryDropdown.js by adding whitespace and formatting!

--- FrontEndReact/src/View/Components/CategoryDropdown.js
+++ FrontEndReact/src/View/Components/CategoryDropdown.js
@@ -4,32 +4,34 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+
+
 export default function CategoryDropdown(props) {
-    var categoryList = [];
+  var categoryList = [];
 
-    props.categories.map((category) => {
-      return categoryList.push(
-        <MenuItem key={category} value={category}>
-          {category}
-        </MenuItem>
-      )
-    });
-
-    return (
-        <FormControl sx={{ m: 3, minWidth: 300 }}>
-            <InputLabel id="demo-simple-select-autowidth-label">Category</InputLabel>
-
-            <Select
-              labelId="demo-simple-select-autowidth-label"
-              id="demo-simple-select-autowidth"
-              value={props.chosenCategoryId}
-              onChange={props.setChosenCategoryId}
-              disabled={props.disabled}
-              autoWidth
-              label="Category"
-            >
-              { categoryList }
-            </Select>
-        </FormControl>
+  props.categories.map((category) => {
+    return categoryList.push(
+      <MenuItem key={category} value={category}>
+        {category}
+      </MenuItem>
     )
+  });
+
+  return (
+    <FormControl sx={{ m: 3, minWidth: 300 }}>
+        <InputLabel id="demo-simple-select-autowidth-label">Category</InputLabel>
+
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={props.chosenCategoryId}
+          onChange={props.setChosenCategoryId}
+          disabled={props.disabled}
+          autoWidth
+          label="Category"
+        >
+          { categoryList }
+        </Select>
+    </FormControl>
+  )
 }
