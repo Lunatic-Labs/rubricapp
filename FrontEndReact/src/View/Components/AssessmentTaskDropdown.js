3e2824c217e69305378c40dc44b8b8a180ca2913
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-03-22 10:05:46 -0500

    Updated AssessmentTaskDropdown.js by adding whitespace and formatting!

--- FrontEndReact/src/View/Components/AssessmentTaskDropdown.js
+++ FrontEndReact/src/View/Components/AssessmentTaskDropdown.js
@@ -4,31 +4,33 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+
+
 export default function AssessmentTaskDropdown(props) {
-    var assessmentTaskList = [];
+  var assessmentTaskList = [];
 
-    props.assessmentTasks.map((assessmentTask) => {
-        return assessmentTaskList.push(
-          <MenuItem key={assessmentTask["assessment_task_id"]} value={assessmentTask["assessment_task_id"]}>
-            {assessmentTask["assessment_task_name"]}
-          </MenuItem>
-        )
-      })
+  props.assessmentTasks.map((assessmentTask) => {
+    return assessmentTaskList.push(
+      <MenuItem key={assessmentTask["assessment_task_id"]} value={assessmentTask["assessment_task_id"]}>
+        {assessmentTask["assessment_task_name"]}
+      </MenuItem>
+    );
+  });
 
-    return (
-        <FormControl sx={{ m: 3, minWidth: 300 }}>
-            <InputLabel id="demo-simple-select-autowidth-label">Assessment Task</InputLabel>
+  return (
+    <FormControl sx={{ m: 3, minWidth: 300 }}>
+      <InputLabel id="demo-simple-select-autowidth-label">Assessment Task</InputLabel>
 
-            <Select
-              labelId="demo-simple-select-autowidth-label"
-              id="demo-simple-select-autowidth"
-              value={props.chosenAssessmentId}
-              onChange={props.setChosenAssessmentId}
-              autoWidth
-              label="Assessment Task"
-            >
-              { assessmentTaskList }
-            </Select>
-        </FormControl>
-    )
+      <Select
+        labelId="demo-simple-select-autowidth-label"
+        id="demo-simple-select-autowidth"
+        value={props.chosenAssessmentId}
+        onChange={props.setChosenAssessmentId}
+        autoWidth
+        label="Assessment Task"
+      >
+        { assessmentTaskList }
+      </Select>
+    </FormControl>
+  )
 }
