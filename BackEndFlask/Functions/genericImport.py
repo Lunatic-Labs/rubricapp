795ad1dbf08292c760e643a56def0a42b7158e63
malloc-nbytes <zdhdev@yahoo.com>
2024-04-09 13:34:49 -0500

    Fixed issue where students were being added even if there was an error in the csv file

--- BackEndFlask/Functions/genericImport.py
+++ BackEndFlask/Functions/genericImport.py
@@ -11,6 +11,66 @@ from sqlalchemy import *
 import itertools
 import csv
 
+def __add_user(owner_id, course_id, first_name, last_name, email, role_id, lms_id):
+        """
+        Description
+        Adds the user to the DB based on the function parameters
+
+        Parameters
+        owner_id: int:  The user_id of the owner of the course.
+        course_id: int: The course_id of the course to add the users to.
+        first_name: str: first name of the user
+        last_name: str: last name of the user
+        email: str: email of the user
+        role_id: int: role ID of the user
+        lms_id: int: LMS ID of the user
+
+        Returns
+        None
+        """
+
+        user = get_user_by_email(email)
+
+        # If the user is not already in the DB.
+        if user is None:
+            helper_create_user(first_name, last_name, email, role_id, lms_id, owner_id)
+        else:
+            updated_user_first_name = user.first_name
+            if first_name != user.first_name:
+                updated_user_first_name = first_name
+
+            updated_user_last_name = user.last_name
+            if last_name != user.last_name:
+                updated_user_first_name = last_name
+
+            updated_user_lms_id = user.lms_id
+            if lms_id != user.lms_id:
+                updated_user_lms_id = lms_id
+
+            user_data = {
+                "first_name": updated_user_first_name,
+                "last_name": updated_user_last_name,
+                "lms_id": updated_user_lms_id,
+                "email": user.email,
+                "password": user.password,
+                "consent": user.consent,
+                "owner_id": user.owner_id,
+            }
+            replace_user(user_data, user.user_id)
+
+        user_id = get_user_user_id_by_email(email)
+        user_course = get_user_course_by_user_id_and_course_id(user_id, course_id)
+
+        if user_course is None:
+            create_user_course({
+                "user_id": user_id,
+                "course_id": course_id,
+                "role_id": role_id,
+            })
+        else:
+            set_inactive_status_of_user_to_active(user_course.user_course_id)
+
+
 def generic_csv_to_db(user_file: str, owner_id: int, course_id: int) -> None|str:
     """
     Description:
@@ -43,6 +103,11 @@ def generic_csv_to_db(user_file: str, owner_id: int, course_id: int) -> None|str
 
         # Renamed `reader` -> `roster`.
         roster: list[list[str]] = list(itertools.tee(csv.reader(student_csv))[0])
+
+        # For keeping students in a "queue" as we are parsing
+        # the file. During parsing, we add the relevant information
+        # to this list (first_name, last_name, email, role_id, lms_id).
+        students: list[tuple] = []
 
         for row in range(0, len(roster)):
             person_attribs: list[str] = roster[row]
@@ -80,46 +145,10 @@ def generic_csv_to_db(user_file: str, owner_id: int, course_id: int) -> None|str
             if not helper_verify_email_syntax(email):
                 raise SuspectedMisformatting
 
-            user = get_user_by_email(email)
+            students.append((first_name, last_name, email, role_id, lms_id))
 
-            # If the user is not already in the DB.
-            if user is None:
-                helper_create_user(first_name, last_name, email, role_id, lms_id, owner_id)
-            else:
-                updated_user_first_name = user.first_name
-                if first_name != user.first_name:
-                    updated_user_first_name = first_name
-
-                updated_user_last_name = user.last_name
-                if last_name != user.last_name:
-                    updated_user_first_name = last_name
-
-                updated_user_lms_id = user.lms_id
-                if lms_id != user.lms_id:
-                    updated_user_lms_id = lms_id
-
-                user_data = {
-                    "first_name": updated_user_first_name,
-                    "last_name": updated_user_last_name,
-                    "lms_id": updated_user_lms_id,
-                    "email": user.email,
-                    "password": user.password,
-                    "consent": user.consent,
-                    "owner_id": user.owner_id,
-                }
-                replace_user(user_data, user.user_id)
-
-            user_id = get_user_user_id_by_email(email)
-            user_course = get_user_course_by_user_id_and_course_id(user_id, course_id)
-
-            if user_course is None:
-                create_user_course({
-                    "user_id": user_id,
-                    "course_id": course_id,
-                    "role_id": role_id,
-                })
-            else:
-                set_inactive_status_of_user_to_active(user_course.user_course_id)
+        for first_name, last_name, email, role_id, lms_id in students:
+            __add_user(owner_id, course_id, first_name, last_name, email, role_id, lms_id)
 
         student_csv.close()
         delete_xlsx(user_file, is_xlsx)
