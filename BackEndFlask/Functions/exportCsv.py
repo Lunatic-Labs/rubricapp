8a674485110dc1e07092e24eb9011d6914475ddf
Aldo <aeveraespinoza@mail.lipscomb.edu>
2024-05-09 10:33:04 -0500

    removed a lot of useless commnets and made the code more readable

--- BackEndFlask/Functions/exportCsv.py
+++ BackEndFlask/Functions/exportCsv.py
@@ -53,8 +53,6 @@ class Catagories_csv(Enum):
     GOAL_ID = "Identifying the Goal"
     SYNTH = "Synthesizing"
 
-#testing needs to be intergrated
-#init for security has preverse structure
 def create_csv(at_name:str, file_name:str):
     """
     Description:
@@ -109,70 +107,4 @@ def create_csv(at_name:str, file_name:str):
                                     + [sfi_oc_data[0][j][1]]
                                     + ["SFI"]
                                     )
-
-    """ with app.app_context():
-        with open("./Functions/" + file_name, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile, quoting=csv.QUOTE_MINIMAL)
-            allAssessmentData = get_csv_data_by_at_name(at_name)
-            for entry in allAssessmentData:
-                at_type = ["Team"] if entry[Csv_locations.AT_TYPE.value] else ["Individual"]
-                for i in ["Analyzing", "Evaluating", "Forming Arguments (Structure)",
-                          "Forming Arguments (Validity)", "Identifying the Goal",
-                          "Synthesizing"]:
-                    oc = entry[Csv_locations.JSON.value][i]["observable_characteristics"]
-                    for j in oc:
-                        writer.writerow([entry[Csv_locations.AT_NAME.value]] 
-                                    + at_type
-                                    + [entry[Csv_locations.AT_COMPLETER.value]]
-                                    + [entry[Csv_locations.TEAM_NAME.value]]
-                                    + [entry[Csv_locations.FIRST_NAME.value]]
-                                    + [entry[Csv_locations.LAST_NAME.value]]
-                                    + [entry[Csv_locations.COMP_DATE.value].strftime("%m/%d/%Y")]
-                                    + [i]
-                                    )
-                    sfi = entry[Csv_locations.JSON.value][i]["suggestions"]
-                    for j in sfi:
-                        writer.writerow([entry[Csv_locations.AT_NAME.value]] 
-                                    + at_type
-                                    + [entry[Csv_locations.AT_COMPLETER.value]]
-                                    + [entry[Csv_locations.TEAM_NAME.value]]
-                                    + [entry[Csv_locations.FIRST_NAME.value]]
-                                    + [entry[Csv_locations.LAST_NAME.value]]
-                                    + [entry[Csv_locations.COMP_DATE.value].strftime("%m/%d/%Y")]
-                                    + [i]
-                                    )
- """
-
-"""
-{"Analyzing": {"comments": "ASDFASDFASDFASDFASDF", "description": 
-"Interpreted information to determine meaning and to extract relevant evidence", 
-"observable_characteristics":
- "101", "rating": 3, "rating_json": {"0": "No evidence", "1": "Inaccurately", "2": "", "3":
-   "With some errors", "4": "", "5": "Accurately"}, "suggestions": "00100"},
-    
-     
-      
-       
-        
-         
-          
- "Evaluating": 
-   {"comments": "ASDFJKL;", "description": "Determined the relevance and reliability of information 
-   that might be used to support the conclusion or argument", "observable_characteristics": "111", 
-   "rating": 1, "rating_json": {"0": "No evidence", "1": "Minimally", "2": "", "3": "Partially", "4": 
-   "", "5": "Extensively"}, "suggestions": "00000"}, "Forming Arguments (Structure)": {"comments": "", 
-   "description": "Made an argument that includes a claim (a position), supporting information, and 
-   reasoning.", "observable_characteristics": "0100", "rating": 5, "rating_json": {"0": "No evidence", 
-   "1": "Minimally", "2": "", "3": "Partially", "4": "", "5": "Completely"}, "suggestions": "1111111"}, 
-   "Forming Arguments (Validity)": {"comments": "", "description": "The claim, evidence, and reasoning 
-   were logical and consistent with broadly accepted principles.", "observable_characteristics": "10110", 
-   "rating": 4, "rating_json": {"0": "No evidence", "1": "Minimally", "2": "", "3": "Partially", "4": "", 
-   "5": "Fully"}, "suggestions": "11111"}, "Identifying the Goal": {"comments": "", "description": 
-   "Determined the purpose/context of the argument or conclusion that needed to be made", 
-   "observable_characteristics": "110", "rating": 2, "rating_json": {"0": "No evidence", "1": 
-   "Minimally", "2": "", "3": "Partially", "4": "", "5": "Completely"}, "suggestions": "1010"}, 
-   "Synthesizing": {"comments": "", "description": "Connected or integrated information to support an 
-   argument or reach a conclusion", "observable_characteristics": "101", "rating": 2, "rating_json": 
-   {"0": "No evidence", "1": "Inaccurately", "2": "", "3": "With some errors", "4": "", "5": "Accurately"}
-   , "suggestions": "10001"}, "comments": "", "done": true}
-"""
+                        
