02c74137ac460b7a224a8f0da7dac5b8069bc565
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-05-10 09:32:21 -0500

    Updated AdminAddAssessmentTask.js by converting snake case variable name to camel case!

--- FrontEndReact/src/View/Admin/Add/AddTask/AdminAddAssessmentTask.js
+++ FrontEndReact/src/View/Admin/Add/AddTask/AdminAddAssessmentTask.js
@@ -101,10 +101,10 @@ class AdminAddAssessmentTask extends Component {
     };
 
     handleTeams = (event) => {
-        const unit_of_assessment = event.target.value === 'true' ? true : false;
+        const unitOfAssessment = event.target.value === 'true' ? true : false;
 
         this.setState({
-            usingTeams: unit_of_assessment,
+            usingTeams: unitOfAssessment,
         });
     };
 
@@ -173,7 +173,6 @@ class AdminAddAssessmentTask extends Component {
             confirmCreateResource("AssessmentTask");
         }
     };
-
 
     hasErrors = () => {
         const { errors } = this.state;
@@ -295,7 +294,11 @@ class AdminAddAssessmentTask extends Component {
                                             sx={{ mb: 2 }}
                                             onChange={this.handleTeams}
                                         >
+<<<<<<< HEAD
                                             <FormControlLabel value={false} control={<Radio />} label="Individual Assessment" aria-label="addAssessmentInvididualAssessmentRadioOption"/>
+=======
+                                            <FormControlLabel value={false} control={<Radio />} label="Individual Assessment" />
+>>>>>>> ab3a2212 (Updated AdminAddAssessmentTask.js by converting snake case variable name to camel case!)
 
                                             <FormControlLabel value={true} control={<Radio />} label="Group Assessment" aria-label="addAssessmentGroupAssessmentRadioOption" />
                                         </RadioGroup>
