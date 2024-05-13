2c494e4a9edf14f31c8aac5b0444b35fa10c4a9e
aparriaran <aparriaran@mail.lipscomb.edu>
2024-05-13 10:20:16 -0500

    setting up test cases for AdminAddCustomRubric

---
+++ FrontEndReact/src/View/Admin/Add/AddCustomRubric/__tests__/AdminAddCustomRubric.test.js
@@ -0,0 +1,40 @@
+import { render, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Login from "../../../../Login/Login.js";
+
+import {
+    clickElementWithAriaLabel,
+    expectElementWithAriaLabelToBeInDocument,
+    changeElementWithAriaLabelWithInput,
+    expectElementWithAriaLabelToHaveErrorMessage,
+    clickFirstElementWithAriaLabel
+} from "../../../../../testUtilities.js";
+
+import {
+    demoAdminPassword
+} from "../../../../../App.js";
+
+
+
+var lb = "loginButton";
+var ei = "emailInput";
+var pi = "passwordInput";
+
+
+
+test("NOTE: Tests 1-?? will not pass if Demo Data is not loaded!", () => {
+    expect(true).toBe(true);
+});
+
+
+test("AdminAddCustomRubric.test.js Test 1: ", async () => {
+    render(<Login />);
+
+    changeElementWithAriaLabelWithInput(ei, "demoadmin02@skillbuilder.edu");
+
+    changeElementWithAriaLabelWithInput(pi, demoAdminPassword);
+
+    clickElementWithAriaLabel(lb);
+
+    
+});
