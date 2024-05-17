04eab38ebfbc0fe8740c6c20d2c0881f32f81aeb
aparriaran <aparriaran@mail.lipscomb.edu>
2024-05-16 19:22:15 -0500

    updated the completed assessments tab

--- BackEndFlask/models/queries.py
+++ BackEndFlask/models/queries.py
@@ -689,7 +689,9 @@ def get_completed_assessment_by_user_id(course_id, user_id):
         CompletedAssessment.rating_observable_characteristics_suggestions_data,
         CompletedAssessment.done,
         UserCourse.course_id,
-        AssessmentTask.assessment_task_name
+        AssessmentTask.assessment_task_name,
+        AssessmentTask.rubric_id,
+        Team.team_name
     ).join(
         UserCourse, UserCourse.user_id == CompletedAssessment.user_id
     ).join(
@@ -698,6 +700,9 @@ def get_completed_assessment_by_user_id(course_id, user_id):
         CompletedAssessment.user_id == user_id,
         UserCourse.course_id == course_id,
         AssessmentTask.assessment_task_id == CompletedAssessment.assessment_task_id
+    ).join(
+        Team,
+        Team.team_id == CompletedAssessment.team_id
     ).all()
 
     return complete_assessments
