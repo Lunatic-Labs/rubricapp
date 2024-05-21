c229f38b5b3f2caacc13b68c37bc2c6da0c399e7
aparriaran <aparriaran@mail.lipscomb.edu>
2024-05-21 09:50:48 -0500

    Save progress of TADashboardTests.

--- FrontEndReact/src/View/Student/__tests__/TADashboard.test.js
+++ FrontEndReact/src/View/Student/__tests__/TADashboard.test.js
@@ -26,6 +26,7 @@ var catb = "completedAssessmentTasksButton";
 var catvib = "completedAssessmentTasksViewIconButton";
 var vatit = "viewAssessmentTaskInstructionsTitle";
 var mhbb = "mainHeaderBackButton";
+var vaticb = "viewAssessmentTaskInstructionsContinueButton";
 
 
 
@@ -34,7 +35,7 @@ test("NOTE: Tests 1-3 will not pass if Demo Data is not loaded!", () => {
 });
 
 
-test("TADashboard.test.js Test 1: Should render both assessment tables if valid TA information is input to login", async () => {
+test("TADashboard.test.js Test 1: Should render both assessment tables if valid TA information is input to login.", async () => {
     render(<Login />);
 
     changeElementWithAriaLabelWithInput(ei, "demotainstructor03@skillbuilder.edu");
@@ -57,7 +58,7 @@ test("TADashboard.test.js Test 1: Should render both assessment tables if valid 
 });
 
 
-test("TADashboard.test.js Test 2: Should render the completed assessment task page if the complete assessment task button is clicked", async () => {
+test("TADashboard.test.js Test 2: Should render the completed assessment task page if the complete assessment task button is clicked.", async () => {
     render(<Login />);
 
     await waitFor(() => {
@@ -82,7 +83,7 @@ test("TADashboard.test.js Test 2: Should render the completed assessment task pa
 });
 
 
-test("TADashboard.test.js Test 3: Should render the view completed assessment task page if the view button is clicked", async () => {
+test("TADashboard.test.js Test 3: Should render the view completed assessment task page if the view button is clicked.", async () => {
     render(<Login />);
 
     await waitFor(() => {
@@ -132,7 +133,7 @@ test("TADashboard.test.js Test 4: Should render to the course dashboard when the
 });
 
 
-test("TADashboard.test.js Test 5: Should render to the my Assessment Tasks and Completed Assessments page when the back button is clicked on  page.", async () => {
+test("TADashboard.test.js Test 5: Should render to the my Assessment Tasks and Completed Assessments page when the back button is clicked on page.", async () => {
     render(<Login />);
 
     await waitFor(() => {
@@ -145,13 +146,58 @@ test("TADashboard.test.js Test 5: Should render to the my Assessment Tasks and C
         expectElementWithAriaLabelToBeInDocument(matt);
 
         expectElementWithAriaLabelToBeInDocument(catt);
-    });
 
-    clickElementWithAriaLabel(mhbb);
+        clickElementWithAriaLabel(catb);
+    });
     
     await waitFor(() => {
         setTimeout(() => {
-            expectElementWithAriaLabelToBeInDocument(ct);
+            expectElementWithAriaLabelToBeInDocument(vatit);
+
+            clickElementWithAriaLabel(mhbb);
+        }, 3000);
+    });
+    
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToBeInDocument(matt);
+
+            expectElementWithAriaLabelToBeInDocument(catt);
+        }, 3000);
+    });
+});
+
+
+test("TADashboard.test.js Test 6: Should render to the Feedback page of the Assessment Task when the continue button is selected.", async () => {
+    render(<Login />);
+
+    await waitFor(() => {
+        expectElementWithAriaLabelToBeInDocument(ct);
+    });
+
+    clickFirstElementWithAriaLabel(vcib);
+
+    await waitFor(() => {
+        expectElementWithAriaLabelToBeInDocument(matt);
+
+        expectElementWithAriaLabelToBeInDocument(catt);
+
+        clickElementWithAriaLabel(catb);
+    });
+    
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToBeInDocument(vatit);
+
+            clickElementWithAriaLabel(mhbb);
+        }, 3000);
+    });
+    
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToBeInDocument(matt);
+
+            expectElementWithAriaLabelToBeInDocument(catt);
         }, 3000);
     });
 });
