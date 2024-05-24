a4a5db1f3b1a4e95607f8a4421b6bcfcf7989c05
aparriaran <aparriaran@mail.lipscomb.edu>
2024-05-24 14:03:43 -0500

    removed an import.

--- FrontEndReact/src/View/Components/SendNotification.js
+++ FrontEndReact/src/View/Components/SendNotification.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import { TextField } from "@mui/material";
