f7f2190201edde1f27fcc10378972ee26c80aa74
aparriaran <aparriaran@mail.lipscomb.edu>
2024-05-29 11:37:29 -0500

    Added a jest test for the helperText Error in the Add Message prompt when it is left empty

--- FrontEndReact/src/View/Admin/View/ViewCompleteAssessmentTasks/__tests__/AdminViewCompleteAssessmentTasks.test.js
+++ FrontEndReact/src/View/Admin/View/ViewCompleteAssessmentTasks/__tests__/AdminViewCompleteAssessmentTasks.test.js
@@ -6,7 +6,8 @@ import {
     clickElementWithAriaLabel,
     expectElementWithAriaLabelToBeInDocument,
     changeElementWithAriaLabelWithInput,
-    clickFirstElementWithAriaLabel
+    clickFirstElementWithAriaLabel,
+    expectElementWithAriaLabelToHaveErrorMessage
 } from "../../../../../testUtilities.js";
 
 import {
@@ -490,4 +491,53 @@ test("AdminViewCompleteAssessmentTasks.test.js Test 9: Should save the selection
             expectElementWithAriaLabelToBeInDocument(cbs);
         }, 3000);
     });
+});
+
+
+test("AdminViewCompleteAssessmentTasks.test.js Test 10: Should provide a HelperText error when the Add Message box is left empty", async () => {
+    render(<Login />);
+
+    await waitFor(() => {
+        expectElementWithAriaLabelToBeInDocument(ct);
+    });
+
+    clickFirstElementWithAriaLabel(vcib);
+
+    await waitFor(() => {
+        expectElementWithAriaLabelToBeInDocument(rt);
+    });
+
+    clickElementWithAriaLabel(at);
+
+    await waitFor(() => {
+        expectElementWithAriaLabelToBeInDocument(adt);
+    });
+
+    await waitFor(() => {
+        setTimeout(() => {
+            clickFirstElementWithAriaLabel(vcib);
+        }, 3000);
+    });
+
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToBeInDocument(vcatt);
+
+            clickElementWithAriaLabel(vcasnb);
+        }, 3000);
+    });
+
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToBeInDocument(vcaamt);
+
+            clickElementWithAriaLabel(ampsnb);
+        }, 3000);
+    });
+
+    await waitFor(() => {
+        setTimeout(() => {
+            expectElementWithAriaLabelToHaveErrorMessage(vcaamt,"Notification Message cannot be empty");
+        }, 3000);
+    })
 });
