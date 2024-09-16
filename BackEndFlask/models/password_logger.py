7c27ffab52ad05b99c01d903bf46871612d0a357
Mr615-TN <tneema501@gmail.com>
2024-09-16 14:46:56 -0500

    working on passwordLogger.py, but probably need to move it somewhere else and probably integrate it somewhere else

---
+++ BackEndFlask/models/password_logger.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from logger import Logger
+
+app = Flask(__name__)
+
+passwordResetLogger = Logger("passwordResetLogger", logfile="/BackEndFlask/logs/reset_passwords.log")
+
+@app.route('/reset-password', methods=['POST'])
+def reset_password():
+    email=request.form.get("email")
