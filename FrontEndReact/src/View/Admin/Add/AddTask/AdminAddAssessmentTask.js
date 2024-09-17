74c7377612dd02f7ddc681ef0399990491dbe3f1
AdhamA <Adhamalammuri@gmail.com>
2024-09-17 09:46:36 -0500

    numberOfTeams in add assessment task now must be an integer greater than 0 on the frontend

--- FrontEndReact/src/View/Admin/Add/AddTask/AdminAddAssessmentTask.js
+++ FrontEndReact/src/View/Admin/Add/AddTask/AdminAddAssessmentTask.js
@@ -98,6 +98,19 @@ class AdminAddAssessmentTask extends Component {
     handleChange = (e) => {
         const { id, value } = e.target;
 
+        if (id === 'numberOfTeams') {
+            const regex = /^[1-9]\d*$/;
+            if (value !== '' && !regex.test(value)) {
+                this.setState({
+                    errors: {
+                        ...this.state.errors,
+                        [id]: 'Number of teams must be greater than zero',
+                    },
+                });
+                return;
+            }
+        }
+
         this.setState({
             [id]: value,
             errors: {
@@ -142,6 +155,18 @@ class AdminAddAssessmentTask extends Component {
         var assessmentTask = state.assessmentTask;
         var chosenCourse = state.chosenCourse;
 
+        if (usingTeams && !chosenCourse["use_fixed_teams"]) {
+            if (!numberOfTeams || !/^[1-9]\d*$/.test(numberOfTeams)) {
+                this.setState({
+                    errors: {
+                        ...this.state.errors,
+                        numberOfTeams: 'Number of teams must be greater than zero',
+                    },
+                });
+                return;
+            }
+            
+        }
         if (taskName === '' || timeZone === '' || roleId === '' || rubricId === '' || notes === '') {
             this.setState({
                 errors: {
@@ -331,10 +356,16 @@ class AdminAddAssessmentTask extends Component {
                                             name="newPassword"
                                             variant='outlined'
                                             label="Number of teams"
+                                            value={this.state.numberOfTeams}
                                             error={!!errors.numberOfTeams}
+                                            helperText={errors.numberOfTeams}
                                             onChange={this.handleChange}
                                             required
-                                            type={"number"}
+                                            type={"text"}
+                                            inputProps={{ 
+                                                pattern: "[1-9][0-9]*", 
+                                                inputMode: "numeric"
+                                            }}
                                             sx={{ mb: 2 }}
                                         />
                                     }
