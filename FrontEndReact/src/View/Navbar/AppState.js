50834af179dfa2f7da9dac5e057a639eb16e9819
rtkells <rtkells@mail.lipscomb.edu>
2024-09-24 16:44:43 -0500

    Added formatting whitespace in AppState.js

--- FrontEndReact/src/View/Navbar/AppState.js
+++ FrontEndReact/src/View/Navbar/AppState.js
@@ -313,6 +313,7 @@ class AppState extends Component {
                             user: null,
                             addUser: null
                         });
+                        
                     } else if (resource === "Course") {
                         this.setState({
                             activeTab: "Courses",
