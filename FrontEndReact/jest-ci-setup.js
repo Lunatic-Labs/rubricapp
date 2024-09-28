89d616b2e11694a76b48771c3e61661789b5dc18
jcallison1 <152124968+jcallison1@users.noreply.github.com>
2024-09-28 11:06:53 -0500

    Try to fix timeout issues

---
+++ FrontEndReact/jest-ci-setup.js
@@ -0,0 +1,8 @@
+const oldSetTimeout = global.setTimeout;
+
+global.setTimeout = function(callback, ms, ...args) {
+	if (ms === 3000) ms = 10_000;
+	
+	return oldSetTimeout(callback, ms, ...args);
+};
+
