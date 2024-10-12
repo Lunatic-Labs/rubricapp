47cb8840413042777d32b1f78e538c7c59d67fb4
Tanish Neema <tneema501@gmail.com>
2024-10-11 20:34:42 -0500

    working on AdminDeleteTeam.js

--- FrontEndReact/src/View/Admin/Add/AddTeam/AdminDeleteTeam.js
+++ FrontEndReact/src/View/Admin/Add/AddTeam/AdminDeleteTeam.js
@@ -7,3 +7,53 @@ import { genericResourcePOST, genericResourcePUT, genericResourceGET } from "../
 import { FormControl, MenuItem, InputLabel, Select } from "@mui/material";
 import Cookies from 'universal-cookie';
 import FormHelperText from '@mui/material/FormHelperText';
+
+class AdminDeleteTeam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: null,
+      errorMessage: null,
+      validMessage: "",
+      editTeam: false,
+      observerId: '',
+      teamName: "",
+      users: null,
+
+      errors: {
+        teamName: "",
+        observerId: '',
+      } 
+    }
+    this.handleSelect = (event) => {
+      this.setState({
+        observerId: event.target.value,
+      });
+    };
+  }
+
+  componentDidMount() {
+    var navbar = this.props.navbar; 
+
+    var state = navbar.state; 
+
+    var team = state.team; 
+
+    var deleteTeam = state.deleteTeam;
+
+
+    genericResourceGET(
+      `/user?course_id=${this.props.navbar.state.chosenCourse["course_id"]}&role_id=4`, 
+      "users", this);
+        
+    if (team !== null && !deleteTeam) {
+      this.setState({
+        teamName: null,
+
+        observerId: null,
+
+        editTeam: false,
+      });
+    }
+  }
+}
