deeac057b137baf2d85c6985f35124922599f27a
Zara Malik <zara@macbook-pro-185.bisonnet.lipscomb.edu>
2024-11-04 14:34:52 -0600

    auth decorators

--- BackEndFlask/models/suggestions.py
+++ BackEndFlask/models/suggestions.py
@@ -42,7 +42,8 @@ def create_suggestion(suggestion):
 
     return new_suggestion
 
-
+# Do I need to make another error log, because it wasn't there before.
+# @error_log
 def replace_suggestion(suggestion, id):
     one_suggestion = SuggestionsForImprovement.query.filter_by(suggestion_id=id).first()
     
