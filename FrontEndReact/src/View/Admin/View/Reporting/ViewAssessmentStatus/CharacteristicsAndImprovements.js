fc9e3bc4e8b5979966f6dbbfb1d3a0e1990990ad
Hudah <hudahn424@gmail.com>
2024-11-05 14:49:47 -0600

    unused variables

--- FrontEndReact/src/View/Admin/View/Reporting/ViewAssessmentStatus/CharacteristicsAndImprovements.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewAssessmentStatus/CharacteristicsAndImprovements.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { BarChart, CartesianGrid, XAxis, YAxis, Bar, LabelList, ResponsiveContainer, Tooltip } from 'recharts';
 
 const truncateText = (text, limit = 15) => {
