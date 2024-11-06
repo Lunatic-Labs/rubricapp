ed418147bb81a5b89ade258a1534c7a413616333
malloc-nbytes <zdhdev@yahoo.com>
2024-11-06 15:53:17 -0600

    Started work on implementation for the API call for locking ATs.

--- FrontEndReact/src/View/Admin/View/ViewAssessmentTask/ViewAssessmentTasks.js
+++ FrontEndReact/src/View/Admin/View/ViewAssessmentTask/ViewAssessmentTasks.js
@@ -159,6 +159,8 @@ class ViewAssessmentTasks extends Component {
         var rubricNames = adminViewAssessmentTask.rubricNames;
         var assessmentTasks = adminViewAssessmentTask.assessmentTasks;
 
+        const lockStatus = this.state.lockStatus;
+
         let assessmentTasksToDueDates = {};
 
         for(let index = 0; index < assessmentTasks.length; index++) {
@@ -418,26 +420,6 @@ class ViewAssessmentTasks extends Component {
                         )
                     }
                 }
-            },
-            {
-                name: "lock",
-                label: "Lock",
-                options: {
-                    filter: false,
-                    sort: false,
-                    setCellHeaderProps: () => { return { align:"center", width:"70px", className:"button-column-alignment"}},
-                    setCellProps: () => { return { align:"center", width:"70px", className:"button-column-alignment"} },
-                    customBodyRender: (assessmentTaskId) => {
-                        return(
-                            <>
-                                <IconButton aria-label='unlock'>
-                                    <LockOpenIcon />
-                                </IconButton>
-                            </>
-                        )
-                    }
-                }
-
             },
             {
                 name: "assessment_task_id",
