ffa0a5756e711f8db3dd302e7265214a568435bf
aldo <aldo@cool.com>
2024-11-14 15:36:35 -0600

    Refactored to a manner that is eaiser to change formats in the future for similar exports.

--- BackEndFlask/Functions/exportCsv.py
+++ BackEndFlask/Functions/exportCsv.py
@@ -4,10 +4,8 @@
 # and returns to a csv file to a customer.
 #
 # NOTE:
-#   the current way to write out things is as follows:
-# AT_name, RN(AT_type, AT_completer), TeamName, IndividualName, CompDate, Category, datapoint
-#            /             \                                                            |
-#        unitofasess...     roleid                                                     rating,oc,sfi
+#   The Template method pattern was used since both exports deal with similar related data fetches
+#   from the data base but just handle formating differently. This is hopefully expandable.
 #----------------------------------------------------------------------------------------------------
 import csv
 import io
@@ -45,7 +43,7 @@ def rounded_hours_difference(completed: datetime, seen: datetime) -> int:
 
     return int(hours_remainder[0]) if hours_remainder[1] < 30.0 else int(hours_remainder[0]) + 1
 
-class Csv_data(Enum):
+class Csv_Data(Enum):
     """
     Description:
     Locations associated to where they are in the json file.
@@ -82,131 +80,160 @@ class Csv_data(Enum):
 
     JSON = 13
 
+class Csv_Creation(ABC):
+    """
+    Description: Abstract class that leads to the creation of a csv formated string.
+    """
 
+    def __init__(self, at_id) -> None:
+        """
+        Parameters:
+        at_id: <class 'int'> 
+        """
+        self._at_id = at_id
+        self._csv_file = io.StringIO()
+        self._writer = csv.writer(self._csv_file, delimiter='\t')
+        self._completed_assessment_data = None
+        self._oc_sfi_data = None
+        self._is_teams = False
+        self._singular_data = None
 
-"""
+    def return_csv_str(self) -> str:
+        """
+        Description: Returns a csv formated string.
 
+        Return:
+        <class 'str'>
 
+        Note: Once called the memory is freed in the instance, so save it. Think of this as a singleton that
+        should preform a task and be deleted.
+        """
 
+        # Writting a common identifying data.
+        self._writer.writerow(["Course Name"])
+        self._writer.writerow([get_course_name_by_at_id(self._at_id)])
+        self._writer.writerow([' '])
 
+        
+        # List of dicts: Each list is another individual in the AT and the dict is there related data. 
+        self._completed_assessment_data = get_csv_data_by_at_id(self._at_id)
 
-"""
+        if len(self._completed_assessment_data) == 0:
+            return self._csv_file.getvalue()
+        
+        self._singular = self._completed_assessment_data[0]
+        self._is_teams = False if self._singular[Csv_Data.TEAM_NAME.value] == None else True
 
-def create_ratings(at_id:int) -> str:
-     with app.app_context():
-        with io.StringIO() as csvFile:
-            writer = csv.writer(csvFile, delimiter='\t')
+        self._format()
 
-            writer.writerow(["Course Name"])
-            writer.writerow([get_course_name_by_at_id(at_id)])
-            writer.writerow([''])
+        return self._csv_file.getvalue()
+    
+    def __del__(self) -> None:
+        """
+        Description: Freeing resources.
+        """
+        self._csv_file.close()
 
-           # List of dicts: Each list is another individual in the AT and the dict is there related data. 
-            completed_assessment_data = get_csv_data_by_at_id(at_id)
+    @abstractmethod
+    def _format(self) -> None:
+        pass
 
-            if len(completed_assessment_data) == 0:
-                return csvFile.getvalue()
+class Ratings_Csv(Csv_Creation):
+    """
+    Description: Singleton that creates a csv string of ratings.
+    """
+    def __init__(self, at_id:int) -> None:
+        """
+        Parameters:
+        at_id: <class 'int'> 
+        """
+        super().__init__(at_id)
+    
+    def _format(self) -> None:
+        """
+        Description: Formats the data in the csv string.
+        Exceptions: None except what IO can rise.
+        """
+        column_name = ["First Name"] + ["Last Name"] if not self._is_teams else ["Team Name"]
 
-            singular = completed_assessment_data[0]
+        column_name += [i for i in self._singular[Csv_Data.JSON.value] if (i != "done" and i !="comments")]
 
-            is_teams = False if singular[Csv_data.TEAM_NAME.value] == None else True
+        column_name += ["Lag Time"]
 
-            column_name = ["First Name"] + ["Last Name"] if not is_teams else ["Team Name"]
+        self._writer.writerow(column_name)
 
-            column_name += [i for i in singular[Csv_data.JSON.value] if (i != "done" and i !="comments")]
+        row_info = None
 
-            column_name += ["Lag Time"]
+        # Notice that in the list comphrehensions done and comments are skiped since they are categories but dont hold relavent data.
+        for individual in self._completed_assessment_data:
 
-            writer.writerow(column_name)
+            row_info = [individual[Csv_Data.FIRST_NAME.value]] + [individual[Csv_Data.LAST_NAME.value]] if not self._is_teams else [individual[Csv_Data.TEAM_NAME.value]]
 
-            row_info = None
-
-            for individual in completed_assessment_data:
-
-                row_info = [individual[Csv_data.FIRST_NAME.value]] + [individual[Csv_data.LAST_NAME.value]] if not is_teams else [individual[Csv_data.TEAM_NAME.value]]
-
-                row_info += [individual[Csv_data.JSON.value][category]["rating"] for category in individual[Csv_data.JSON.value] if (category != "done" and category !="comments")]
+            row_info += [individual[Csv_Data.JSON.value][category]["rating"] for category in individual[Csv_Data.JSON.value] if (category != "done" and category !="comments")]
                 
-                lag = [" "]
-                try:
-                    # Possible that a particular individual has not yet seen so its a Nonetype in the backend.
-                    lag = [rounded_hours_difference(individual[Csv_data.COMP_DATE.value], individual[Csv_data.LAG_TIME.value])]
-                except TypeError:
-                    pass
+            lag = [" "]
+            try:
+                # Possible that a particular individual has not yet seen so its a Nonetype in the backend.
+                lag = [rounded_hours_difference(individual[Csv_Data.COMP_DATE.value], individual[Csv_Data.LAG_TIME.value])]
+            except TypeError:
+                pass
 
-                row_info += lag
-                writer.writerow(row_info) 
+            row_info += lag
+            self._writer.writerow(row_info)
 
-            return csvFile.getvalue()
-
-
-def create_ocs_sfis_csv(at_id: int) -> str:
+class Ocs_Sfis_Csv(Csv_Creation):
     """
-    Description:
-    Creates a string that has properly formated csv data for
-    Ocs and sfis according to client specifiications.
-
-    Parameters:
-    at_id: <class 'int'> (assessment_task_id)
-
-    Returns:
-    <class 'str'>
-
-    Exceptions:
-    None
+    Description: Singleton that creates a csv string of ratings.
     """
+    def __init__(self, at_id: int) -> None:
+        """
+        Parameters:
+        at_id: <class 'int'> 
+        """
+        super().__init__(at_id)
+        self.__checkmark = "✔"
+        self.__crossmark = " "
 
+    def _format(self) -> None:
+        """
+        Description: Formats the data in the csv string.
+        Exceptions: None except what IO can rise.
+        """
+        # Writing out in category chuncks. 
+        for category in self._singular[Csv_Data.JSON.value]:
+            if category == "done" or category == "comments": # Yes those two are "categories" at least from how the data is pulled.
+                continue
+
+            headers = ["First Name"] + ["Last Name"] if not self._is_teams else ["Team Name"]
+
+            oc_sfi_per_category = get_csv_categories(self._singular[Csv_Data.RUBRIC_ID.value],
+                                                    self._singular[Csv_Data.USER_ID.value],
+                                                    self._singular[Csv_Data.TEAM_ID.value],
+                                                    self._at_id, category)
+
+            headers += ["OC:" + i[0] for i in oc_sfi_per_category[0]] + ["SFI:" + i[0] for i in oc_sfi_per_category[1]]                
+
+            self._writer.writerow([category])
+            self._writer.writerow(headers)
+
+            # Writing the checkmarks.
+            for individual in self._completed_assessment_data:
+                respective_ocs_sfis = [individual[Csv_Data.JSON.value][category]["observable_characteristics"], 
+                                       individual[Csv_Data.JSON.value][category]["suggestions"]]
+                    
+                row = None
+                if not self._is_teams: row = [individual[Csv_Data.FIRST_NAME.value]] + [individual[Csv_Data.LAST_NAME.value]]
+                else: row = [individual[Csv_Data.TEAM_NAME.value]]
+
+                for bits in respective_ocs_sfis:
+                    row += [self.__checkmark if i == "1" else self.__crossmark for i in bits]
+                    
+                self._writer.writerow(row)
+            self._writer.writerow([''])
+        
+
+def create_ratings(at_id:int) ->str:
     with app.app_context():
-        with io.StringIO() as csvFile:
-            writer = csv.writer(csvFile, delimiter='\t')
-
-            writer.writerow(["Course Name"])
-            writer.writerow([get_course_name_by_at_id(at_id)])
-            writer.writerow([''])
-
-            checkmark = "✔"
-            crossmark = " "
-
-           # List of dicts: Each list is another individual in the AT and the dict is there related data. 
-            completed_assessment_data = get_csv_data_by_at_id(at_id)
-
-            if len(completed_assessment_data) == 0:
-                return csvFile.getvalue()
-
-            singular = completed_assessment_data[0]
-
-            is_teams = False if singular[Csv_data.TEAM_NAME.value] == None else True
-
-            # Writing out in category chuncks. 
-            for category in singular[Csv_data.JSON.value]:
-                if category == "done" or category == "comments": # Yes those two are "categories" at least from how the data is pulled.
-                        continue
-
-                headers = ["First Name"] + ["Last Name"] if not is_teams else ["Team Name"]
-
-                oc_sfi_per_category = get_csv_categories(singular[Csv_data.RUBRIC_ID.value],
-                                        singular[Csv_data.USER_ID.value],
-                                        singular[Csv_data.TEAM_ID.value],
-                                        at_id, category)
-
-                headers += ["OC:" + i[0] for i in oc_sfi_per_category[0]] + ["SFI:" + i[0] for i in oc_sfi_per_category[1]]                
-
-                writer.writerow([category])
-                writer.writerow(headers)
-
-                # Writing the checkmarks.
-                for individual in completed_assessment_data:
-                    respective_ocs_sfis = [individual[Csv_data.JSON.value][category]["observable_characteristics"], 
-                                           individual[Csv_data.JSON.value][category]["suggestions"]]
-                    
-                    row = None
-                    if not is_teams: row = [individual[Csv_data.FIRST_NAME.value]] + [individual[Csv_data.LAST_NAME.value]]
-                    else: row = [individual[Csv_data.TEAM_NAME.value]]
-
-                    for bits in respective_ocs_sfis:
-                        row += [checkmark if i == "1" else crossmark for i in bits]
-                    
-                    writer.writerow(row)
-                writer.writerow([''])
-
-            return csvFile.getvalue()
+        answer = Ocs_Sfis_Csv(at_id)
+        value = answer.return_csv_str()
+        return value
