117281015dcb0138362be7efd487545434fc7dfb
aldo <aldo@cool.com>
2024-12-01 16:09:47 -0600

    created the start of the buttons but they need to be moved out of here and placed somewhere cleaner

--- FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewRatingsHeader.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewRatingsHeader.js
@@ -1,12 +1,35 @@
 import * as React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import AssessmentTaskDropdown from '../../../../Components/AssessmentTaskDropdown';
-import { parseAssessmentIndividualOrTeam } from '../../../../../utility';
+import { genericResourceGET, parseAssessmentIndividualOrTeam } from '../../../../../utility';
 
 
 
 export default function ViewRatingsHeader(props) {
   var assessmentIsTeam = parseAssessmentIndividualOrTeam(props.assessmentTasks);
+
+  const handleRatingsExport = () => {
+    let promise = genericResourceGET(
+      //fix the route name
+      `/SpecifiyROute?assessment_task_id=${props.chosenAssessmentId}`,
+      this,
+    )
+
+    promise.then(result => {
+      if (result !== undefined && result.errorMessage === null) {
+        //These next three vars need to be fixed look at the other export button.
+        var assessmentName = 'ToBeFiguredout';
+    
+        var newExportButtonJSON = 'Export Ratings';
+    
+        newExportButtonJSON[assessmentName] = 'Export Ratings';
+    
+        this.setState({
+          downloadedAssessment: assessmentName,
+          exportButtonId: newExportButtonJSON
+        });                }
+    });
+    }
 
   return (
     <Box>
@@ -23,6 +46,28 @@ export default function ViewRatingsHeader(props) {
               { assessmentIsTeam[props.chosenAssessmentId] ? "Team Assignment" : "Individual Assignment" }
             </Typography>
           }
+
+          <Box sx={{display:"flex"}}>
+            <Button
+                className='primary-color'
+                variant='contained' 
+                aria-label='Export Ratings'
+                onClick={handleRatingsExport}
+            >
+                Export Ratings
+            </Button>
+          </Box>
+          <Box sx={{display:"flex", gap:'20px'}}>
+            <Button
+                className='primary-color'
+                variant='contained' 
+                aria-label='Export Ratings'
+                onClick={handleRatingsExport}
+            >
+                Export Comments
+            </Button>
+          </Box>
+
         </Box>
       </Box>
     </Box>
