809b1a3561ddaaed3b9f1bd789df077f7ef34cb5
malloc-nbytes <zdhdev@yahoo.com>
2025-01-10 10:18:03 -0600

    added view CATS button back

--- FrontEndReact/src/View/Student/View/CompletedAssessmentTask/ViewCompletedAssessmentTasks.js
+++ FrontEndReact/src/View/Student/View/CompletedAssessmentTask/ViewCompletedAssessmentTasks.js
@@ -144,6 +144,12 @@ class ViewCompletedAssessmentTasks extends Component {
                                   completedAssessments,
                                   { readOnly: true, skipInstructions: true }
                               );
+                              this.props.navbar.setAssessmentTaskInstructions(
+                                  assessmentTasks,
+                                  atId,
+                                  completedAssessments,
+                                  { readOnly: true, skipInstructions: true }
+                              );
                                       }}
                                     aria-label="completedAssessmentTasksViewIconButton"
                                 >
