f77e50fbb5b0224b3b9eab7232d8480a0de39bf9
malloc-nbytes <zdhdev@yahoo.com>
2025-02-10 16:42:57 -0600

    add startup thread to wsgi.py

--- BackEndFlask/wsgi.py
+++ BackEndFlask/wsgi.py
@@ -2,6 +2,9 @@
 from core import app
 import os
 
+import threading
+from Functions.threads import email_checker_thread
+
 if __name__ == "__main__":
     core.config.rubricapp_running_locally = False
 
