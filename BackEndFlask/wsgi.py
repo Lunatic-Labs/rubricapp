12a626e930597ddf065c140adca1158be17e3e25
malloc-nbytes <zdhdev@yahoo.com>
2025-02-17 15:26:37 -0600

    Main code for checking validations in done

--- BackEndFlask/wsgi.py
+++ BackEndFlask/wsgi.py
@@ -2,9 +2,6 @@
 from core import app
 import os
 
-import threading
-from Functions.threads import email_checker_thread
-
 if __name__ == "__main__":
     core.config.rubricapp_running_locally = False
 
