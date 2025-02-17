f29a0cab1dd96e42d2113a110c42479952622a6f
malloc-nbytes <zdhdev@yahoo.com>
2025-02-17 15:26:37 -0600

    Main code for checking validations in done

--- BackEndFlask/Functions/threads.py
+++ BackEndFlask/Functions/threads.py
@@ -68,38 +68,65 @@ def validate_pending_emails():
             print(f'failed to run thread {e}')
 =======
 import time
+
 from models.email_validation import *
-from models.user import get_user
-from models.utility import check_bounced_emails
 
-def email_checker_thread():
-    timeout = 2
+def spawn_thread(f, *args, **kwargs):
+    threading.Thread(
+        target=f,
+        args=args,
+        kwargs=kwargs,
+        daemon=True
+    ).start()
 
-    while True:
-        with app.app_context():
-            try:
-                print("Fetching emails to check")
-                emails_to_check = get_emails_need_checking()
+def validate_pending_emails():
+    """
+    The purpose of this function is to be the entrypoint of a thread
+    that gets all entries in the EmailValidation table with the
+    status of 'pending'. It will then search for any bounced emails
+    and send an email to the the person that added that user.
+    """
 
-                if emails_to_check:
-                    print(f"Found {len(emails_to_check)} emails to check.")
+    # Imports are placed here to handle circular dependencies.
+    from models.user import get_user
+    from models.utility import check_bounced_emails
 
-                    oldest_time = emails_to_check[0].user.last_update
-                    data = {}
+    time.sleep(7)
 
-                    for email_obj in emails_to_check:
-                        if email_obj.user.last_update < oldest_time:
-                            oldest_time = email_obj.user.last_update
-                        owner_email = get_user(email_obj.user.owner_id).email
-                        user_email = email_obj.user.email
-                        if owner_email not in data:
-                            data[owner_email] = [user_email]
-                        else:
-                            data[owner_email].append(email_obj.user.email)
-                    print(data)
-                    # check_bounced_emails(data, oldest_time)
+    with app.app_context():
+        try:
+            print("Fetching emails to check")
+            emails_to_check = get_emails_need_checking()
 
-                time.sleep(timeout)
+            if emails_to_check:
+                oldest_time, all_pending_emails, data = (
+                    emails_to_check[0].user.last_update, [], {},
+                )
+
+                for email_obj in emails_to_check:
+                    if email_obj.user.last_update < oldest_time:
+                        oldest_time = email_obj.user.last_update
+                    owner_email = get_user(email_obj.user.owner_id).email
+                    user_email = email_obj.user.email
+                    data[user_email] = owner_email
+                    all_pending_emails.append(user_email)
+
+                print(data)
+
+                # Regardless of if we find any bounces or not, we need
+                # to update these.
+                mark_emails_as_checked(all_pending_emails)
+
+                bounced = check_bounced_emails(oldest_time)
+
+                if bounced is None:
+                    return
+
+                for bounce in bounced:
+                    # TODO: Find solution if the student's email in the message is not there.
+                    #       Will this ever happen?
+                    if bounce["to"]:
+                        send_bounced_email_notification(data[bounce['to']], bounce['msg'], bounce['main_failure'])
 
             except Exception as e:
                 print(f"Error in email checker thread: {e}")
