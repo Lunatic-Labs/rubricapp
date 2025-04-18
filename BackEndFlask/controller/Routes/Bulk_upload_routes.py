6da47c0321d1b46d09e502dbf3e3224b21ed8c1e
aldoeve <aldoeve@example.com>
2025-04-18 12:14:12 -0500

    uuid modification

--- BackEndFlask/controller/Routes/Bulk_upload_routes.py
+++ BackEndFlask/controller/Routes/Bulk_upload_routes.py
@@ -39,7 +39,7 @@ def upload_CSV():
 
             user_id = int(request.args.get("user_id"))
 
-            directory = os.path.join(os.getcwd(), "Test-" + str(uuid.uuid4()))
+            directory = os.path.join(os.getcwd(), "Test-" + uuid.uuid4().hex)
 
             os.makedirs(directory, exist_ok=True)
 
