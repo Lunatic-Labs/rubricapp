a2d9e1417f582e5e952c3eb36d76264a96fa774e
aldoeve <aldoeve@example.com>
2025-06-03 15:11:08 -0500

    added case insensitivity

--- .github/workflows/workflow_dependencies.py
+++ .github/workflows/workflow_dependencies.py
@@ -8,9 +8,9 @@
 import difflib
 
 with open('BackEndFlask/requirements.txt', 'r') as req_file:
-    req = [line.replace(" ", "").strip() for line in req_file if line.strip() and not line.startswith("#")]
+    req = [line.replace(" ", "").strip().lower() for line in req_file if line.strip() and not line.startswith("#")]
 with open('temp_dependencies.txt', 'r') as current_file:
-    current = [line.strip() for line in current_file if line.strip()]
+    current = [line.strip().lower() for line in current_file if line.strip()]
 
 diff = difflib.ndiff(req, current)
 delta = ''.join([x for x in diff if x.startswith('-')])
