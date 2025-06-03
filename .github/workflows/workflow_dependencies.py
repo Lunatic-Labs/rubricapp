7d0f8beaff05b5b25bbdc7cb2f623b454040d8d6
aldoeve <aldoeve@example.com>
2025-06-03 15:16:11 -0500

    deleted a file

--- .github/workflows/workflow_dependencies.py
+++ .github/workflows/workflow_dependencies.py
@@ -8,9 +8,9 @@
 import difflib
 
 with open('BackEndFlask/requirements.txt', 'r') as req_file:
-    req = [line.replace(" ", "").strip().lower() for line in req_file if line.strip() and not line.startswith("#")]
+    req = sorted([line.replace(" ", "").strip().lower() for line in req_file if line.strip() and not line.startswith("#")])
 with open('temp_dependencies.txt', 'r') as current_file:
-    current = [line.strip().lower() for line in current_file if line.strip()]
+    current = sorted([line.strip().lower() for line in current_file if line.strip()])
 
 diff = difflib.ndiff(req, current)
 delta = ''.join([x for x in diff if x.startswith('-')])
