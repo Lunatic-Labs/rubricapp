d6349ec44cd6e848614d704892b9a5dff0059591
aldoeve <aldoeve@example.com>
2025-06-03 14:55:56 -0500

    file open logic error corrected and now fetching libs from backend

--- .github/workflows/workflow_dependencies.py
+++ .github/workflows/workflow_dependencies.py
@@ -7,12 +7,9 @@
 
 import difflib
 
-req_file = open('BackEndFlask/requirements.txt')
-current_file = open('temp_dependencies.txt')
-
 with open('BackEndFlask/requirements.txt', 'r') as req_file:
     req = [line.replace(" ", "").strip() for line in req_file if line.strip() and not line.startswith("#")]
-with open('test.txt', 'r') as current_file:
+with open('temp_dependencies.txt', 'r') as current_file:
     current = [line.strip() for line in current_file if line.strip()]
 
 diff = difflib.ndiff(req, current)
