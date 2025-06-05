151077c1bb61db71a6e6da4818f78d18d78774d1
johntable <john.m.tabelisma@gmail.com>
2025-06-04 20:48:12 -0500

    #

--- FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewRatingsTable.js
+++ FrontEndReact/src/View/Admin/View/Reporting/ViewRatings/ViewRatingsTable.js
@@ -26,10 +26,9 @@ class ViewRatingsTable extends Component {
 
       rating["feedback_time_lag"] = currentRating["lag_time"];
 
-      if (currentRating["rating_observable_characteristics_suggestions_data"]) {
-        Object.keys(currentRating["rating_observable_characteristics_suggestions_data"]).forEach((category) => {
-          const categoryData = currentRating["rating_observable_characteristics_suggestions_data"][category];
-          rating[category] = categoryData && categoryData["rating"] ? categoryData["rating"] : 'N/A';
+      if(currentRating["rating_observable_characteristics_suggestions_data"]) {
+        Object.keys(currentRating["rating_observable_characteristics_suggestions_data"]).map((category) => {
+          return rating[category] = currentRating["rating_observable_characteristics_suggestions_data"][category]["rating"];
         });
 
         return allRatings.push(rating);
