c6bca80bd10589ea3fb317697010fd252633672c
johntable <john.m.tabelisma@gmail.com>
2025-06-06 13:55:02 -0500

    debugging

--- BackEndFlask/controller/Routes/Rating_routes.py
+++ BackEndFlask/controller/Routes/Rating_routes.py
@@ -31,7 +31,7 @@ def get_ratings():
     try:
         assessment_task_id = int(request.args.get("assessment_task_id"))
         team_id = request.args.get("team_id")  
-
+        output(f"{datetime.datetime.now()} 1 - Team ID: {team_id}")
         if team_id: 
             ratings = get_team_ratings(assessment_task_id)
             if ratings is None:
@@ -55,6 +55,7 @@ def get_ratings():
                     "rating_observable_characteristics_suggestions_data": team[2],
                     "lag_time": str(lag_time) if lag_time else None,
                 })
+            output(f"{datetime.datetime.now()} 2 - Team ID: {team_id}")
 
         else:
             ratings = get_individual_ratings(assessment_task_id)
@@ -73,7 +74,7 @@ def get_ratings():
                     "rating_observable_characteristics_suggestions_data": rating[2],
                     "lag_time": str(lag_time) if lag_time else None,
                 })
-
+            output(f"{datetime.datetime.now()} 3 - Team ID: {team_id}")
         return create_good_response(result, 200, "ratings")
 
     except Exception as e:
