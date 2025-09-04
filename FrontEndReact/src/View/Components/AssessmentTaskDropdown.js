5c8e273f16e8cecf82a86b809f2f5d81e0d9fee2
TC <timoshka.chirkov@gmail.com>
2025-09-04 16:38:35 -0500

    A stable way to truncate assesment task without causing errors on the page. (I tested it, it works)

--- FrontEndReact/src/View/Components/AssessmentTaskDropdown.js
+++ FrontEndReact/src/View/Components/AssessmentTaskDropdown.js
@@ -5,14 +5,25 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 
-
 export default function AssessmentTaskDropdown(props) {
   var assessmentTaskList = [];
 
   props.assessmentTasks.map((assessmentTask) => {
+    // new code here
+
+    // This new code shortens the name of assessment task when it is displayed on the page
+    // this prevents the text from 'overflowing' and causing miss-alignment with other elements on the page.
+    // However this is a fixed size and thus cannot be altered, should a solution be made to automatically truncate the text?
+    // It also seems to affect the name in the drop-down menu, that might require fixing...
+    const truncatedName = assessmentTask["assessment_task_name"].length > 30 
+    ? assessmentTask["assessment_task_name"].substring(0, 30) + "..."
+    : assessmentTask["assessment_task_name"];
+
+    // {assessmentTask["assessment_task_name"]} was in place of {truncatedName}
+
     return assessmentTaskList.push(
       <MenuItem key={assessmentTask["assessment_task_id"]} value={assessmentTask["assessment_task_id"]}>
-        {assessmentTask["assessment_task_name"]}
+        {truncatedName}
       </MenuItem>
     );
   });
