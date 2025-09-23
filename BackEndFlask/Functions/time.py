6f6efda0cbe3464e601fce353ecbb63527a7d243
EliSullivan <easullivan@mail.lipscomb.edu>
2025-09-23 15:48:49 -0500

    added back the parse_and_convert_timezone() function in case it is going to be used in the future. Currently, completed_assessment.py uses the ensure_utc_datetime() function instead, though.

--- BackEndFlask/Functions/time.py
+++ BackEndFlask/Functions/time.py
@@ -11,6 +11,35 @@ timezone_list = {
     "EDT": "America/New_York",
 }
 
+def parse_and_convert_timezone(time_str, assessment_task):
+    """
+    Description:
+    Parse and convert a time string to the chosen assessment task's timezone.
+    
+    Parameters:
+    time_str: Format time string in UTC (default time)
+    assessment_task: AssessmentTask object
+    
+    Returns:
+    Timezone-aware datetime in the assessment task's timezone
+    """
+
+    if "." not in time_str:
+        time_str = time_str + ".000"
+    
+    if "Z" not in time_str:
+        time_str = time_str + "Z"
+
+    
+    # Parse the time string into a UTC datetime object
+    utc_time = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=pytz.UTC)
+    
+    # Handles the conversion of the chosen assessment task timezone
+    if assessment_task and assessment_task.time_zone:
+        pytz_timezone = pytz.timezone(timezone_list.get(assessment_task.time_zone, "UTC"))
+        return utc_time.astimezone(pytz_timezone)
+    
+    return utc_time
 def ensure_utc_datetime(dt):
     """
     Ensure a datetime object is timezone-aware and in UTC.
