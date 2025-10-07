a6639c842b64bd5c074f634271d9d5eec65b24a6
Abigail Lindquester <alindquester@gmail.com>
2025-10-07 15:18:06 -0500

    Created new migration file for change

---
+++ BackEndFlask/migrations/versions/9aa494f95475_changed_role_name_from_text_to_string.py
@@ -0,0 +1,24 @@
+"""Changed role_name from text to string
+
+Revision ID: 9aa494f95475
+Revises: 0d58fa9b8925
+Create Date: 2025-10-07 15:14:45.243503
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9aa494f95475'
+down_revision = '0d58fa9b8925'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
