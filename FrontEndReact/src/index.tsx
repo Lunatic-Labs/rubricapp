39e2ff1b950d7772348169297f8841ef652df4f8
Antonsalib <aisalib@mail.lipscomb.edu>
2025-11-20 16:02:51 -0600

    "fix"

--- FrontEndReact/src/index.tsx
+++ FrontEndReact/src/index.tsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// @ts-expect-error TS(2307): Cannot find module 'react' or its corresponding ty... Remove this comment to see the full error message
-import React from 'react';
-// @ts-expect-error TS(2307): Cannot find module 'react-dom/client' or its corre... Remove this comment to see the full error message
-import ReactDOM from 'react-dom/client';
-import './SBStyles.css';
-import App from './App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    // @ts-expect-error TS(2307): Cannot find module 'react/jsx-runtime' or its corr... Remove this comment to see the full error message
-    <App />
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './SBStyles.css';
@@ -30,4 +16,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
->>>>>>> 5b40833d7f48a101e02bae62763a138e667e940b
