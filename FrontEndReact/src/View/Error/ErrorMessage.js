611d78581f32623a3a9270318d52be51744b94e9
TC <timoshka.chirkov@gmail.com>
2025-11-30 16:52:11 -0600

    fixed documentation inconsistency in AppState.js, finished Loading.js and ErrorMEssage.js

--- FrontEndReact/src/View/Error/ErrorMessage.js
+++ FrontEndReact/src/View/Error/ErrorMessage.js
@@ -3,6 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Box, Alert } from '@mui/material';
 
 
+/**
+ * Creates an instance of the ErrorMessage component
+ * 
+ * @constructor
+ * @param {Object} props - The properties passed to the component.
+ * @param {string|null} props.errorMessage - The error message text to display, if null or empty, the component still renders but displays no content.
+ */
 
 class ErrorMessage extends Component {
     render() {
--- FrontEndReact/src/View/Navbar/AppState.js
+++ FrontEndReact/src/View/Navbar/AppState.js
@@ -38,7 +38,7 @@ import ViewNotification from '../Admin/View/ViewDashboard/Notifications.js';
  * Creates an instance of the AppState component.
  * 
  * @constructor
- * @param {Object} props - Component properties.
+ * @param {Object} props - The properties passed to the component..
  * @param {string} props.userName - Displayed in the navigation bar.
  * @param {function} props.logout - Callback for user logout.
  * @param {boolean} props.isSuperAdmin - Determines whether the user has Super Admin privileges.
