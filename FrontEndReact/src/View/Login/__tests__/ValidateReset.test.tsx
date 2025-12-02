badcdcab2a6771ff8d36cb0348c2a5bb5fc8b164
Antonsalib <aisalib@mail.lipscomb.edu>
2025-12-02 11:15:04 -0600

    fix one check

--- FrontEndReact/src/View/Login/__tests__/ValidateReset.test.tsx
+++ FrontEndReact/src/View/Login/__tests__/ValidateReset.test.tsx
@@ -1,4 +1,4 @@
-import { test, expect } from "@jest/globals";
+import { test } from "@jest/globals";
 import { render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "../Login";
