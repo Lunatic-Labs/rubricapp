664745433850aceb62ee5a423bb5e0fd904026c3
William Monin <billjamn@gmail.com>
2026-02-26 14:30:29 -0600

    Restore new files from master

---
+++ BackEndFlask/enums/Bounced_email.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------
+# Contains system-wide bounced email enums.
+#---------------------------------------------------
+
+from enum import Enum
+
+class BouncedEmailFields(Enum):
+    """ Represents the feilds a bounced email dict has in the utility.py file."""
+    ID = 'id'
+    TO = 'to'
+    MSG = 'msg'
+    SENDER = 'sender'
+    MAIN_FAILURE = 'main_failure'
