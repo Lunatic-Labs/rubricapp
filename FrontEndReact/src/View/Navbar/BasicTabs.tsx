95c455eee8c6ebce52176194f836126ebcc5d8a4
Abigail Lindquester <alindquester@gmail.com>
2026-03-16 15:07:54 -0500

    finished out View/Admin/Navbar

--- FrontEndReact/src/View/Navbar/BasicTabs.tsx
+++ FrontEndReact/src/View/Navbar/BasicTabs.tsx
@@ -19,7 +19,11 @@ import Box from '@mui/material/Box';
  * @property {boolean} useFixedTeams - Determines whether the "Teams" tab is shown, based on the selected course's `use_fixed_teams` configuration.
  */
 
-export default function BasicTabs (props: any){
+interface BasicTabsProps {
+    navbar: any;
+}
+
+export default function BasicTabs (props: BasicTabsProps){
   var navbar = props.navbar;
   var state = navbar.state;
   var activeTab = state.activeTab;
@@ -44,7 +48,7 @@ export default function BasicTabs (props: any){
    * @param {number} newValue - The index of the newly selected tab within the MUI Tabs component.
    */
 
-  const handleChange = (event: any, newValue: any) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
