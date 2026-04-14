2206a19e63cb2cf652e46ef8f5d7ce1874372b9b
Abigail Lindquester <alindquester@gmail.com>
2026-04-14 14:44:31 -0500

    merged master to branch

--- BackEndFlask/migrations/versions/a1b2c3d4e5f6_add_time_zone_to_course.py
+++ BackEndFlask/migrations/versions/a1b2c3d4e5f6_add_time_zone_to_course.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a1b2c3d4e5f6'
-down_revision = '51ae14b2150d'
+down_revision = 'b3a7c2d0e1f8'
 branch_labels = None
 depends_on = None
 
