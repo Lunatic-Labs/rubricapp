7bd230d1f898ba0dfec72a34c112e8800bf697d5
aldoeve <aldoeve@example.com>
2026-05-20 18:58:08 -0500

    Removed: cleaned up left over debugging modifications

--- FrontEndReact/src/index.tsx
+++ FrontEndReact/src/index.tsx
@@ -8,7 +8,6 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
-        <React.StrictMode><App /></React.StrictMode>
-        //<App />
+        <App />
     );
 }
