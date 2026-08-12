2b66c5eefe16cef48d6eef8c18534fd3e0edd441
aldoeve <aldoeve@example.com>
2026-08-12 16:20:55 -0500

    build(vite.config.ts)!: add legacy mode.

--- FrontEndReact/vite.config.ts
+++ FrontEndReact/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  
+  legacy: {
+    inconsistentCjsInterop: true,
+  },
+
   server: {
     port: 3000,
     host: '0.0.0.0',
