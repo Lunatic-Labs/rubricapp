e93a5e53b20fd7fbc566cda20e57c939d85b5342
aldoeve <aldoeve@example.com>
2026-08-11 21:48:58 -0500

    test: fix viewsasstudent.tsx test

--- FrontEndReact/src/View/Student/__tests__/ViewAsStudent.test.tsx
+++ FrontEndReact/src/View/Student/__tests__/ViewAsStudent.test.tsx
@@ -90,7 +90,8 @@ describe("View as Student Feature Tests", () => {
         role_id: 6,
         has_set_password: true,
         viewingAsStudent: true,
-        originalCourseId: 1
+        originalCourseId: 1,
+        course_id: 1
     };
 
     const testCoursesArray = [
