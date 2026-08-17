36645955add4f4105552bb6b962f3de88b6d7dce
aldoeve <aldoeve@example.com>
2026-08-17 18:04:19 -0500

    chore: update merge driver

--- Scripts/setupMergeDriver.sh
+++ Scripts/setupMergeDriver.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-
 git config merge.regen-lockfile.name "Regenerate lockfile"
-
-git config merge.regen-lockfile.driver \
-  'sh -c "cd FrontEndReact && npm install && cp package-lock.json \"$1\"" _ %A'
+git config merge.regen-lockfile.driver 'sh -c "cd FrontEndReact && npm install && cp package-lock.json \"$1\"" _ %A'
